18912c877662fdf6ff7400ecf93ba6591db0d3c3
Ferdinand <fexcalo@gmail.com>
2018-11-21 20:27:45 +0100

    removed unused methods.

--- src/net/fexcraft/app/fmt/porters/JsonToTMT.java
+++ src/net/fexcraft/app/fmt/porters/JsonToTMT.java
@@ -1,6 +1,5 @@
 package net.fexcraft.app.fmt.porters;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import net.fexcraft.app.fmt.wrappers.BoxWrapper;
@@ -10,7 +9,6 @@ import net.fexcraft.app.fmt.wrappers.PolygonWrapper;
 import net.fexcraft.app.fmt.wrappers.ShapeboxWrapper;
 import net.fexcraft.lib.common.json.JsonUtil;
 import net.fexcraft.lib.common.math.Vec3f;
-import net.fexcraft.lib.tmt.ModelRendererTurbo;
 
 /**
 * Tool to parse `ModelRendererTurbo` objects from JSON.
@@ -54,73 +52,6 @@ public class JsonToTMT {
 	public static final String[] topoffx = new String[]{"top_offset_x", "topoff_x", "topoffx"};
 	public static final String[] topoffy = new String[]{"top_offset_y", "topoff_y", "topoffy"};
 	public static final String[] topoffz = new String[]{"top_offset_z", "topoff_z", "topoffz"};
-	
-	public final static ModelRendererTurbo parse(JsonObject obj, int tx, int ty){
-		ModelRendererTurbo model = new ModelRendererTurbo(null, get(texturex, obj, idef), get(texturey, obj, idef), tx, ty);
-		//
-		float x = get(offx, obj, def);
-		float y = get(offy, obj, def);
-		float z = get(offz, obj, def);
-		int w = get(width, obj, idef);
-		int h = get(height, obj, idef);
-		int d = get(depth, obj, idef);
-		//
-		switch(obj.get("type").getAsString()){
-			case "box": case "cube": case "b": {
-				model.addBox(x, y, z, w, h, d, get(expansion, obj, def));
-				break;
-			}
-			case "shapebox": case "sbox": case "sb": {
-				model.addShapeBox(x, y, z, w, h, d, get(scale, obj, def),
-						get("x0", obj, def), get("y0", obj, def), get("z0", obj, def),
-						get("x1", obj, def), get("y1", obj, def), get("z1", obj, def),
-						get("x2", obj, def), get("y2", obj, def), get("z2", obj, def),
-						get("x3", obj, def), get("y3", obj, def), get("z3", obj, def),
-						get("x4", obj, def), get("y4", obj, def), get("z4", obj, def),
-						get("x5", obj, def), get("y5", obj, def), get("z5", obj, def),
-						get("x6", obj, def), get("y6", obj, def), get("z6", obj, def),
-						get("x7", obj, def), get("y7", obj, def), get("z7", obj, def)
-					);
-				break;
-			}
-			case "cylinder": case "cyl": case "c": {
-				model.addCylinder(x, y, z, get(radius, obj, 1f), get(length, obj, 1f), get(segments, obj, 16), get(basescale, obj, 1f), get(topscale, obj, 1f), get(direction, obj, 4));
-				break;
-			}
-			case "cone": case "cn": {
-				model.addCone(x, y, z, get(radius, obj, 1f), get(length, obj, 1f), get(segments, obj, 12), get(basescale, obj, 1f), get(direction, obj, 4));
-				break;
-			}
-		}
-		model.mirror = JsonUtil.getIfExists(obj, mirror, false);
-		model.flip = JsonUtil.getIfExists(obj, flip, false);
-		//
-		model.rotateAngleX = get(rotx, obj, def);
-		model.rotateAngleY = get(roty, obj, def);
-		model.rotateAngleZ = get(rotz, obj, def);
-		//
-		model.boxName = obj.has("name") ? obj.get("name").getAsString() : null;
-		model.setRotationPoint(get(posx, obj, def), get(posy, obj, def), get(posz, obj, def));
-		return model;
-	}
-
-	public final static ModelRendererTurbo[] parse(JsonArray array, int tx, int ty){
-		if(array != null){
-			ModelRendererTurbo[] model = new ModelRendererTurbo[array.size()];
-			for(int i = 0; i < array.size(); i++){
-				model[i] = parse(array.get(i).getAsJsonObject(), tx, ty);
-			}
-			return model;
-		}
-		return new ModelRendererTurbo[0];
-	}
-
-	public final static ModelRendererTurbo[] parse(String string, JsonObject object, int tx, int ty){
-		if(object.has(string) && object.get(string).isJsonArray()){
-			return parse(object.get(string).getAsJsonArray(), tx, ty);
-		}
-		return new ModelRendererTurbo[0];
-	}
 	
 	public static final float get(String s, JsonObject obj, float def){
 		if(obj.has(s)){
