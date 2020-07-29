e5d1b112dab561963797506890e5e767e4bc5df0
Ferdinand <fercalo96@yahoo.de>
2020-07-29 11:10:02 +0200

    Font (Model) Utils backend.

--- src/net/fexcraft/app/fmt/utils/FontUtils.java
+++ src/net/fexcraft/app/fmt/utils/FontUtils.java
@@ -1,9 +1,11 @@
 package net.fexcraft.app.fmt.utils;
 
+import static net.fexcraft.app.fmt.utils.Logging.log;
 import static net.fexcraft.app.fmt.utils.Translator.translate;
 import static org.liquidengine.legui.event.MouseClickEvent.MouseClickAction.CLICK;
 
 import java.io.File;
+import java.util.zip.ZipFile;
 
 import org.liquidengine.legui.component.Button;
 import org.liquidengine.legui.component.Dialog;
@@ -12,10 +14,18 @@ import org.liquidengine.legui.component.SelectBox;
 import org.liquidengine.legui.event.MouseClickEvent;
 import org.liquidengine.legui.listener.MouseClickEventListener;
 
+import com.google.gson.JsonObject;
+
 import net.fexcraft.app.fmt.FMTB;
+import net.fexcraft.app.fmt.ui.DialogBox;
 import net.fexcraft.app.fmt.ui.UserInterfaceUtils;
 import net.fexcraft.app.fmt.ui.field.NumberField;
 import net.fexcraft.app.fmt.ui.field.TextField;
+import net.fexcraft.app.fmt.wrappers.GroupCompound;
+import net.fexcraft.app.fmt.wrappers.PolygonWrapper;
+import net.fexcraft.app.fmt.wrappers.TurboList;
+import net.fexcraft.lib.common.json.JsonUtil;
+import net.fexcraft.lib.common.utils.ZipUtil;
 
 public class FontUtils {
 	
@@ -24,6 +34,9 @@ public class FontUtils {
 	private static File selected;
 	private static int width = 300;
 	private static String text;
+	//
+	private static float space_width;
+	private static Char[] chars;
 
 	public static void open(){
 		Dialog dialog = new Dialog(translate("font_util.dialog"), width, 0);
@@ -85,7 +98,109 @@ public class FontUtils {
 	}
 
 	private static void generate(){
-		//
+		new Thread("font-gen"){
+			@Override
+			public void run(){
+				generate0();
+			}
+		}.start();
+	}
+	
+	protected static void generate0(){
+		log("Collecting data to generate font...");
+		log("Font Text: " + text);
+		log("Font Location: " + px + ", " + py + ", " + pz);
+		log("Font Rotation: " + rx + ", " + ry + ", " + rz);
+		if(selected == null || !selected.exists()){
+			log("Invalid or no file selected, cancelling font generation.");
+			return;
+		}
+		log("Font File: " + selected.getPath());
+		loadFontModelFile(selected);
+		String textgroupid = genTextGroupId();
+		char[] textchars = text.toCharArray();
+		float passed = 0;
+		for(int i = 0; i < textchars.length; i++){
+			if(textchars[i] == ' '){
+				passed += space_width;
+				continue;
+			}
+			Char cher = findChar(textchars[i]);
+			if(cher == null){
+				passed += space_width;
+				continue;
+			}
+			for(PolygonWrapper wrapper : cher.wrappers){
+				PolygonWrapper clone = wrapper.clone(FMTB.MODEL);
+				clone.pos.xCoord -= cher.offset;
+				clone.off.xCoord += passed;
+				passed += cher.width;
+				clone.pos = clone.pos.addVector(px, py, pz);
+				clone.rot = clone.rot.addVector(rx, ry, rz);
+				FMTB.MODEL.add(clone, textgroupid, false);
+			}
+		}
+		FMTB.MODEL.clearSelection();
+		FMTB.MODEL.getGroups().get(textgroupid).selected = true;
+		FMTB.MODEL.recompile();
+	}
+
+	private static String genTextGroupId(){
+		int idint = 0;
+		while(FMTB.MODEL.getGroups().contains("text" + idint)) idint++;
+		return "text" + idint;
+	}
+
+	private static void loadFontModelFile(File file){
+		GroupCompound compound = null;
+		try{
+			boolean contains = ZipUtil.contains(file, "model.jtmt");
+			ZipFile zip = new ZipFile(file);
+			JsonObject obj = JsonUtil.getObjectFromInputStream(zip.getInputStream(zip.getEntry("model.jtmt")));
+			if(contains){
+				compound = SaveLoad.getModel(file, obj, false, true);
+				//if(!compound.name.startsWith("font/")) compound.name = "font/" + compound.name;
+			}
+			else{
+				DialogBox.showOK("font_util.load_pack.title", null, null, "font_util.load_pack.title.invalid_file");
+			}
+			zip.close();
+			//compound.recompile();
+			zip.close();
+		}
+		catch(Exception e){
+			log(e);
+			DialogBox.showOK("font_util.load_pack.title", null, null, "font_util.load_pack.title.errors");
+		}
+		space_width = compound.name.contains("_") ? Float.parseFloat(compound.name.split("_")[1]) : 4;
+		chars = new Char[compound.getGroups().size()];
+		int index = 0;
+		for(TurboList list : compound.getGroups()){
+			Char cher = new Char();
+			String[] arr = list.id.split("_");
+			cher.id = arr[0].toCharArray()[0];
+			if(arr.length > 1) cher.width = Float.parseFloat(arr[1]);
+			if(arr.length > 2) cher.height = Float.parseFloat(arr[2]);
+			if(arr.length > 3) cher.offset = Float.parseFloat(arr[3]);
+			cher.wrappers = list;
+			chars[index++] = cher;
+		}
+	}
+
+	public static class Char {
+		
+		private char id;
+		@SuppressWarnings("unused")
+		private float width, height, offset;
+		private TurboList wrappers;
+		
+	}
+
+	private static Char findChar(char c){
+		for(Char cher : chars){
+			if(cher.id == c) return cher;
+		}
+		return null;
 	}
 
 }
