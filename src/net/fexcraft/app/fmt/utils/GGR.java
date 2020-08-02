dd8d4f033e6b66d14725821e14ce2636398dbf27
Ferdinand <fercalo96@yahoo.de>
2020-08-02 10:14:19 +0200

    Update GGR.java

--- src/net/fexcraft/app/fmt/utils/GGR.java
+++ src/net/fexcraft/app/fmt/utils/GGR.java
@@ -66,7 +66,7 @@ public class GGR {
     public void pollInput(float delta){
 		if(grabbed && cursor_moved){
         	double x = posx - oposx, y = posy - oposy;
-        	if(x < FMTB.WIDTH / 2 && y < FMTB.HEIGHT / 2){
+        	if(x < FMTB.WIDTH && y < FMTB.HEIGHT){
                 if(Settings.orbital_camera()){
                     rotation.yCoord -= x * Settings.mouse_sensivity.directFloat() * delta;
                     rotation.xCoord += y * Settings.mouse_sensivity.directFloat() * delta;
