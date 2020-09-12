573d140fbb770882df3b18cf516935fdff413b6f
Ferdinand <fercalo96@yahoo.de>
2020-09-12 15:21:56 +0200

    Update MarkerExporter.java

--- src/net/fexcraft/app/fmt/porters/MarkerExporter.java
+++ src/net/fexcraft/app/fmt/porters/MarkerExporter.java
@@ -17,6 +17,7 @@ import com.google.gson.JsonObject;
 import net.fexcraft.app.fmt.FMTB;
 import net.fexcraft.app.fmt.porters.PorterManager.ExImPorter;
 import net.fexcraft.app.fmt.utils.Setting;
+import net.fexcraft.app.fmt.utils.Setting.StringArraySetting;
 import net.fexcraft.app.fmt.wrappers.GroupCompound;
 import net.fexcraft.app.fmt.wrappers.MarkerWrapper;
 import net.fexcraft.app.fmt.wrappers.PolygonWrapper;
@@ -34,7 +35,7 @@ public class MarkerExporter extends ExImPorter {
 	private static final ArrayList<Setting> settings = new ArrayList<>();
 	
 	public MarkerExporter(){
-		settings.add(new Setting("as_fvtm_seats", true));
+		settings.add(new StringArraySetting("type", "normal", "fvtm_seats_json", "tsiv_coords"));
 	}
 
 	@Override
@@ -45,8 +46,8 @@ public class MarkerExporter extends ExImPorter {
 	@Override
 	public String exportModel(GroupCompound compound, File file, Map<String, Setting> settings){
 		StringBuffer buffer = new StringBuffer();
-		boolean bool = settings.get("as_fvtm_seats").getBooleanValue();
-		if(bool){
+		String type = settings.get("type").getStringValue();
+		if(type.equals("fvtm_seats_json")){
 			JsonObject obj = new JsonObject();
 			obj.addProperty("__comment", "FVTM Seat List // FMT version: " + FMTB.VERSION);
 			for(TurboList list : compound.getGroups()){
@@ -69,14 +70,19 @@ public class MarkerExporter extends ExImPorter {
 			JsonUtil.write(file, obj);
 			return "Success!";
 		}
+		boolean tsiv = type.equals("tsiv_coords");
 		buffer.append("# FMT Marker List // FMT version: " + FMTB.VERSION + "\n");
+		if(tsiv) buffer.append("# TS/IV Corrected Export Mode\n");
 		buffer.append("# Model: " + (compound.name == null ? "unnamed" : compound.name.toLowerCase()) + "\n\n");
 		for(TurboList list : compound.getGroups()){
 			List<PolygonWrapper> coll = list.stream().filter(pre -> pre instanceof MarkerWrapper).collect(Collectors.toList());
 			if(!coll.isEmpty()){
 				buffer.append("# Group: " + list.id + "\n");
 				for(PolygonWrapper wrapper : list){
-					buffer.append(wrapper.name() + ": " + wrapper.pos.xCoord + ", " + wrapper.pos.yCoord + ", " + wrapper.pos.zCoord + ";\n");
+					if(tsiv){
+						buffer.append(String.format("\"pos\": [ %s, %s, %s],\n", nmz(wrapper.pos.zCoord / 16), nmz(-wrapper.pos.yCoord / 16), nmz(wrapper.pos.xCoord / 16)));
+					}
+					else buffer.append(wrapper.name() + ": " + wrapper.pos.xCoord + ", " + wrapper.pos.yCoord + ", " + wrapper.pos.zCoord + ";\n");
 				}
 			}
 		}
@@ -90,6 +96,11 @@ public class MarkerExporter extends ExImPorter {
 			return "Error:" + e.getMessage();
 		}
 		return "Success!";
+	}
+
+	private String nmz(float f){
+		if(f > 0 || f < 0) return f + "";
+		return "0";
 	}
 
 	@Override
