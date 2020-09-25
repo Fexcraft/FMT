4013f5733dc6bd9da30f90b39dd91a0809c64d56
Ferdinand <fercalo96@yahoo.de>
2020-09-25 23:10:01 +0200

    Update Logging.java

--- src/net/fexcraft/app/fmt/utils/Logging.java
+++ src/net/fexcraft/app/fmt/utils/Logging.java
@@ -31,16 +31,23 @@ public class Logging {
 	}
 	
 	public static void log(Throwable e){
+		log((String)null, e);
+	}
+	
+	public static void log(String errorinfo, Throwable e){
 		LOGGER_MAIN.log(Level.INFO, "ERROR: " + e.getLocalizedMessage());
+		if(errorinfo != null) LOGGER_MAIN.log(Level.INFO, "INFO: " + errorinfo);
 		//LOGGER_MAIN.log(Level.INFO, "ERROR: " + e.getMessage());
 		for(StackTraceElement trace : e.getStackTrace()){
 			LOGGER_MAIN.log(Level.ERROR, "\t" + trace);
 		}
 		try{
-			String[] str = new String[e.getStackTrace().length + 1];
+			String[] str = new String[e.getStackTrace().length + 1 + (errorinfo == null ? 0 : 1)];
 			str[0] = "ERROR: " + e.getLocalizedMessage();
-			for(int i = 0; i + 1 < str.length; i++){
-				str[i + 1] = "\t" + e.getStackTrace()[i];
+			if(errorinfo != null) str[1] = "INFO: " + errorinfo;
+			int idx = errorinfo == null ? 1 : 2;
+			for(int i = 0; i + idx < str.length; i++){
+				str[i + idx] = "\t" + e.getStackTrace()[i];
 			}
 			DialogBox.show(600, "error.dialog_title", "dialogbox.button.ok", "toolbar.utils.clipboard.copy", null, () -> {
 				String string = new String(str[0] + "\n");
