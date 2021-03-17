1a81b75c23800bc66217a79a80742f027fd91947
Ferdinand <fercalo96@yahoo.de>
2021-03-17 23:18:16 +0100

    Internal File Chooser

--- src/net/fexcraft/app/fmt/ui/FileChooser.java
+++ src/net/fexcraft/app/fmt/ui/FileChooser.java
@@ -1,13 +1,25 @@
 package net.fexcraft.app.fmt.ui;
 
 import static net.fexcraft.app.fmt.settings.Settings.INTERNAL_CHOOSER;
+import static org.liquidengine.legui.event.MouseClickEvent.MouseClickAction.CLICK;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.function.Consumer;
 
+import org.liquidengine.legui.component.Button;
+import org.liquidengine.legui.component.Dialog;
+import org.liquidengine.legui.component.Label;
+import org.liquidengine.legui.component.ScrollablePanel;
+import org.liquidengine.legui.component.optional.align.HorizontalAlign;
+import org.liquidengine.legui.event.MouseClickEvent;
+import org.liquidengine.legui.listener.MouseClickEventListener;
 import org.lwjgl.PointerBuffer;
 import org.lwjgl.system.MemoryStack;
 import org.lwjgl.util.tinyfd.TinyFileDialogs;
+
+import net.fexcraft.app.fmt.FMT;
+import net.fexcraft.app.fmt.utils.Translator;
 
 /**
  * 
@@ -19,6 +31,7 @@ public class FileChooser {
 	public static FileType TYPE_PNG = new FileType("Portable Network Graphics", "*.png");
 	public static FileType TYPE_IMG = new FileType("Image Files", "*.png", ".jpg", ".jpeg", ".bmp");
 	public static FileType TYPE_FMTB = new FileType("FMT Save File", "*.fmtb");
+	public static final int INTERNAL_HEIGHT = 500;
 	
 	public static void chooseFile(String title, String root, FileType type, boolean save, Consumer<File> task){
 		if(!root.endsWith("/")) root += "/";
@@ -46,8 +59,79 @@ public class FileChooser {
 	}
 	
 	private static void openInternalChooser(String title, String root, FileType type, boolean save, Consumer<File> task){
-		// TODO Auto-generated method stub
-		
+		Dialog dialog = new Dialog(Translator.translate("filechooser.title"), 500, INTERNAL_HEIGHT + (save ? 30 : 0));
+        Label label = new Label(title, 10, 10, 480, 20);
+        File rootfile = new File(root);
+        if(!rootfile.exists()) rootfile.mkdirs();
+		Button rootbutton = new Button(rootfile.getAbsolutePath(), 10, 40, 480, 20);
+		rootbutton.getStyle().setBorderRadius(0);
+		rootbutton.getStyle().setHorizontalAlign(HorizontalAlign.LEFT);
+		rootbutton.getListenerMap().addListener(MouseClickEvent.class, (MouseClickEventListener) e -> {
+        	if(CLICK == e.getAction()){
+        		if(rootfile.getParentFile() != null){
+        			dialog.close();
+        			openInternalChooser(title, rootfile.getParentFile().getAbsolutePath(), type, save, task);
+        		}
+        	}
+		});
+		ScrollablePanel panel = new ScrollablePanel(10, 70, 480, INTERNAL_HEIGHT - 100);
+        panel.setHorizontalScrollBarVisible(false);
+		ArrayList<File> filtered = new ArrayList<>();
+		if(rootfile.listFiles() != null){
+			for(File file : rootfile.listFiles()){
+				if(file.isDirectory()){
+					filtered.add(file);
+					continue;
+				}
+				for(int i = 0; i < type.extensions.length; i++){
+					if(file.getName().endsWith(type.extensions[i].replace("*", ""))){
+						filtered.add(file);
+						continue;
+					}
+				}
+			}
+		}
+		else{
+			filtered.add(new File("error.no_files_here"));
+		}
+		filtered.sort((file, other) -> {
+			if(file.isDirectory() && !other.isDirectory()){
+				return -1;
+			}
+			if(!file.isDirectory() && other.isDirectory()){
+				return 1;
+			}
+			return file.compareTo(other);
+		});
+		int size = filtered.size() * 22;
+		panel.getContainer().setSize(panel.getSize().x, size < panel.getSize().y ? panel.getSize().y : size);
+		for(int i = 0; i < filtered.size(); i++){
+			File file = filtered.get(i);
+			Button filebutton = new Button(file.getName() + (file.isDirectory() ? "/" : ""), 5, 1 + i * 22, 470, 20);
+			filebutton.getStyle().setBorderRadius(0);
+			filebutton.getStyle().getBorder().setEnabled(false);
+			filebutton.getStyle().setHorizontalAlign(HorizontalAlign.LEFT);
+			filebutton.getListenerMap().addListener(MouseClickEvent.class, (MouseClickEventListener) e -> {
+	        	if(CLICK == e.getAction()){
+        			dialog.close();
+	        		if(file.isDirectory()){
+	        			openInternalChooser(title, file.getAbsolutePath(), type, save, task);
+	        		}
+	        		else{
+	        			task.accept(file);
+	        		}
+	        	}
+			});
+			panel.getContainer().add(filebutton);
+		}
+		if(save){
+			//TODO text field for filename input
+		}
+		dialog.setResizable(false);
+		dialog.getContainer().add(panel);
+		dialog.getContainer().add(rootbutton);
+		dialog.getContainer().add(label);
+		dialog.show(FMT.FRAME);
 	}
 
 	public static record FileType(String name, String... extensions){}
