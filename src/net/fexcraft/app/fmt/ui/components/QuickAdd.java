cf6dd1bbe0e12b344ff08889649ab68cec358361
Ferdinand <fercalo96@yahoo.de>
2021-05-12 00:34:11 +0200

    Update QuickAdd.java

--- src/net/fexcraft/app/fmt/ui/components/QuickAdd.java
+++ src/net/fexcraft/app/fmt/ui/components/QuickAdd.java
@@ -2,6 +2,7 @@ package net.fexcraft.app.fmt.ui.components;
 
 import net.fexcraft.app.fmt.FMT;
 import net.fexcraft.app.fmt.polygon.Box;
+import net.fexcraft.app.fmt.polygon.Cylinder;
 import net.fexcraft.app.fmt.polygon.Shapebox;
 import net.fexcraft.app.fmt.ui.EditorComponent;
 import net.fexcraft.app.fmt.ui.Icon;
@@ -13,7 +14,7 @@ public class QuickAdd extends EditorComponent {
 		int yoff = HEIGHT, xoff = 4, size = 28;
 		this.add(new Icon(0, size, 2, xoff, yoff, "./resources/textures/icons/polygon/box.png", () -> addBox()).addTooltip(LANG_PREFIX + id + ".add_box"));
 		this.add(new Icon(1, size, 2, xoff, yoff, "./resources/textures/icons/polygon/shapebox.png", () -> addShapebox()).addTooltip(LANG_PREFIX + id + ".add_shapebox"));
-		this.add(new Icon(2, size, 2, xoff, yoff, "./resources/textures/icons/polygon/cylinder.png", () -> {}).addTooltip(LANG_PREFIX + id + ".add_cylinder"));
+		this.add(new Icon(2, size, 2, xoff, yoff, "./resources/textures/icons/polygon/cylinder.png", () -> addCylinder()).addTooltip(LANG_PREFIX + id + ".add_cylinder"));
 		this.add(new Icon(3, size, 2, xoff, yoff, "./resources/textures/icons/polygon/boundingbox.png", () -> {}).addTooltip(LANG_PREFIX + id + ".add_boundingbox"));
 		this.add(new Icon(4, size, 2, xoff, yoff, "./resources/textures/icons/polygon/object.png", () -> {}).addTooltip(LANG_PREFIX + id + ".add_object"));
 		this.add(new Icon(5, size, 2, xoff, yoff, "./resources/textures/icons/polygon/marker.png", () -> {}).addTooltip(LANG_PREFIX + id + ".add_marker"));
@@ -33,6 +34,10 @@ public class QuickAdd extends EditorComponent {
 		String name = "group" + FMT.MODEL.groups().size();
 		while(FMT.MODEL.contains(name)) name += "0";
 		FMT.MODEL.addGroup(name);
+	}
+
+	public static void addCylinder(){
+		FMT.MODEL.add(null, new Cylinder(null));
 	}
 
 }
