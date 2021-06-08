89b98ff0d72a05fabdc2a0734a4e1a4585669241
Ferdinand <fercalo96@yahoo.de>
2021-06-08 22:21:48 +0200

    Update QuickAdd.java

--- src/net/fexcraft/app/fmt/ui/components/QuickAdd.java
+++ src/net/fexcraft/app/fmt/ui/components/QuickAdd.java
@@ -3,6 +3,7 @@ package net.fexcraft.app.fmt.ui.components;
 import net.fexcraft.app.fmt.FMT;
 import net.fexcraft.app.fmt.polygon.Box;
 import net.fexcraft.app.fmt.polygon.Cylinder;
+import net.fexcraft.app.fmt.polygon.Marker;
 import net.fexcraft.app.fmt.polygon.Shapebox;
 import net.fexcraft.app.fmt.ui.EditorComponent;
 import net.fexcraft.app.fmt.ui.Icon;
@@ -17,7 +18,7 @@ public class QuickAdd extends EditorComponent {
 		this.add(new Icon(2, size, 2, xoff, yoff, "./resources/textures/icons/polygon/cylinder.png", () -> addCylinder()).addTooltip(LANG_PREFIX + id + ".add_cylinder"));
 		this.add(new Icon(3, size, 2, xoff, yoff, "./resources/textures/icons/polygon/boundingbox.png", () -> {}).addTooltip(LANG_PREFIX + id + ".add_boundingbox"));
 		this.add(new Icon(4, size, 2, xoff, yoff, "./resources/textures/icons/polygon/object.png", () -> {}).addTooltip(LANG_PREFIX + id + ".add_object"));
-		this.add(new Icon(5, size, 2, xoff, yoff, "./resources/textures/icons/polygon/marker.png", () -> {}).addTooltip(LANG_PREFIX + id + ".add_marker"));
+		this.add(new Icon(5, size, 2, xoff, yoff, "./resources/textures/icons/polygon/marker.png", () -> addMarker()).addTooltip(LANG_PREFIX + id + ".add_marker"));
 		this.add(new Icon(6, size, 2, xoff, yoff, "./resources/textures/icons/polygon/group.png", () -> addGroup()).addTooltip(LANG_PREFIX + id + ".add_group"));
 		this.add(new Icon(7, size, 2, xoff, yoff, "./resources/textures/icons/polygon/voxel.png", () -> {}).addTooltip(LANG_PREFIX + id + ".add_voxel"));
 	}
@@ -38,6 +39,10 @@ public class QuickAdd extends EditorComponent {
 
 	public static void addCylinder(){
 		FMT.MODEL.add(null, new Cylinder(null));
+	}
+
+	public static void addMarker(){
+		FMT.MODEL.add(null, new Marker(null));
 	}
 
 }
