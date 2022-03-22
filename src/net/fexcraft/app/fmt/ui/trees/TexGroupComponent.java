963f6b7d80decf1d4cfd3e430a341a5acd7d3157
Ferdinand Calo <fercalo96@yahoo.de>
2022-03-22 21:18:46 +0100

    Update TexGroupComponent.java

--- src/net/fexcraft/app/fmt/ui/trees/TexGroupComponent.java
+++ src/net/fexcraft/app/fmt/ui/trees/TexGroupComponent.java
@@ -1,15 +1,21 @@
 package net.fexcraft.app.fmt.ui.trees;
 
 import static net.fexcraft.app.fmt.settings.Settings.ASK_TEXTURE_GROUP_REMOVAL;
+import static org.liquidengine.legui.event.MouseClickEvent.MouseClickAction.CLICK;
 
 import java.util.Collections;
 
+import org.liquidengine.legui.component.Button;
+import org.liquidengine.legui.component.Dialog;
 import org.liquidengine.legui.component.Label;
+import org.liquidengine.legui.event.MouseClickEvent;
+import org.liquidengine.legui.listener.MouseClickEventListener;
 import org.liquidengine.legui.style.color.ColorConstants;
 import org.liquidengine.legui.style.length.Length;
 import org.liquidengine.legui.style.length.LengthType;
 
 import net.fexcraft.app.fmt.FMT;
+import net.fexcraft.app.fmt.attributes.UpdateHandler;
 import net.fexcraft.app.fmt.attributes.UpdateType;
 import net.fexcraft.app.fmt.settings.Settings;
 import net.fexcraft.app.fmt.texture.TextureGroup;
@@ -17,6 +23,7 @@ import net.fexcraft.app.fmt.texture.TextureManager;
 import net.fexcraft.app.fmt.ui.Editor;
 import net.fexcraft.app.fmt.ui.EditorComponent;
 import net.fexcraft.app.fmt.ui.GenericDialog;
+import net.fexcraft.app.fmt.ui.fields.TextField;
 import net.fexcraft.app.fmt.utils.Logging;
 import net.fexcraft.app.fmt.utils.Translator;
 
@@ -37,10 +44,44 @@ public class TexGroupComponent extends EditorComponent {
 		label.getStyle().setPaddingLeft(new Length<Float>(5f, LengthType.PIXEL));
 		Settings.applyBorderless(label);
 		this.getStyle().getBackground().setColor(FMT.rgba(Settings.GROUP_INVISIBLE.value));
-		this.add(new OptionLabel(this, 0, "texture.group.rename", () -> {}));
+		this.add(new OptionLabel(this, 0, "texture.group.rename", () -> openRenameDialog()));
 		this.add(new OptionLabel(this, 1, "texture.group.resize", () -> {}));
 		this.add(new OptionLabel(this, 2, "texture.group.generate", () -> {}));
 		this.add(new OptionLabel(this, 3, "texture.group.select", () -> {}));
+	}
+
+	private void openRenameDialog(){
+		Logging.log("opening");
+		float width = 400;
+        Dialog dialog = new Dialog(Translator.translate("editor.tree.texture.rename_group.title"), width, 130);
+        Settings.applyComponentTheme(dialog.getContainer());
+        dialog.setResizable(true);
+    	Label label0 = new Label(Translator.translate("editor.tree.texture.rename_group.desc"), 10, 10, width - 20, 20);
+    	dialog.getContainer().add(label0);
+    	TextField field = new TextField(group.name, 10, 35, width - 20, 20);
+    	dialog.getContainer().add(field);
+    	//
+        Button button0 = new Button(Translator.translate("dialog.button.confirm"), 10, 80, 100, 20);
+        button0.getListenerMap().addListener(MouseClickEvent.class, (MouseClickEventListener) e -> {
+        	if(CLICK == e.getAction()){
+        		String newname = field.getTextState().getText();
+        		if(TextureManager.getGroup(newname) != null){
+        			GenericDialog.showOK(null, null, null, "editor.tree.texture.rename_group.duplicate");
+        			return;
+        		}
+    			UpdateHandler.update(UpdateType.TEXGROUP_RENAMED, group, group.name, group.name = newname);
+        		dialog.close();
+        	}
+        });
+        dialog.getContainer().add(button0);
+        //
+        Button button1 = new Button(Translator.translate("dialog.button.cancel"), 120, 80, 100, 20);
+        button1.getListenerMap().addListener(MouseClickEvent.class, (MouseClickEventListener) e -> {
+        	if(CLICK == e.getAction()) dialog.close();
+        });
+        dialog.getContainer().add(button1);
+        //
+        dialog.show(FMT.FRAME);
 	}
 
 	private int genFullheight(){
@@ -89,6 +130,9 @@ public class TexGroupComponent extends EditorComponent {
 			Settings.applyBorderless(this);
 			setPosition(4, index * 21 + 26);
 			setSize(Editor.CWIDTH - 8, PH);
+	        getListenerMap().addListener(MouseClickEvent.class, e -> {
+	        	if(CLICK == e.getAction()) run.run();
+	        });
 		}
 		
 	}
