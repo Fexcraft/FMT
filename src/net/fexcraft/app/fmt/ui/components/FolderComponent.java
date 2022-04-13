c7401696cfa9d3eef42525f41c5149011ef31d29
Ferdinand Calo <fercalo96@yahoo.de>
2022-04-13 23:39:30 +0200

    Update FolderComponent.java

--- src/net/fexcraft/app/fmt/ui/components/FolderComponent.java
+++ src/net/fexcraft/app/fmt/ui/components/FolderComponent.java
@@ -6,87 +6,172 @@ import java.util.ArrayList;
 import org.liquidengine.legui.component.Component;
 import org.liquidengine.legui.component.Label;
 import org.liquidengine.legui.component.ScrollablePanel;
+import org.liquidengine.legui.event.MouseClickEvent;
+import org.liquidengine.legui.event.MouseClickEvent.MouseClickAction;
+import org.liquidengine.legui.input.Mouse.MouseButton;
+import org.liquidengine.legui.style.Style.DisplayType;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.util.tinyfd.TinyFileDialogs;
 
 import net.fexcraft.app.fmt.settings.Settings;
 import net.fexcraft.app.fmt.ui.Editor;
 import net.fexcraft.app.fmt.ui.EditorComponent;
 import net.fexcraft.app.fmt.ui.Icon;
 import net.fexcraft.app.fmt.ui.fields.RunButton;
+import net.fexcraft.app.fmt.utils.Translator;
 
 public class FolderComponent extends EditorComponent {
 	
+	public static final int ROWHEIGHT = 30;
 	public ArrayList<DirComponent> rootfolders = new ArrayList<DirComponent>();
 	public ScrollablePanel panel;
-	public File folder = new File("./");
+	public File folder = new File("./workspace/");
 	public int height = 300, scrollableheight;
 
 	public FolderComponent(){
 		super("folder", 300, false, true);
-		this.add(new RunButton(LANG_PREFIX + "folder.select", F31, height - 28, F3S, HEIGHT, () -> genView()));
+		this.add(new RunButton(LANG_PREFIX + "folder.select", F31, height - 28, F3S, HEIGHT, () -> {
+	        try(MemoryStack stack = MemoryStack.stackPush()){
+	    		String string = TinyFileDialogs.tinyfd_selectFolderDialog(Translator.translate("editor.component.folder.select.dialog"), folder.toString());
+	    		if(string != null && string.trim().length() > 0){
+					folder = new File(string);
+					genView();
+	    		}
+	        }
+		}));
 		this.add(new RunButton(LANG_PREFIX + "folder.refresh", F32, height - 28, F3S, HEIGHT, () -> genView()));
 		panel = new ScrollablePanel(5, 30, Editor.CWIDTH - 10, fullheight - 65);
-		//panel.setHorizontalScrollBarVisible(false);
-		panel.getContainer().setSize(Editor.CWIDTH - 10, scrollableheight = fullheight - 65);
+		//panel.setVerticalScrollBarVisible(false);
+		panel.getContainer().setSize(500, scrollableheight = fullheight - 65);
 		Settings.applyBorderless(panel);
 		this.add(panel);
+		if(!folder.exists()) folder.mkdirs();
 		genView();
 	}
 
 	private void genView(){
-		panel.getContainer().getChildComponents().removeAll(rootfolders);
-		rootfolders.clear();
-		float height = fullheight - 65;
-		scrollableheight = 0;
-		showFolder(folder, null, 0);
-		panel.getContainer().setSize(Editor.CWIDTH - 10, scrollableheight < height ? height : scrollableheight);
+		new Thread("FolderViewGenerator"){
+			@Override
+			public void run(){
+				panel.getContainer().removeAll(rootfolders);
+				rootfolders.clear();
+				addFolder(folder, null, 0);
+				resize();
+			}
+		}.start();
 	}
 
-	private int showFolder(File folder, DirComponent root, int rrow){
+	private int addFolder(File folder, DirComponent root, int rrow){
 		if(!folder.isDirectory()) return rrow;
 		DirComponent com = null;
 		if(folder.listFiles().length == 0){
-			com = new DirComponent(FileType.EMPTY_FOLDER, folder, rrow);
+			com = new DirComponent(FileType.EMPTY_FOLDER, this, root, folder, rrow);
 		}
 		else{
-			com = new DirComponent(FileType.NORMAL_FOLDER, folder,  rrow);
+			com = new DirComponent(FileType.NORMAL_FOLDER, this, root, folder,  rrow);
 			int row = 1;
 			for(File file : folder.listFiles()){
-				if(file.isDirectory()) row += showFolder(file, com, row);
+				if(file.isDirectory()) row += addFolder(file, com, row);
 				else continue;
-				scrollableheight += 25;
 			}
 			for(File file : folder.listFiles()){
-				if(!file.isDirectory()) com.add(new DirComponent(FileType.FILE, file, row++));
+				if(!file.isDirectory()){
+					FileType type = FileType.fromFile(file);
+					if(type == FileType.FVTM_ADDONPACKFILE){
+						root.root.updateIcon(FileType.FVTMPACK, this);
+					}
+					com.addSub(new DirComponent(type, this, root, file, row++));
+				}
 				else continue;
-				scrollableheight += 25;
 			}
 		}
 		if(root == null){
-			com.getPosition().x -= DirComponent.xoff;
 			rootfolders.add(com);
 			panel.getContainer().add(com);
 		}
-		else root.add(com);
-		scrollableheight += 25;
+		else root.addSub(com);
 		return folder.list().length;
+	}
+
+	private void resize(){
+		float height = fullheight - 65;
+		scrollableheight = 0;
+		for(DirComponent com : rootfolders){
+			scrollableheight += com.resize(scrollableheight, true);
+		}
+		scrollableheight = 0;
+		for(DirComponent com : rootfolders){
+			scrollableheight += com.fullsize();
+		}
+		panel.getContainer().setSize(Editor.CWIDTH - 10, scrollableheight < height ? height : scrollableheight);
 	}
 	
 	public static class DirComponent extends Component {
-		
-		public static final int xoff = 10;
-		public boolean expanded;
-		private Label label = new Label(30, 0, LW, 25);;
-		private File file;
 
-		public DirComponent(FileType type, File file, int row){
-			this.file = file;
+		public static final int xoff = 10;
+		private ArrayList<DirComponent> subcom = new ArrayList<>();
+		private boolean expanded = false;
+		private DirComponent root;
+		private Label label;
+		private Icon icon;
+		//private File file;
+
+		public DirComponent(FileType type, FolderComponent folcom, DirComponent root, File file, int row){
+			//this.file = file;
+			this.root = root;
+			add(label = new Label(ROWHEIGHT + 2, 0, LW, ROWHEIGHT));
 			label.getTextState().setText(file.getName());
 			Settings.applyBorderless(label);
-			getPosition().set(xoff, row * 25);
-			getSize().set(300, file.isFile() ? 25 : file.list().length * 25 + 25);
 			Settings.applyBorderless(this);
-			this.add(label);
-			this.add(new Icon(0, 24, 0, 0, 0, "./resources/textures/icons/filetree/" + type.filename() + ".png", () -> {}));
+			label.getListenerMap().addListener(MouseClickEvent.class, listener -> {
+				if(listener.getAction() == MouseClickAction.CLICK && listener.getButton() == MouseButton.MOUSE_BUTTON_LEFT){
+					expanded = !expanded;
+					folcom.resize();
+				}
+			});
+			updateIcon(type, folcom);
+		}
+
+		public void addSub(DirComponent com){
+			subcom.add(com);
+			this.add(com);
+		}
+
+		public int resize(int offset, boolean noff){
+			int size = ROWHEIGHT;
+			getPosition().set(noff ? 0 : xoff, noff ? offset : size + offset);
+			if(expanded){
+				int off = 0;
+				for(DirComponent com : subcom){
+					int siz = com.resize(off, false);
+					size += siz;
+					off += siz;
+					com.getStyle().setDisplay(DisplayType.MANUAL);
+				}
+			}
+			else{
+				for(DirComponent com : subcom){
+					com.getStyle().setDisplay(DisplayType.NONE);
+				}
+			}
+			getSize().set(300, size);
+			return (int)getSize().y;
+		}
+
+		public int fullsize(){
+			int size = ROWHEIGHT;
+			for(DirComponent com : subcom){
+				size += com.fullsize();
+			}
+			return size;
+		}
+		
+		public void updateIcon(FileType type, FolderComponent folcom){
+			this.remove(icon);
+			this.add(icon = new Icon(0, 32, 0, 0, -1, "./resources/textures/icons/filetree/" + type.filename() + ".png", () -> {
+				expanded = !expanded;
+				folcom.resize();
+			}));
 		}
 		
 	}
@@ -94,16 +179,48 @@ public class FolderComponent extends EditorComponent {
 	public static enum FileType {
 		
 		NORMAL_FOLDER, EMPTY_FOLDER, FVTMPACK,
-		FILE;
+		FILE, FVTM_ADDONPACKFILE, FVTM_CONFIGFILE, JSON;
 
 		public String filename(){
 			switch(this){
-				case FILE: return "file";
 				case EMPTY_FOLDER: return "folder_empty";
 				case FVTMPACK: return "folder_fvtmpack";
-				case NORMAL_FOLDER:
-				default: return "folder";
+				case NORMAL_FOLDER: return "folder";
+				case FVTM_ADDONPACKFILE: return "file_fvtmaddonpack";
+				case FVTM_CONFIGFILE: return "file_fvtmcfg";
+				case JSON: return "file_json";
+				case FILE:
+				default: return "file";
 			}
+		}
+
+		static FileType fromFile(File file){
+			if(file.getName().equals("addonpack.fvtm")) return FVTM_ADDONPACKFILE;
+			String name = file.getName();
+			if(name.contains(".")){
+				String[] split = name.split("\\.");
+				if(split.length > 1){
+					String suffix = split[split.length - 1];
+					switch(suffix){
+						case "json":
+							return JSON;
+						case "block":
+						case "cloth":
+						case "container":
+						case "material":
+						case "vehicle":
+						case "part":
+						case "gauge":
+						case "wire":
+						case "consumable":
+						case "fuel":
+						case "recipe":
+							return FVTM_CONFIGFILE;
+						default: break;
+					}
+				}
+			}
+			return FILE;
 		}
 		
 	}
