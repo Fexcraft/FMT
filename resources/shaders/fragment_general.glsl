e61d83447aea502e1425eac1a41512f025fa49ab
Ferdinand Calo <fercalo96@yahoo.de>
2022-08-07 10:16:21 +0200

    Update fragment_general.glsl

--- resources/shaders/fragment_general.glsl
+++ resources/shaders/fragment_general.glsl
@@ -4,11 +4,17 @@ in vec4 color;
 in vec2 uv;
 in vec3 normal;
 in float light;
+in vec3 fragpos;
 
 uniform sampler2D tex_sampler;
 uniform vec4 line_color;
 uniform vec4 poly_color;
 uniform bool textured;
+uniform bool lighting;
+uniform vec3 lightcolor;
+uniform vec3 lightpos;
+uniform float ambient;
+uniform float diffuse;
 
 out vec4 color_out;
 
@@ -22,5 +28,14 @@ void main(){
 		if(light > -1){
 			color_out = vec4(color_out.rgb * light, color_out.a);
 		}
+	}
+	if(lighting){
+	    vec3 amb = ambient * lightcolor;
+	    //
+	    vec3 light_dir = normalize(lightpos - fragpos);
+	    float diff = max(dot(normalize(normal), light_dir), 0);
+	    vec3 dif = diff * diffuse * lightcolor;
+	    //
+	    color_out = vec4((amb + dif) * color_out.xyz, color_out.w);
 	}
 }
