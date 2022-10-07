82b6eec48e80a3ddc10612ea5e78b8a43f287f65
Ferdinand Calo <fercalo96@yahoo.de>
2022-10-07 23:34:39 +0200

    Update ModelOrientation.java

--- src/net/fexcraft/app/fmt/polygon/ModelOrientation.java
+++ src/net/fexcraft/app/fmt/polygon/ModelOrientation.java
@@ -2,15 +2,24 @@ package net.fexcraft.app.fmt.polygon;
 
 public enum ModelOrientation {
 	
-	CLASSIC;
+	CLASSIC_VEHICLE(10, "floor_vehicle"),
+	CLASSIC_BLOCK(0.125f, "floor_block");
+	
+	public final float floor_height;
+	public final String floor_texture;
+
+	ModelOrientation(float floor, String tex){
+		floor_height = floor;
+		floor_texture = tex;
+	}
 
 	public static ModelOrientation fromString(String string){
-		if(string == null) return CLASSIC;
+		if(string == null) return CLASSIC_VEHICLE;
 		string = string.toUpperCase();
 		for(ModelOrientation orient : values()){
 			if(orient.name().equals(string)) return orient;
 		}
-		return CLASSIC;
+		return CLASSIC_VEHICLE;
 	}
 
 }
