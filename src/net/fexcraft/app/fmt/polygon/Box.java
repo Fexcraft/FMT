1b529da433a6f98ce33a18ffab0bbb363d6ec537
Ferdinand Calo <fercalo96@yahoo.de>
2022-10-10 23:52:40 +0200

    Update Box.java

--- src/net/fexcraft/app/fmt/polygon/Box.java
+++ src/net/fexcraft/app/fmt/polygon/Box.java
@@ -7,6 +7,7 @@ import org.joml.Vector3f;
 import net.fexcraft.app.fmt.attributes.PolyVal.PolygonValue;
 import net.fexcraft.app.fmt.polygon.uv.BoxFace;
 import net.fexcraft.app.fmt.polygon.uv.Face;
+import net.fexcraft.app.fmt.polygon.uv.UVCoords;
 import net.fexcraft.app.json.JsonArray;
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.lib.common.math.RGB;
@@ -82,16 +83,16 @@ public class Box extends Polygon {
 	}
 
 	@Override
-	public float[] getFaceColor(int idx){
+	public RGB getFaceColor(int idx){
 		switch(idx){
-			case 0: return blu0.toFloatArray();
-			case 1: return blu1.toFloatArray();
-			case 2: return red1.toFloatArray();
-			case 3: return red0.toFloatArray();
-			case 4: return gre1.toFloatArray();
-			case 5: return gre0.toFloatArray();
+			case 0: return blu0;
+			case 1: return blu1;
+			case 2: return red1;
+			case 3: return red0;
+			case 4: return gre1;
+			case 5: return gre0;
 		}
-		return RGB.GREEN.toFloatArray();
+		return RGB.GREEN;
 	}
 	
 	public float getValue(PolygonValue polyval){
@@ -123,6 +124,146 @@ public class Box extends Polygon {
 	@Override
 	public Face[] getUVFaces(){
 		return BoxFace.values();
+	}
+
+	@Override
+	public float[][][] newUV(boolean include_offsets, boolean exclude_detached) {
+		float w = size.x, h = size.y, d = size.z;
+        if(w % 1 != 0) w = w < 1 ? 1 : (int)w + (w % 1 > 0.5f ? 1 : 0);
+        if(h % 1 != 0) h = h < 1 ? 1 : (int)h + (h % 1 > 0.5f ? 1 : 0);
+        if(d % 1 != 0) d = d < 1 ? 1 : (int)d + (d % 1 > 0.5f ? 1 : 0);
+		float[][][] vecs = new float[6][][];
+		//
+    	float yp = detached(2) && detached(3) ? 0 : d;
+    	float x0 = detached(1) ? 0 : d;
+    	float x1 = detached(2) ? 0 : w;
+    	float x2 = detached(4) ? 0 : w;
+    	float x3 = detached(0) ? 0 : d;
+		//
+		if(!sides[0] && !detached(0, exclude_detached)){
+			vecs[0] = new float[][]{
+				new float[]{ x0 + x2, yp },
+				new float[]{ x0 + x2 + d, yp + h }
+			};
+			if(include_offsets && !cuv.get(BoxFace.FRONT).automatic()){
+				vecs[0] = gets(BoxFace.FRONT, vecs[0]);
+			}
+		}
+		if(!sides[1] && !detached(1, exclude_detached)){
+			vecs[1] = new float[][]{
+				new float[]{ 0, yp },
+				new float[]{ d, yp + h }
+			};
+			if(include_offsets && !cuv.get(BoxFace.BACK).automatic()){
+				vecs[1] = gets(BoxFace.BACK, vecs[1]);
+			}
+		}
+		if(!sides[2] && !detached(2, exclude_detached)){
+			vecs[2] = new float[][]{
+				new float[]{ x0, 0 },
+				new float[]{ x0 + w, d }
+			};
+			if(include_offsets && !cuv.get(BoxFace.TOP).automatic()){
+				vecs[2] = gets(BoxFace.TOP, vecs[2]);
+			}
+		}
+		if(!sides[3] && !detached(3, exclude_detached)){
+			vecs[3] = new float[][]{
+				new float[]{ x0 + x1, 0 },
+				new float[]{ x0 + x1 + w, d }
+			};
+			if(include_offsets && !cuv.get(BoxFace.DOWN).automatic()){
+				vecs[3] = gets(BoxFace.DOWN, vecs[3]);
+			}
+		}
+		if(!sides[4] && !detached(4, exclude_detached)){
+			vecs[4] = new float[][]{
+				new float[]{ x0, yp },
+				new float[]{ x0 + w, yp + h }
+			};
+			if(include_offsets && !cuv.get(BoxFace.RIGHT).automatic()){
+				vecs[4] = gets(BoxFace.RIGHT, vecs[4]);
+			}
+		}
+		if(!sides[5] && !detached(5, exclude_detached)){
+			vecs[5] = new float[][]{
+				new float[]{ x0 + x2 + x3, yp },
+				new float[]{ x0 + x2 + x3 + w, yp + h }
+			};
+			if(include_offsets && !cuv.get(BoxFace.LEFT).automatic()){
+				vecs[5] = gets(BoxFace.LEFT, vecs[5]);
+			}
+		}
+		return vecs;
+	}
+
+	private boolean detached(int index, boolean exclude_detached){
+		return exclude_detached && cuv.get(BoxFace.values()[index]).detached();
+	}
+
+	private boolean detached(int i){
+		return sides[i] || cuv.get(BoxFace.values()[i]).detached();
+	}
+
+	private float[][] gets(Face face, float[][] def){
+		UVCoords coords = cuv.get(face);
+		float[] arr = coords.value();
+		float[][] res = null;
+		switch(coords.type()){
+			case DETACHED:
+			case OFFSET:{
+				def[1][0] -= def[0][0];
+				def[1][1] -= def[0][1];
+				def[0][0] = def[0][1] = 0;
+				res = new float[][]{
+					new float[]{ def[0][0] + arr[0], def[0][1] + arr[1] },
+					new float[]{ def[1][0] + arr[0], def[1][1] + arr[1] }
+				};
+				break;
+			}
+			case DETACHED_ENDS:
+			case OFFSET_ENDS:{
+				float minx, miny, maxx, maxy;
+				minx = maxx = arr[0];
+				miny = maxy = arr[1];
+				if(minx > arr[2]) minx = arr[2];
+				if(maxx < arr[2]) maxx = arr[2];
+				if(miny > arr[3]) miny = arr[3];
+				if(maxy < arr[3]) maxy = arr[3];
+				res = new float[][]{
+					new float[]{ minx, miny },
+					new float[]{ maxx, maxy }
+				};
+				break;
+			}
+			case DETACHED_FULL:
+			case OFFSET_FULL:{
+				float minx, miny, maxx, maxy;
+				minx = maxx = arr[0];
+				miny = maxy = arr[1];
+				for(int i = 0; i < 4; i++){
+					if(arr[i * 2] < minx) minx = arr[i * 2];
+					if(arr[i * 2 + 1] < miny) miny = arr[i * 2 + 1];
+					if(arr[i * 2] > maxx) maxx = arr[i * 2];
+					if(arr[i * 2 + 1] > maxy) maxy = arr[i * 2 + 1];
+				}
+				res = new float[][]{
+					new float[]{ minx, miny },
+					new float[]{ maxx, maxy }
+				};
+				break;
+			}
+			default: return null;
+		}
+		return res;
+	}
+
+	@Override
+	public boolean isActive(Face face){
+		for(Face fuv : BoxFace.values()){
+			if(fuv == face) return !sides[face.index()];
+		}
+		return false;
 	}
 
 }
