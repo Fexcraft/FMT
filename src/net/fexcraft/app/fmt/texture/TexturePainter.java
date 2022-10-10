ef7c4be396444c78e5fe0e01e39cbcf322d1d446
Ferdinand Calo <fercalo96@yahoo.de>
2022-10-10 23:52:52 +0200

    Create TexturePainter.java

---
+++ src/net/fexcraft/app/fmt/texture/TexturePainter.java
@@ -0,0 +1,16 @@
+package net.fexcraft.app.fmt.texture;
+
+import net.fexcraft.lib.common.math.RGB;
+
+/**
+ * 
+ * @author Ferdinand Calo' (FEX___96)
+ *
+ */
+public class TexturePainter {
+
+	public static byte[] getCurrentColor(){
+		return RGB.WHITE.toByteArray();
+	}
+
+}
