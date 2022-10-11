97abd8897b63071594bc73971326ffea67d82830
Ferdinand Calo <fercalo96@yahoo.de>
2022-10-11 11:47:23 +0200

    Update TexturePainter.java

--- src/net/fexcraft/app/fmt/texture/TexturePainter.java
+++ src/net/fexcraft/app/fmt/texture/TexturePainter.java
@@ -8,9 +8,25 @@ import net.fexcraft.lib.common.math.RGB;
  *
  */
 public class TexturePainter {
+	
+	public static final RGB PRIMARY = RGB.BLACK.copy();
+	public static final RGB SECONDARY = RGB.WHITE.copy();
+	
+	public static byte[] getCurrentColor(boolean primary){
+		return (primary ? PRIMARY : SECONDARY).toByteArray();
+	}
 
-	public static byte[] getCurrentColor(){
-		return RGB.WHITE.toByteArray();
+	public static byte[] getPrimaryColor(){
+		return PRIMARY.toByteArray();
+	}
+	
+	public static byte[] getSecondaryColor(){
+		return SECONDARY.toByteArray();
+	}
+
+	public static void updateColor(Integer value, boolean primary){
+		if(primary) PRIMARY.packed = value;
+		else SECONDARY.packed = value;
 	}
 
 }
