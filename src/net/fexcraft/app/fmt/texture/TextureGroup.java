84fa8a1a7c06932316a32aa8ede79362c3d7e66e
Ferdinand Calo <fercalo96@yahoo.de>
2022-10-17 14:56:47 +0200

    Update TextureGroup.java

--- src/net/fexcraft/app/fmt/texture/TextureGroup.java
+++ src/net/fexcraft/app/fmt/texture/TextureGroup.java
@@ -4,41 +4,32 @@ import static net.fexcraft.app.fmt.utils.Logging.log;
 
 import java.io.File;
 
-import net.fexcraft.app.json.JsonObject;
+import net.fexcraft.lib.common.math.RGB;
 
 public class TextureGroup {
 	
 	public Texture texture;
+	public Texture painter;
 	public String name;
 
 	public TextureGroup(String id){
 		name = id;
 	}
 	
-	public TextureGroup(String id, File file){
+	public TextureGroup(String id, File root){
 		this(id);
 		String texid = "group-" + id;
-		loadTexture(texid, file);
-		texture = TextureManager.get(texid, false);
-	}
-
-	public TextureGroup(String id, String existing){
-		this(id);
-		texture = TextureManager.get(existing, false);
-	}
-	
-	/** To be used while loading in FMTBs */
-	public TextureGroup(JsonObject<?> elm){
-		name = elm.string_value();
-		texture = null;//assigned/loaded later;
+		loadTexture(texid, root);
 	}
 
 	public void reAssignTexture(){
 		texture = TextureManager.get("group-" + name, false);
+		genPainterTex();
 	}
 
-	public void loadTexture(String texid, File file){
-		if(!file.getParentFile().exists()) file.getParentFile().mkdirs();
+	public void loadTexture(String texid, File root){
+		if(!root.exists()) root.mkdirs();
+		File file = new File(root, "group-" + name + ".png");
 		if(!file.exists()){
 			Texture texture = new Texture(texid, 256, 256);
 			log("Generated blank texgroup texture.");
@@ -49,6 +40,28 @@ public class TextureGroup {
 		if(!TextureManager.containsTexture(texid)){
 			TextureManager.loadFromFile(texid, file);
 		}
+		texture = TextureManager.get(texid, false);
+		genPainterTex();
+	}
+
+	public void genPainterTex(){
+		painter = TextureManager.get("pt-group-" + name, true);
+		if(painter == null){
+			painter = TextureManager.createTexture("pt-group-" + name, texture.getWidth(), texture.getHeight());
+			painter.setFile(new File("./temp/" + painter.name + ".png"));
+		}
+		else{
+			painter.resize(texture.getWidth(), texture.getHeight());
+		}
+		int lastint = 0;
+		for(int x = 0; x < painter.getWidth(); x++){
+			for(int y = 0; y < painter.getHeight(); y++){
+				painter.set(x, y, new RGB(lastint).toByteArray());
+				lastint++;
+			}
+		}
+		painter.save();
+		painter.rebind();
 	}
 
 }
