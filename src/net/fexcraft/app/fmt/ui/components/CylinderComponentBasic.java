4c9efb124e290708621e9f961f8f2fa97bbc3f6b
Ferdinand Calo <fercalo96@yahoo.de>
2022-10-29 21:19:56 +0200

    Update CylinderComponentBasic.java

--- src/net/fexcraft/app/fmt/ui/components/CylinderComponentBasic.java
+++ src/net/fexcraft/app/fmt/ui/components/CylinderComponentBasic.java
@@ -19,8 +19,8 @@ public class CylinderComponentBasic extends EditorComponent {
 		this.add(new NumberField(this, F20, row(1), F2S, HEIGHT).setup(0.5f, Integer.MAX_VALUE, true, new PolygonValue(PolyVal.RADIUS, ValAxe.N)));
 		this.add(new NumberField(this, F21, row(0), F2S, HEIGHT).setup(0.5f, Integer.MAX_VALUE, true, new PolygonValue(PolyVal.LENGTH, ValAxe.N)));
 		this.add(new Label(translate(LANG_PREFIX + id + ".seg_dir"), L5, row(1), LW, HEIGHT));
-		this.add(new NumberField(this, F20, row(1), F2S, HEIGHT).setup(3, 360, false, new PolygonValue(PolyVal.SEGMENTS, ValAxe.N)));
-		this.add(new NumberField(this, F21, row(0), F2S, HEIGHT).setup(0, 5, false, new PolygonValue(PolyVal.DIRECTION, ValAxe.N)));
+		this.add(new NumberField(this, F20, row(1), F2S, HEIGHT).setup(3, 360, false, new PolygonValue(PolyVal.SEGMENTS, ValAxe.N)).index());
+		this.add(new NumberField(this, F21, row(0), F2S, HEIGHT).setup(0, 5, false, new PolygonValue(PolyVal.DIRECTION, ValAxe.N)).index());
 		this.add(new Label(translate(LANG_PREFIX + id + ".base_top_scale"), L5, row(1), LW, HEIGHT));
 		this.add(new NumberField(this, F20, row(1), F2S, HEIGHT).setup(0, Integer.MAX_VALUE, true, new PolygonValue(PolyVal.BASE_SCALE, ValAxe.N)));
 		this.add(new NumberField(this, F21, row(0), F2S, HEIGHT).setup(0, Integer.MAX_VALUE, true, new PolygonValue(PolyVal.TOP_SCALE, ValAxe.N)));
