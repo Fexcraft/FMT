0cd984845845ad11719898e4eb2b943c2b128313
Ferdinand Calo <fercalo96@yahoo.de>
2023-01-28 21:59:14 +0100

    Create ExSetList.java

---
+++ src/net/fexcraft/app/fmt/export/ExSetList.java
@@ -0,0 +1,16 @@
+package net.fexcraft.app.fmt.export;
+
+import net.fexcraft.app.fmt.settings.Setting;
+
+import java.util.ArrayList;
+
+public class ExSetList extends ArrayList<Setting<?>> {
+
+    public Setting<?> g(String str){
+        for(Setting<?> setting : this){
+            if(setting.id.equals(str)) return setting;
+        }
+        return null;
+    }
+
+}
