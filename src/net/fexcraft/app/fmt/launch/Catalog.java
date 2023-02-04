4eb4ee88609386a1b821c320725c2a235d9b766b
Ferdinand Calo <fercalo96@yahoo.de>
2023-02-04 09:48:51 +0100

    Update Catalog.java

--- src/net/fexcraft/app/fmt/launch/Catalog.java
+++ src/net/fexcraft/app/fmt/launch/Catalog.java
@@ -28,20 +28,20 @@ public class Catalog {
 	private static String REMOTE_ROOT = "http://fexcraft.net/files/app_data/fmt/";
 	//private static boolean DIALOG;
 
-	public static void fetch(){
-		Launcher.log("Fetching catalog...");
+	public static void fetch(boolean log){
+		log(log, "Fetching catalog...");
 		JsonMap map = JsonHandler.parseURL("http://fexcraft.net/files/app_data/fmt/catalog.fmt");
 		if(!CATALOG_FILE.getParentFile().exists()) CATALOG_FILE.getParentFile().mkdirs();
 		if(map == null || map.empty()) return;
 		JsonHandler.print(CATALOG_FILE, map, PrintOption.FLAT);
 	}
 	
-	public static boolean load(){
+	public static boolean load(boolean log){
 		clear();
-		Launcher.log("Loading file catalog...");
+		log(log, "Loading file catalog...");
 		JsonMap map = JsonHandler.parse(CATALOG_FILE);
 		if(map == null || !map.has("files")){
-			Launcher.log("> Catalog is empty or missing.");
+			log(log, "> Catalog is empty or missing.");
 			return false;
 		}
 		REMOTE_ROOT = map.get("file_root", REMOTE_ROOT);
@@ -54,23 +54,23 @@ public class Catalog {
 		mismatches.clear();
 	}
 
-	public static boolean check(){
+	public static boolean check(boolean log){
 		mismatches.clear();
-		Launcher.log("Looking for missing or outdated files...");
+		log(log, "Looking for missing or outdated files...");
 		for(Resource res : files){
 			if(!res.file.exists() || (res.file.lastModified() < res.date && res.override) || res.remove) mismatches.add(res);
 		}
 		if(mismatches.size() > 0){
-			Launcher.log("Found " + mismatches.size() + " missing or outdated files.");
+			log(log, "Found " + mismatches.size() + " missing or outdated files.");
 			return true;
 		}
 		else{
-			Launcher.log("# No missing or outdated files found.");
-			Launcher.log("# FMT can be launched.");
+			log(log, "# No missing or outdated files found.");
+			log(log, "# FMT can be launched.");
 			return false;
 		}
 	}
-	
+
 	public static class Resource {
 		
 		public String id;
@@ -135,6 +135,15 @@ public class Catalog {
 		});
 		thread.setName("File Remover/Downloader");
 		thread.start();
+	}
+
+	private static void log(boolean log, String string){
+		if(log) Launcher.log(string);
+		else System.out.println(string);
+	}
+
+	public static int get(){
+		return mismatches.size();
 	}
 
 }
