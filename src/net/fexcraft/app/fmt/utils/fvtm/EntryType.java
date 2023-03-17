d010262707468408ac2596ee974e8d832de21d84
Ferdinand Calo <fercalo96@yahoo.de>
2023-03-17 23:27:01 +0100

    Update EntryType.java

--- src/net/fexcraft/app/fmt/utils/fvtm/EntryType.java
+++ src/net/fexcraft/app/fmt/utils/fvtm/EntryType.java
@@ -22,4 +22,24 @@ public enum EntryType {
 
     ;
 
+    public boolean subs(){
+        return this == ARRAY || this == OBJECT || this == ARRAY_OR_TEXT || this == OBJECT_KEY_VAL;
+    }
+
+    public boolean bool(){
+        return this == BOOLEAN;
+    }
+
+    public boolean numer(){
+        return this == INTEGER || this == DECIMAL;
+    }
+
+    public boolean color(){
+        return this == COLOR;
+    }
+
+    public boolean trio(){
+        return this == POSITION || this == ROTATION;
+    }
+
 }
