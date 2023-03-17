78e212061a779e171331cbaae9c37438d59525fc
Ferdinand Calo <fercalo96@yahoo.de>
2023-03-17 22:48:08 +0100

    Create Reference.java

---
+++ src/net/fexcraft/app/fmt/utils/fvtm/Reference.java
@@ -0,0 +1,11 @@
+package net.fexcraft.app.fmt.utils.fvtm;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public interface Reference {
+
+    public List<ConfigEntry> getEntries();
+
+}
