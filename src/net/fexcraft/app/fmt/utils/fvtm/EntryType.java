4bdc53b7882ffa042e6e958c7a9534894c088d6c
Ferdinand Calo <fercalo96@yahoo.de>
2023-03-17 22:48:04 +0100

    Create EntryType.java

---
+++ src/net/fexcraft/app/fmt/utils/fvtm/EntryType.java
@@ -0,0 +1,25 @@
+package net.fexcraft.app.fmt.utils.fvtm;
+
+public enum EntryType {
+
+    TEXT,
+    PACKID,
+    TEXLOC,
+    MODELLOC,
+    INTEGER,
+    DECIMAL,
+    BOOLEAN,
+    COLOR,
+
+    ARRAY,
+    OBJECT,
+    ARRAY_OR_TEXT,
+    OBJECT_KEY_VAL,
+    ENUM,
+
+    POSITION,
+    ROTATION,
+
+    ;
+
+}
