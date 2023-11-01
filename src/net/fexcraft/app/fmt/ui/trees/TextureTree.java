2618b47a9bda2466cd8340339e99e54e4b4342b8
Ferdinand Calo <fercalo96@yahoo.de>
2023-11-01 18:28:26 +0100

    Update TextureTree.java

--- src/net/fexcraft/app/fmt/ui/trees/TextureTree.java
+++ src/net/fexcraft/app/fmt/ui/trees/TextureTree.java
@@ -17,8 +17,8 @@ public class TextureTree extends Editor {
 	
 	private static UpdateCompound updcom = new UpdateCompound();
 
-	public TextureTree(String name, boolean alignment){
-		super(TREES.get(1), name == null ? "Texture Tree" : name, true, alignment);
+	public TextureTree(){
+		super(TREES.get(1), "Texture Tree", true);
 		this.addTreeIcons(2);
 		updcom.add(TexGroupAdded.class, event -> addTexGroup(event.group()));
 		updcom.add(TexGroupRemoved.class, event -> remTexGroup(event.group()));
@@ -27,10 +27,6 @@ public class TextureTree extends Editor {
 		this.add(new RunButton("editor.tree.texture.auto_pos", 155, 30, 135, 24, () -> AutoUVPositioner.runAutoPos(), false));
 		this.add(new RunButton("editor.tree.texture.reset_pos", 10, 60, 135, 24, () -> AutoUVPositioner.runReset(false), false));
 		this.add(new RunButton("editor.tree.texture.reset_type", 155, 60, 135, 24, () -> AutoUVPositioner.runReset(true), false));
-	}
-
-	public TextureTree(String key, JsonMap obj){
-		this(obj.get("name", "Texture Tree"), obj.get("alignment", true));
 	}
 	
 	@Override
