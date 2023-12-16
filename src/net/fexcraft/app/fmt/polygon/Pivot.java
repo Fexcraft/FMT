e68dc0698f0158f5763ad32355a1d90aa673000a
Ferdinand Calo <fexcalo@gmail.com>
2023-12-16 21:37:55 +0100

    Create Pivot.java

---
+++ src/net/fexcraft/app/fmt/polygon/Pivot.java
@@ -0,0 +1,13 @@
+package net.fexcraft.app.fmt.polygon;
+
+import org.joml.Vector3f;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class Pivot {
+
+    public Vector3f pos;
+    public Vector3f rot;
+
+}
