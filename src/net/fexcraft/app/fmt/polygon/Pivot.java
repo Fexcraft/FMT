d91547b5cf11f87b8e3bfbf1e0a481b64b901e6d
Ferdinand Calo <fexcalo@gmail.com>
2023-12-19 18:50:17 +0100

    Update Pivot.java

--- src/net/fexcraft/app/fmt/polygon/Pivot.java
+++ src/net/fexcraft/app/fmt/polygon/Pivot.java
@@ -1,6 +1,7 @@
 package net.fexcraft.app.fmt.polygon;
 
 import net.fexcraft.app.fmt.utils.Logging;
+import net.fexcraft.app.json.JsonArray;
 import net.fexcraft.app.json.JsonMap;
 import org.joml.Vector3f;
 
@@ -15,8 +16,8 @@ public class Pivot {
     public ArrayList<Group> groups = new ArrayList<>();
     public boolean minimized;
     public boolean visible = true;
-    public Vector3f pos;
-    public Vector3f rot;
+    public Vector3f pos = new Vector3f();
+    public Vector3f rot = new Vector3f();
     public String id;
 
     public Pivot(String id, boolean root){
@@ -31,11 +32,29 @@ public class Pivot {
     public Pivot(String key, JsonMap map){
         id = key;
         root = map.getBoolean("root", false);
+        if(map.has("pos")){
+            JsonArray apos = map.getArray("pos");
+            pos.x = apos.get(0).float_value();
+            pos.y = apos.get(1).float_value();
+            pos.z = apos.get(2).float_value();
+        }
+        if(map.has("rot")){
+            JsonArray arot = map.getArray("rot");
+            rot.x = arot.get(0).float_value();
+            rot.y = arot.get(1).float_value();
+            rot.z = arot.get(2).float_value();
+        }
+        visible = map.getBoolean("visible", true);
+        minimized = map.getBoolean("minimized", false);
     }
 
     public JsonMap save(){
         JsonMap map = new JsonMap();
         map.add("root", root);
+        map.add("pos", new JsonArray(pos.x, pos.y, pos.z));
+        map.add("rot", new JsonArray(rot.x, rot.y, rot.z));
+        map.add("visible", visible);
+        map.add("minimized", minimized);
         return map;
     }
 
