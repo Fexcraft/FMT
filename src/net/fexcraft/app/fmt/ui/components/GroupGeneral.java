52f38a1055cb3f296ae2682af46686326d5e900a
Ferdinand Calo <fexcalo@gmail.com>
2023-12-19 19:13:14 +0100

    Update GroupGeneral.java

--- src/net/fexcraft/app/fmt/ui/components/GroupGeneral.java
+++ src/net/fexcraft/app/fmt/ui/components/GroupGeneral.java
@@ -5,10 +5,9 @@ import static net.fexcraft.app.fmt.utils.Translator.translate;
 
 import java.util.ArrayList;
 
-import net.fexcraft.app.fmt.update.UpdateEvent.GroupSelected;
-import net.fexcraft.app.fmt.update.UpdateEvent.TexGroupAdded;
-import net.fexcraft.app.fmt.update.UpdateEvent.TexGroupRemoved;
-import net.fexcraft.app.fmt.update.UpdateEvent.TexGroupRenamed;
+import net.fexcraft.app.fmt.polygon.Pivot;
+import net.fexcraft.app.fmt.update.UpdateEvent;
+import net.fexcraft.app.fmt.update.UpdateEvent.*;
 import com.spinyowl.legui.component.Label;
 import com.spinyowl.legui.component.SelectBox;
 
@@ -18,19 +17,44 @@ import net.fexcraft.app.fmt.texture.TextureGroup;
 import net.fexcraft.app.fmt.texture.TextureManager;
 import net.fexcraft.app.fmt.ui.EditorComponent;
 import net.fexcraft.app.fmt.ui.fields.TextField;
+import net.fexcraft.app.fmt.update.UpdateHandler;
+import net.fexcraft.app.fmt.utils.Logging;
 
 public class GroupGeneral extends EditorComponent {
 	
 	private SelectBox<String> texgroups = new SelectBox<>();
 	private SelectBox<Integer> texsx = new SelectBox<>(), texsy = new SelectBox<>();
+	private SelectBox<String> pivots = new SelectBox<>();
 	private static final String NOGROUPSEL = "< no group directly selected >";
 	protected static final String genid = "group.general";
 	private TextField name;
 	
 	public GroupGeneral(){
-		super(genid, 180, false, true);
+		super(genid, 240, false, true);
 		this.add(new Label(translate(LANG_PREFIX + genid + ".name/id"), L5, row(1), LW, HEIGHT));
 		this.add(name = new TextField(NOGROUPSEL, L5, row(1), LW, HEIGHT, false).accept(con -> rename(con)));
+		this.add(new Label(translate(LANG_PREFIX + genid + ".pivot"), L5, row(1), LW, HEIGHT));
+		pivots.setPosition(L5, row(1));
+		pivots.setSize(LW, HEIGHT);
+		updcom.add(ModelLoad.class, event -> refreshPivotEntries());
+		updcom.add(PivotAdded.class, event -> refreshPivotEntries());
+		updcom.add(PivotRenamed.class, event -> refreshPivotEntries());
+		updcom.add(PivotRemoved.class, event -> refreshPivotEntries());
+		pivots.addSelectBoxChangeSelectionEventListener(listener -> {
+			ArrayList<Group> groups = FMT.MODEL.selected_groups();
+			if(groups.isEmpty()) return;
+			Pivot pivot = FMT.MODEL.getP(listener.getNewValue());
+			for(Group group : groups){
+				Pivot old = FMT.MODEL.getP(group.pivot);
+				if(old != null) old.groups.remove(group);
+				group.pivot = pivot.id;
+				pivot.groups.add(group);
+				UpdateHandler.update(new PivotChanged(group, pivot));
+			}
+		});
+		pivots.setVisibleCount(6);
+		refreshPivotEntries();
+		add(pivots);
 		this.add(new Label(translate(LANG_PREFIX + genid + ".tex_size"), L5, row(1), LW, HEIGHT));
 		texsx.setSize(F2S, HEIGHT);
 		texsx.setPosition(F20, row(1));
@@ -109,6 +133,16 @@ public class GroupGeneral extends EditorComponent {
 		Group group = FMT.MODEL.first_selected_group();
 		if(group == null || group.texgroup == null) texgroups.setSelected(0, true);
 		else texgroups.setSelected(group.texgroup.name, true);
+	}
+
+	private void refreshPivotEntries(){
+		while(pivots.getElements().size() > 0) pivots.removeElement(0);
+		for(Pivot pivot : FMT.MODEL.pivots()){
+			pivots.addElement(pivot.id);
+		}
+		Group group = FMT.MODEL.first_selected_group();
+		if(group == null) pivots.setSelected(0, true);
+		else pivots.setSelected(group.pivot, true);
 	}
 
 	private void rename(String string){
