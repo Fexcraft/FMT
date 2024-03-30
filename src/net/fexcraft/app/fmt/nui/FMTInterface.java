af0a6d8becee9b0224bc28f5a405ea41da3b67c2
Ferdinand Calo <fexcalo@gmail.com>
2024-03-30 14:45:34 +0100

    Update FMTInterface.java

--- src/net/fexcraft/app/fmt/nui/FMTInterface.java
+++ src/net/fexcraft/app/fmt/nui/FMTInterface.java
@@ -1,5 +1,8 @@
 package net.fexcraft.app.fmt.nui;
 
+import net.fexcraft.app.fmt.FMT;
+import net.fexcraft.app.fmt.polygon.PolyRenderer;
+import net.fexcraft.app.fmt.settings.Settings;
 import net.fexcraft.lib.common.math.RGB;
 
 import java.util.ArrayList;
@@ -9,11 +12,19 @@ import java.util.ArrayList;
  */
 public class FMTInterface extends Element {
 
+	public static Element toolbar;
+
 	public FMTInterface(){
 		super();
-		elements = new ArrayList<>();
-		elements.add(new Element().pos(0, 0).size(100, 200).color(RGB.BLUE).linecolor(RGB.WHITE).recompile());
-		elements.add(new Element().pos(200, 0).size(500, 100).color(RGB.GREEN).linecolor(RGB.BLACK).recompile());
+		add(toolbar = new Element().pos(0, 0).size(FMT.WIDTH, 35).color(Settings.THEME_BACKGROUND.value));
+		toolbar.add(new Element().pos(2, 2).size(32, 32).texture("icons/toolbar/info"));
+		toolbar.add(new Element().pos(0, 40).size(100, 200).color(RGB.BLUE).linecolor(new RGB(256, 256, 0)).rounded(true));
+		toolbar.add(new Element().pos(200, 40).size(500, 100).color(RGB.GREEN).linecolor(RGB.BLACK).rounded(true));
+	}
+
+	@Override
+	public void render(){
+		for(Element elm : elements) elm.render();
 	}
 
 	@Override
