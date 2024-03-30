05a95f6bb8b2abb223adfbcf8a66da9f74a8412c
Ferdinand Calo <fexcalo@gmail.com>
2024-03-30 22:21:24 +0100

    Update Menu.java

--- src/net/fexcraft/app/fmt/nui/Menu.java
+++ src/net/fexcraft/app/fmt/nui/Menu.java
@@ -5,35 +5,45 @@ package net.fexcraft.app.fmt.nui;
  */
 public class Menu extends Element {
 
+	private boolean open;
 	private int ran;
+
+	@Override
+	public Element root(Element elm){
+		super.root(elm);
+		z++;
+		return this;
+	}
 
 	@Override
 	public void add(Element elm){
 		elm.visible = false;
 		elm.size(w - 2, h);
 		super.add(elm);
-		elm.pos(0, 30);
+		elm.x(x());
+		elm.z -= 2;
 	}
 
 	@Override
 	public void update(){
-		if(hovered){
-			size(200, elements.size() * 30 + 30);
+		if(hovered) open = true;
+		if(open){
 			for(int i = 0; i < elements.size(); i++){
-				if(ran > 30) continue;
+				if(ran >= 30) continue;
 				elements.get(i).visible = true;
-				elements.get(i).ya(i);
+				elements.get(i).ya(i + 1);
 			}
-			ran++;
+			if(ran < 30) ran++;
 		}
 		else{
-			size(200, 30);
 			for(int i = 0; i < elements.size(); i++){
-				elements.get(i).visible = elements.get(i).y() > 30;
-				if(elements.get(i).y() > 30) elements.get(i).ya(-2);
+				if(ran <= 0) continue;
+				elements.get(i).visible = elements.get(i).y() > 0;
+				elements.get(i).ya(-i - 1);
 			}
 			if(ran > 0) ran--;
 		}
+		if(ran >= 30 && !hovered) open = false;
 		super.update();
 	}
 
