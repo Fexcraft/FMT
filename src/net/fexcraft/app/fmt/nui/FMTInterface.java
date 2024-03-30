edb64f7d8898ded79cd3563f03272666dfe995cd
Ferdinand Calo <fexcalo@gmail.com>
2024-03-30 20:58:45 +0100

    Update FMTInterface.java

--- src/net/fexcraft/app/fmt/nui/FMTInterface.java
+++ src/net/fexcraft/app/fmt/nui/FMTInterface.java
@@ -3,7 +3,11 @@ package net.fexcraft.app.fmt.nui;
 import net.fexcraft.app.fmt.FMT;
 import net.fexcraft.app.fmt.polygon.PolyRenderer;
 import net.fexcraft.app.fmt.settings.Settings;
+import net.fexcraft.app.fmt.ui.Icon;
+import net.fexcraft.app.fmt.ui.ProfileDialog;
+import net.fexcraft.app.fmt.ui.SettingsDialog;
 import net.fexcraft.app.fmt.utils.Picker;
+import net.fexcraft.app.fmt.utils.SaveHandler;
 import net.fexcraft.lib.common.math.RGB;
 
 import java.util.ArrayList;
@@ -18,7 +22,30 @@ public class FMTInterface extends Element {
 	public FMTInterface(){
 		super();
 		add(toolbar = new Element().pos(0, 0).size(FMT.WIDTH, 35).color(Settings.THEME_BACKGROUND.value));
-		toolbar.add(new Element().pos(2, 2).size(32, 32).texture("icons/toolbar/info").hoverable(true));
+		toolbar.add(new Element().pos(2, 2).size(32, 32)
+			.texture("icons/toolbar/info").hoverable(true)
+			.onclick(() -> FMT.openLink("https://fexcraft.net/wiki/app/fmt"))
+			.tooltip("toolbar.icon.info"));
+		toolbar.add(new Element().pos(36, 2).size(32, 32)
+			.texture("icons/toolbar/settings").hoverable(true)
+			.onclick(() -> SettingsDialog.open())
+			.tooltip("toolbar.icon.settings"));
+		toolbar.add(new Element().pos(70, 2).size(32, 32)
+			.texture("icons/toolbar/profile").hoverable(true)
+			.onclick(() -> ProfileDialog.open())
+			.tooltip("toolbar.icon.profile"));
+		toolbar.add(new Element().pos(104, 2).size(32, 32)
+			.texture("icons/toolbar/save").hoverable(true)
+			.onclick(() -> SaveHandler.save(FMT.MODEL, null, null, false, false))
+			.tooltip("toolbar.icon.save"));
+		toolbar.add(new Element().pos(138, 2).size(32, 32)
+			.texture("icons/toolbar/open").hoverable(true)
+			.onclick(() -> SaveHandler.openDialog(null))
+			.tooltip("toolbar.icon.open"));
+		toolbar.add(new Element().pos(174, 2).size(32, 32)
+			.texture("icons/toolbar/new").hoverable(true)
+			.onclick(() -> SaveHandler.newDialog())
+			.tooltip("toolbar.icon.new"));
 		toolbar.add(new Element().pos(0, 40).size(100, 200).color(RGB.BLUE).linecolor(new RGB(256, 256, 0)).rounded(true));
 		toolbar.add(new Element().pos(200, 40).size(500, 100).color(RGB.GREEN).linecolor(RGB.BLACK).rounded(true));
 	}
