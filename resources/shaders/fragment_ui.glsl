56764927e919e68e9190089d95f08d7072f671a0
Ferdinand Calo <fexcalo@gmail.com>
2024-03-31 16:35:39 +0200

    Update fragment_ui.glsl

--- resources/shaders/fragment_ui.glsl
+++ resources/shaders/fragment_ui.glsl
@@ -8,6 +8,7 @@ uniform sampler2D tex_sampler;
 uniform vec4 line_color;
 uniform vec4 poly_color;
 uniform bool textured;
+uniform bool tinted;
 
 out vec4 color_out;
 
@@ -17,6 +18,7 @@ void main(){
 	else if(color.a > 0 && !textured) color_out = color;
 	else{
 		color_out = texture(tex_sampler, uv).rgba;
+		if(color.a > 0 && tinted) color_out *= color;
 		if(color_out.a < 0.1) discard;
 	}
 }
