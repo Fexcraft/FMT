c39b914faa8adb6a41c2d60c8b23e1c7deea06c6
Ferdinand Calo <fexcalo@gmail.com>
2024-03-31 16:36:00 +0200

    Create Text.java

---
+++ src/net/fexcraft/app/fmt/nui/Text.java
@@ -0,0 +1,59 @@
+package net.fexcraft.app.fmt.nui;
+
+import net.fexcraft.app.fmt.polygon.GLObject;
+import net.fexcraft.app.fmt.polygon.PolyRenderer;
+import net.fexcraft.app.fmt.texture.TextureManager;
+import net.fexcraft.lib.common.math.RGB;
+import net.fexcraft.lib.frl.Polyhedron;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class Text {
+
+	public final Element root;
+	public Polyhedron<GLObject> hedron;
+	public FontRenderer.FontType font = FontRenderer.FontType.PLAIN;
+	public RGB color = RGB.BLACK;
+	public boolean centered;
+	private String text;
+	public int w;
+	public int h;
+
+	public Text(Element elm){
+		hedron = new Polyhedron<>();
+		hedron.setGlObj(new GLObject());
+		root = elm;
+	}
+
+	public void recompile(){
+		hedron.recompile = true;
+		hedron.clear();
+		if(hedron.glObj.pickercolor == null) hedron.glObj.pickercolor = root.hedron.glObj.pickercolor;
+		hedron.glObj.textured = true;
+		FontRenderer.compile(this, text, font, color);
+		w = FontRenderer.getWidth(text, font);
+		h = FontRenderer.getHeight(text, font);
+		root.postext();
+	}
+
+	public void render(){
+		TextureManager.bind("font/" + font);
+		PolyRenderer.mode(PolyRenderer.DrawMode.UI_TEXT);
+		hedron.render();
+		PolyRenderer.mode(PolyRenderer.DrawMode.UI);
+	}
+
+	public Text text(String ntext){
+		text = ntext;
+		recompile();
+		return this;
+	}
+
+	public Text centered(boolean bool){
+		centered = bool;
+		root.postext();
+		return this;
+	}
+
+}
