461fbdf03621f1fc010ec6c68cac07d82335767a
Ferdinand Calo <fexcalo@gmail.com>
2024-03-31 21:13:46 +0200

    Update Menu.java

--- src/net/fexcraft/app/fmt/nui/Menu.java
+++ src/net/fexcraft/app/fmt/nui/Menu.java
@@ -1,10 +1,14 @@
 package net.fexcraft.app.fmt.nui;
 
+import java.util.function.Consumer;
+
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class Menu extends Element {
 
+	private Consumer<Menu> onhover;
+	private boolean vertical;
 	private boolean open;
 	private int ran;
 
@@ -16,7 +20,8 @@ public class Menu extends Element {
 	@Override
 	public Element root(Element elm){
 		super.root(elm);
-		z++;
+		vertical = !(elm instanceof Menu);
+		if(vertical) z++;
 		return this;
 	}
 
@@ -26,7 +31,7 @@ public class Menu extends Element {
 		elm.hoverable = true;
 		elm.size(w - 2, h);
 		super.add(elm);
-		elm.pos(1, 0);
+		elm.pos(1 + (vertical ? 0 : w), vertical ? 0 : (elements.size() - 1) * 30);
 		elm.z -= 2;
 		elm.recompile();
 	}
@@ -35,23 +40,44 @@ public class Menu extends Element {
 	public void update(){
 		if(hoveredx()) open = true;
 		if(open){
-			for(int i = 0; i < elements.size(); i++){
-				if(ran >= 30) continue;
-				elements.get(i).visible = true;
-				elements.get(i).ya(i + 1);
+			if(vertical){
+				for(int i = 0; i < elements.size(); i++){
+					if(ran >= 10) continue;
+					elements.get(i).visible = true;
+					elements.get(i).ya((i + 1) * 3);
+				}
+				if(ran < 10) ran++;
 			}
-			if(ran < 30) ran++;
+			else{
+				for(Element elm : elements) elm.visible = true;
+			}
 		}
 		else{
-			for(int i = 0; i < elements.size(); i++){
-				if(ran <= 0) continue;
-				elements.get(i).visible = elements.get(i).y() > 0;
-				elements.get(i).ya(-i - 1);
+			if(vertical){
+				for(int i = 0; i < elements.size(); i++){
+					if(ran <= 0) continue;
+					elements.get(i).visible = elements.get(i).y() > 0;
+					elements.get(i).ya((-i - 1) * 3);
+				}
+				if(ran > 0) ran--;
 			}
-			if(ran > 0) ran--;
+			else{
+				for(Element elm : elements) elm.visible = false;
+			}
 		}
 		if(!hoveredx()) open = false;
 		super.update();
+	}
+
+	@Override
+	public void hovered(boolean bool){
+		super.hovered(bool);
+		if(onhover != null) onhover.accept(this);
+	}
+
+	public Menu onhover(Consumer<Menu> cons){
+		onhover = cons;
+		return this;
 	}
 
 }
