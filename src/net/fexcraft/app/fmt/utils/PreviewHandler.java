43efc573fa117bac1351e6517fac367a54b7cbfd
Ferdinand Calo <fexcalo@gmail.com>
2024-05-17 23:30:16 +0200

    Update PreviewHandler.java

--- src/net/fexcraft/app/fmt/utils/PreviewHandler.java
+++ src/net/fexcraft/app/fmt/utils/PreviewHandler.java
@@ -2,8 +2,6 @@ package net.fexcraft.app.fmt.utils;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 
 import net.fexcraft.app.fmt.polygon.Model;
@@ -15,6 +13,7 @@ import net.fexcraft.app.json.JsonMap;
 public class PreviewHandler {
 
 	public static final ArrayList<Model> previews = new ArrayList<>();
+	public static Model SELECTED = null;
 
 	public static Model loadFrame(File file){
 		// TODO Auto-generated method stub
@@ -22,30 +21,46 @@ public class PreviewHandler {
 	}
 
 	public static Model loadFMTB(File file){
-		Model model = new Model(null, "");
+		Model model = new Model(null, "fmtb/" + file.getName().substring(0, file.getName().lastIndexOf(".")));
+		model.helper = true;
 		SaveHandler.open(model, file, true);
-		previews.add(model);
-		UpdateHandler.update(new UpdateEvent.HelperLoad(model));
+		add(model);
 		return model;
 	}
 
 	public static Model load(File file, Importer porter, JsonMap map){
 		Model model = new Model(null, map.getString("name", file.getName()));
 		porter._import(model, file);
-		previews.add(model);
-		UpdateHandler.update(new UpdateEvent.HelperLoad(model));
+		add(model);
 		return model;
 	}
 
 	public static void clear(){
 		previews.removeIf(preview -> {
-			UpdateHandler.update(new UpdateEvent.HelperUnload(preview));
+			UpdateHandler.update(new UpdateEvent.HelperRemoved(preview));
 			return true;
 		});
 	}
 
 	public static List<Model> getLoaded(){
 		return previews;
+	}
+
+	private static void add(Model model){
+		model.helper = true;
+		previews.add(model);
+		model.allgroups().forEach(group -> group.selected = false);
+		UpdateHandler.update(new UpdateEvent.HelperAdded(model));
+	}
+
+	public static void remove(Model model){
+		if(previews.remove(model)){
+			UpdateHandler.update(new UpdateEvent.HelperRemoved(model));
+		}
+	}
+
+	public static void select(Model model){
+		SELECTED = model;
 	}
 
 }
