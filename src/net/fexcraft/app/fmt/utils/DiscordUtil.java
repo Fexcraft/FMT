0132e356007f70fc6ccab2cd0ed3943a139d6721
Ferdinand Calo <fexcalo@gmail.com>
2024-05-22 19:55:41 +0200

    Update DiscordUtil.java

--- src/net/fexcraft/app/fmt/utils/DiscordUtil.java
+++ src/net/fexcraft/app/fmt/utils/DiscordUtil.java
@@ -1,91 +1,75 @@
 package net.fexcraft.app.fmt.utils;
 
-import static net.fexcraft.app.fmt.utils.Logging.log;
-
-import net.arikia.dev.drpc.DiscordRPC;
-import net.arikia.dev.drpc.DiscordRichPresence;
-import net.arikia.dev.drpc.DiscordUser;
-import net.arikia.dev.drpc.callbacks.DisconnectedCallback;
-import net.arikia.dev.drpc.callbacks.ErroredCallback;
-import net.arikia.dev.drpc.callbacks.JoinGameCallback;
-import net.arikia.dev.drpc.callbacks.JoinRequestCallback;
-import net.arikia.dev.drpc.callbacks.ReadyCallback;
-import net.arikia.dev.drpc.callbacks.SpectateGameCallback;
+import de.jcm.discordgamesdk.Core;
+import de.jcm.discordgamesdk.CreateParams;
+import de.jcm.discordgamesdk.LogLevel;
+import de.jcm.discordgamesdk.activity.Activity;
+import de.jcm.discordgamesdk.activity.ActivityButton;
+import de.jcm.discordgamesdk.activity.ActivityButtonsMode;
+import de.jcm.discordgamesdk.activity.ActivityType;
 import net.fexcraft.app.fmt.FMT;
 import net.fexcraft.app.fmt.settings.Settings;
 import net.fexcraft.lib.common.math.Time;
 
-//** Using "Discord-RPC" from https://github.com/Vatuu/discord-rpc ! **//
+import java.time.Instant;
+
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+
+/** Using https://github.com/JnCrMx/discord-game-sdk4j */
 public class DiscordUtil {
-	
-	public static Thread DISCORD_THREAD;
-	
-	public static class SpectateGameEventHandler implements SpectateGameCallback {
 
-		@Override
-		public void apply(String arg0){
-			// TODO Auto-generated method stub
-
-		}
-
-	}
-
-	public static class JoinRequestEventHandler implements JoinRequestCallback {
-
-		@Override
-		public void apply(DiscordUser arg0){
-			// TODO Auto-generated method stub
-
-		}
-
-	}
-
-	public static class JoinGameEventHandler implements JoinGameCallback {
-
-		@Override
-		public void apply(String arg0){
-			// TODO Auto-generated method stub
-
-		}
-
-	}
-
+	public static Core CORE;
 	public static long starttime;
-	
-	public static class ErroredEventHandler implements ErroredCallback {
 
-		@Override
-		public void apply(int arg0, String arg1){
-			// TODO Auto-generated method stub
-
-		}
-
-	}
-
-	public static class DisconectedEventHandler implements DisconnectedCallback {
-
-		@Override
-		public void apply(int arg0, String arg1){
-			// TODO Auto-generated method stub
-
-		}
-
-	}
-
-	public static class ReadyEventHandler implements ReadyCallback {
-
-		@Override
-		public void apply(DiscordUser user){
-			log("Received Discord ID: " + user.username + "#" + user.discriminator + "");
-		}
-
-	}
-	
 	public static void update(boolean updatetime){
 		if(updatetime) starttime = Time.getDate();
-		DiscordRichPresence veryrichnot = new DiscordRichPresence.Builder("Modelling (on " + FMT.VERSION + ")").setBigImage("icon", FMT.TITLE)
-			.setStartTimestamps(starttime).setDetails(Settings.DISCORD_HIDE.value ? "Working on an unknown Model" : FMT.MODEL.name).build();
-		DiscordRPC.discordUpdatePresence(veryrichnot);
+		if(CORE == null) return;
+		try{
+			Activity activity = new Activity();
+			activity.setType(ActivityType.PLAYING);
+			activity.setActivityButtonsMode(ActivityButtonsMode.BUTTONS);
+			activity.setDetails("Modelling (on v." + FMT.VERSION + ")");
+			activity.setState(Settings.DISCORD_HIDE.value ? "Working on an unknown Model" : FMT.MODEL.name);
+			activity.timestamps().setStart(Instant.ofEpochMilli(starttime));
+			ActivityButton button0 = new ActivityButton();
+			button0.setLabel("FMT Download");
+			button0.setUrl("https://fexcraft.net/app/fmt");
+			activity.addButton(button0);
+			ActivityButton button1 = new ActivityButton();
+			button1.setLabel("FMT Support");
+			button1.setUrl("https://discord.gg/5evQCq2xt8");
+			activity.addButton(button1);
+			activity.assets().setLargeImage("icon");
+			CORE.activityManager().updateActivity(activity);
+		}
+		catch(Throwable e){
+			e.printStackTrace();
+		}
+	}
+
+	public static void start(){
+		new Thread(() -> {
+			CreateParams params = new CreateParams();
+			params.setClientID(587016218196574209L);
+			params.setFlags(CreateParams.getDefaultFlags());
+			try{
+				CORE = new Core(params);
+				CORE.setLogHook(LogLevel.VERBOSE, (a, b) -> {});
+				DiscordUtil.update(true);
+				while(!glfwWindowShouldClose(FMT.getWindow())){
+					CORE.runCallbacks();
+					try{
+						Thread.sleep(100);
+					}
+					catch(InterruptedException e){
+						e.printStackTrace();
+					}
+				}
+			}
+			catch(Throwable e){
+				e.printStackTrace();
+			}
+		}).start();
 	}
 
 }
