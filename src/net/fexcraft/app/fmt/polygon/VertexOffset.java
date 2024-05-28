79daff27134c4f4465e90c297a734afc1f04e675
Ferdinand Calo <fexcalo@gmail.com>
2024-05-28 09:36:53 +0200

    Update VertexOffset.java

--- src/net/fexcraft/app/fmt/polygon/VertexOffset.java
+++ src/net/fexcraft/app/fmt/polygon/VertexOffset.java
@@ -1,7 +1,10 @@
 package net.fexcraft.app.fmt.polygon;
 
+import net.fexcraft.app.fmt.FMT;
 import net.fexcraft.lib.common.math.Vec3f;
 import net.fexcraft.lib.frl.Vertex;
+
+import java.util.Map;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -9,6 +12,7 @@ import net.fexcraft.lib.frl.Vertex;
 public class VertexOffset {
 
 	public Vertex vertex;
+	public float[] color;
 	public Vector3F off = new Vector3F();
 	public Vec3f org;
 	public float ou, ov;
@@ -31,6 +35,24 @@ public class VertexOffset {
 		float sv = polygon.glm.glObj.grouptex ? polygon.group().texSizeY : polygon.model().texSizeY;
 		vertex.u = ou + u / su;
 		vertex.v = ov + v / sv;
+	}
+
+	public static VertexOffset getPicked(int pick){
+		Vertex vert = null;
+		for(Map.Entry<Vertex, Integer> entry : Polygon.vertcolors.entrySet()){
+			if(entry.getValue() != pick) continue;
+			vert = entry.getKey();
+			break;
+		}
+		if(vert == null) return null;
+		for(Group group : FMT.MODEL.allgroups()){
+			for(Polygon poly : group){
+				for(VertexOffset off : poly.vertices){
+					if(off.vertex == vert) return off;
+				}
+			}
+		}
+		return null;
 	}
 
 }
