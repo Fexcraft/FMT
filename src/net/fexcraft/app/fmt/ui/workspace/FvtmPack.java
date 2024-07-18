a967b3124775c6138578dcee742f2e234adb1b32
Ferdinand Calo <fexcalo@gmail.com>
2024-07-18 19:30:36 +0200

    Create FvtmPack.java

---
+++ src/net/fexcraft/app/fmt/ui/workspace/FvtmPack.java
@@ -0,0 +1,24 @@
+package net.fexcraft.app.fmt.ui.workspace;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class FvtmPack extends DirComponent {
+
+	public final HashMap<FvtmType, ArrayList<DirComponent>> content = new HashMap<>();
+	public final ArrayList<DirComponent> textures = new ArrayList<>();
+	public final String id;
+
+	public FvtmPack(ViewerFileType type, WorkspaceViewer folcom, DirComponent root, File file, String id){
+		super(type, folcom, root, file, 0);
+		for(FvtmType ft : FvtmType.values()){
+			content.put(ft, new ArrayList<>());
+		}
+		this.id = id;
+	}
+
+}
