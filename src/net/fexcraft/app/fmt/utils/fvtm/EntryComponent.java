96be4ce0a86ba463f93014dd818a9ff8f9c0a406
Ferdinand Calo <fexcalo@gmail.com>
2024-07-20 18:27:56 +0200

    Update EntryComponent.java

--- src/net/fexcraft/app/fmt/utils/fvtm/EntryComponent.java
+++ src/net/fexcraft/app/fmt/utils/fvtm/EntryComponent.java
@@ -1,13 +1,15 @@
 package net.fexcraft.app.fmt.utils.fvtm;
 
-import com.spinyowl.legui.component.Component;
-import com.spinyowl.legui.component.Label;
-import com.spinyowl.legui.component.SelectBox;
-import com.spinyowl.legui.component.TextInput;
+import com.spinyowl.legui.component.*;
 import com.spinyowl.legui.component.event.textinput.TextInputContentChangeEvent;
+import net.fexcraft.app.fmt.FMT;
+import net.fexcraft.app.fmt.ui.Editor;
+import net.fexcraft.app.fmt.ui.EditorComponent;
 import net.fexcraft.app.fmt.ui.Icon;
 import net.fexcraft.app.fmt.ui.fields.BoolButton;
 import net.fexcraft.app.fmt.ui.fields.ColorField;
+import net.fexcraft.app.fmt.ui.workspace.FvtmPack;
+import net.fexcraft.app.fmt.ui.workspace.WorkspaceViewer;
 import net.fexcraft.app.json.JsonArray;
 import net.fexcraft.app.json.JsonValue;
 
@@ -22,8 +24,20 @@ public class EntryComponent extends Component {
 	public TextInput input;
 	private static String[] xyz = { "x", "y", "z" };
 	private ArrayList<EntryComponent> coms = new ArrayList<>();
+	private FVTMConfigEditor editor;
+	private EntryComponent rcom;
+	private ConfigEntry entry;
+	private JsonValue root;
+	private JsonValue obj;
+	private Object key;
 
-	public EntryComponent(ConfigEntry entry, JsonValue root, Object idxkey, JsonValue obj, Boolean container){
+	public EntryComponent(FVTMConfigEditor confeditor, EntryComponent rootcom, ConfigEntry entry, JsonValue root, Object idxkey, JsonValue obj, Boolean container){
+		editor = confeditor;
+		this.entry = entry;
+		this.root = root;
+		this.obj = obj;
+		rcom = rootcom;
+		key = idxkey;
 		add(label = new Label((entry.name == null ? idxkey : entry.name) + (entry.required ? "*" : ""), 30, 0, 200, 30));
 		if(container != null){
 			if(container){
@@ -35,61 +49,85 @@ public class EntryComponent extends Component {
 			return;
 		}
 		add(new Icon(0, 20, 0, 5, 5, "./resources/textures/icons/configeditor/" + entry.type.icon() + ".png", () -> {
+
 		}).addTooltip(entry.type.icon()));
+		if(rcom != null){
+			add(new Icon(0, 20, 0, 525 + (entry.type.select() ? 25 : 0), 5, "./resources/textures/icons/configeditor/remove.png", () -> {
+				if(rcom.entry.type.subs()){
+					if(root.isMap()) root.asMap().rem((String)idxkey);
+					else root.asArray().rem((int)idxkey);
+					rcom.gensubs();
+				}
+				else{
+					input.getTextState().setText(entry.gendef().string_value());
+				}
+			}).addTooltip("remove/reset"));
+		}
+		if(entry.type.select()){
+			add(new Icon(0, 20, 0, 525, 5, "./resources/textures/icons/configeditor/select.png", () -> {
+				switch(entry.type){
+					case PACKID: {
+						Dialog dialog = new Dialog("Select a pack.", 240, 70);
+						SelectBox<String> box = new SelectBox<>(10, 10, 220, 30);
+						box.setVisibleCount(8);
+						for(FvtmPack pack : WorkspaceViewer.viewer.rootfolders) box.addElement(pack.id);
+						box.addSelectBoxChangeSelectionEventListener(lis -> {
+							input.getTextState().setText(lis.getNewValue());
+							obj.value(lis.getNewValue());
+							dialog.close();
+						});
+						dialog.getContainer().add(box);
+						dialog.setResizable(false);
+						dialog.show(FMT.FRAME);
+						break;
+					}
+					case MODELLOC:{
+						Dialog dialog = new Dialog("Select a Model.", 240, 70);
+						SelectBox<String> box = new SelectBox<>(10, 10, 220, 30);
+						box.setVisibleCount(8);
+						box.addElement("(no model)");
+						for(FvtmPack pack : WorkspaceViewer.viewer.rootfolders){
+							box.addElement(pack.id);
+							//
+						}
+						box.addSelectBoxChangeSelectionEventListener(lis -> {
+							String val = lis.getNewValue();
+							if(val.equals("(no model)")) val = "null";
+							input.getTextState().setText(val);
+							obj.value(val);
+							dialog.close();
+						});
+						dialog.getContainer().add(box);
+						dialog.setResizable(false);
+						dialog.show(FMT.FRAME);
+						break;
+					}
+					case TEXLOC:{
+						Dialog dialog = new Dialog("Select a Texture.", 240, 70);
+						SelectBox<String> box = new SelectBox<>(10, 10, 220, 30);
+						box.setVisibleCount(8);
+						box.addElement("(no texture)");
+						for(FvtmPack pack : WorkspaceViewer.viewer.rootfolders){
+							box.addElement(pack.id);
+							//
+						}
+						box.addSelectBoxChangeSelectionEventListener(lis -> {
+							String val = lis.getNewValue();
+							if(val.equals("(no texture)")) val = "No Texture;fvtm:textures/entity/null.png";
+							input.getTextState().setText(val);
+							obj.value(val);
+							dialog.close();
+						});
+						dialog.getContainer().add(box);
+						dialog.setResizable(false);
+						dialog.show(FMT.FRAME);
+						break;
+					}
+				}
+			}).addTooltip("select"));
+		}
 		if(entry.type.subs()){
-			if(entry.type == EntryType.ARRAY){
-				if(obj != null){
-					JsonArray arr = obj.isArray() ? obj.asArray() : new JsonArray();
-					for(int i = 0; i < arr.size(); i++){
-						EntryComponent con = new EntryComponent(ConfigEntry.EMPTY, arr, i + "", null, false);
-						//
-						addsub(con);
-					}
-				}
-			}
-			else if(entry.type == EntryType.ARRAY_SIMPLE){
-				if(obj != null){
-					JsonArray arr = obj.isArray() ? obj.asArray() : null;
-					if(arr == null){
-						root.asMap().add(idxkey.toString(), arr = new JsonArray());
-						arr.add(obj);
-					}
-					for(int i = 0; i < arr.size(); i++){
-						if(entry.subs == null) addsub(new EntryComponent(ConfigEntry.TEXT, arr, i, arr.get(i), null));
-						else addsub(new EntryComponent(entry.subs.get(0), arr, i, arr.get(i), null));
-					}
-				}
-			}
-			else if(entry.type == EntryType.OBJECT){
-				if(obj != null){
-					obj.asMap().entries().forEach(e -> {
-						EntryComponent con = new EntryComponent(ConfigEntry.EMPTY, obj.asMap(), e.getKey(), null, true);
-						//
-						addsub(con);
-					});
-				}
-			}
-			else if(entry.type == EntryType.OBJECT_KEY_VAL){
-				if(obj != null){
-					obj.asMap().entries().forEach(e -> {
-						addsub(new EntryComponent(entry.subs.get(0), obj.asMap(), e.getKey(), e.getValue(), null));
-					});
-				}
-			}
-			add(new Icon(0, 20, 0, 220, 5, "./resources/textures/icons/configeditor/add.png", () -> {
-				if(entry.type == EntryType.ARRAY){
-					//
-				}
-				else if(entry.type == EntryType.ARRAY_SIMPLE){
-					//
-				}
-				else if(entry.type == EntryType.OBJECT){
-					//
-				}
-				else if(entry.type == EntryType.OBJECT_KEY_VAL){
-					//
-				}
-			}));
+			gensubs();
 		}
 		else if(entry.type.trio()){
 			Object ik = idxkey;
@@ -147,7 +185,7 @@ public class EntryComponent extends Component {
 			add(box);
 		}
 		else{//text
-			add(input = new TextInput(obj == null ? entry.typedef() : obj.string_value(), 220, 2, 300, 26));
+			add(input = new TextInput(obj == null ? entry.gendef().string_value() : obj.string_value(), 220, 2, 300, 26));
 			input.addTextInputContentChangeEventListener(event -> {
 				if(obj == null){
 					if(root.isMap()){
@@ -158,6 +196,66 @@ public class EntryComponent extends Component {
 				else obj.value(get(event, entry.type));
 			});
 		}
+	}
+
+	private void gensubs(){
+		coms.clear();
+		removeIf(com -> com instanceof EntryComponent);
+		if(entry.type == EntryType.ARRAY){
+			if(obj != null){
+				JsonArray arr = obj.isArray() ? obj.asArray() : new JsonArray();
+				for(int i = 0; i < arr.size(); i++){
+					EntryComponent con = new EntryComponent(editor, this, ConfigEntry.EMPTY, arr, i + "", null, false);
+					//
+					addsub(con);
+				}
+			}
+		}
+		else if(entry.type == EntryType.ARRAY_SIMPLE){
+			JsonArray arr = obj == null || !obj.isArray() ? null : obj.asArray();
+			if(arr == null){
+				root.asMap().add(key.toString(), arr = new JsonArray());
+				if(obj != null) arr.add(obj);
+				obj = arr;
+			}
+			for(int i = 0; i < arr.size(); i++){
+				addsub(new EntryComponent(editor, this, entry.subs.get(0), arr, i, arr.get(i), null));
+			}
+		}
+		else if(entry.type == EntryType.OBJECT){
+			if(obj != null){
+				obj.asMap().entries().forEach(e -> {
+					EntryComponent con = new EntryComponent(editor, this, ConfigEntry.EMPTY, obj.asMap(), e.getKey(), null, true);
+					//
+					addsub(con);
+				});
+			}
+		}
+		else if(entry.type == EntryType.OBJECT_KEY_VAL){
+			if(obj != null){
+				obj.asMap().entries().forEach(e -> {
+					addsub(new EntryComponent(editor, this, entry.subs.get(0), obj.asMap(), e.getKey(), e.getValue(), null));
+				});
+			}
+		}
+		add(new Icon(0, 20, 0, 220, 5, "./resources/textures/icons/configeditor/add.png", () -> {
+			if(entry.type == EntryType.ARRAY){
+				//
+			}
+			else if(entry.type == EntryType.ARRAY_SIMPLE){
+				JsonArray arr = root.asMap().getArray(key.toString());
+				arr.add(entry.subs.get(0).gendef());
+				addsub(new EntryComponent(editor, this, entry.subs.get(0), arr, arr.size() - 1, arr.get(arr.size() - 1), null));
+				editor.resize();
+			}
+			else if(entry.type == EntryType.OBJECT){
+				//
+			}
+			else if(entry.type == EntryType.OBJECT_KEY_VAL){
+				//
+			}
+		}));
+		editor.resize();
 	}
 
 	private Object get(TextInputContentChangeEvent event, EntryType type){
