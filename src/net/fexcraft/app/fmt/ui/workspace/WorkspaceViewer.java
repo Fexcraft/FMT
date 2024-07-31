0ee4098180720458aa63903ad3c62cb69065a72f
Ferdinand Calo <fexcalo@gmail.com>
2024-07-31 23:58:42 +0200

    Update WorkspaceViewer.java

--- src/net/fexcraft/app/fmt/ui/workspace/WorkspaceViewer.java
+++ src/net/fexcraft/app/fmt/ui/workspace/WorkspaceViewer.java
@@ -1,6 +1,5 @@
 package net.fexcraft.app.fmt.ui.workspace;
 
-import com.spinyowl.legui.component.Label;
 import com.spinyowl.legui.component.ScrollablePanel;
 import com.spinyowl.legui.component.Widget;
 import com.spinyowl.legui.component.event.component.ChangeSizeEvent;
@@ -10,7 +9,10 @@ import net.fexcraft.app.fmt.ui.fields.RunButton;
 import net.fexcraft.app.fmt.utils.Logging;
 import org.joml.Vector2f;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
@@ -23,7 +25,9 @@ public class WorkspaceViewer extends Widget {
 	public ArrayList<FvtmPack> rootfolders = new ArrayList<FvtmPack>();
 	public ScrollablePanel packspanel;
 	public ScrollablePanel infopanel;
-	public RunButton button;
+	public RunButton refresh;
+	public RunButton run12;
+	public RunButton run20;
 	public File folder;
 	public int scrollableheight;
 
@@ -53,17 +57,53 @@ public class WorkspaceViewer extends Widget {
 			getContainer().setSize(vec);
 			packspanel.setSize(310, getSize().y - 30);
 			infopanel.setSize(vec.x - 310, getSize().y - 30);
-			button.setPosition(vec.x - 110, 5);
+			refresh.setPosition(vec.x - 110, 5);
+			run12.setPosition(vec.x - 220, 5);
+			run20.setPosition(vec.x - 330, 5);
 			resize();
 		});
 		Settings.applyBorderless(packspanel);
 		Settings.applyBorderless(infopanel);
 		getContainer().add(packspanel);
 		getContainer().add(infopanel);
-		getContainer().add(button = new RunButton("refresh", getSize().x - 110, 5, 100, 20, () -> genView()));
+		getContainer().add(refresh = new RunButton("Refresh", getSize().x - 110, 5, 100, 20, () -> genView()));
+		getContainer().add(run12 = new RunButton("Run 1.12", getSize().x - 220, 5, 100, 20, () -> run(true)));
+		getContainer().add(run20 = new RunButton("Run 1.20", getSize().x - 330, 5, 100, 20, () -> run(false)));
 		folder = new File(Settings.WORKSPACE_ROOT.value);
 		if(!folder.exists()) folder.mkdirs();
 		genView();
+	}
+
+	public static void run(boolean bool){
+		String cmd;
+		if(bool){
+			cmd = Settings.M12RCMD.value.replace("{JAVA}", Settings.JAVA8_PATH.value + "/bin/java");
+		}
+		else{
+			cmd = Settings.M20RCMD.value.replace("{JAVA}", Settings.JAVA17_PATH.value + "/bin/java");
+		}
+		try{
+			Process pr = Runtime.getRuntime().exec(new String[]{ cmd }, new String[0], new File(bool ? Settings.M12PATH.value : Settings.M20PATH.value));
+			new Thread(() -> {
+				Logging.log("=================");
+				Logging.log("RUN CMD LOG START");
+				BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+				String line = null;
+				try{
+					while((line = input.readLine()) != null) Logging.log(line);
+				}
+				catch(IOException e){
+					Logging.log(e);
+				}
+				Logging.log("RUN CMD LOG END");
+				Logging.log("=================");
+			}).start();
+		}
+		catch(IOException e){
+			Logging.log(e);
+			Logging.log("RUN FAILED");
+			Logging.log("=================");
+		}
 	}
 
 	public static void show0(){
