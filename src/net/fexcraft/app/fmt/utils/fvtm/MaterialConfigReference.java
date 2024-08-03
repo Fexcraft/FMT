59f44901d0754e9145396e45e2afaa2be985be48
Ferdinand Calo <fexcalo@gmail.com>
2024-08-03 17:17:26 +0200

    Create MaterialConfigReference.java

---
+++ src/net/fexcraft/app/fmt/utils/fvtm/MaterialConfigReference.java
@@ -0,0 +1,43 @@
+package net.fexcraft.app.fmt.utils.fvtm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static net.fexcraft.app.fmt.utils.fvtm.ConfigEntry.of;
+import static net.fexcraft.app.fmt.utils.fvtm.EntryType.*;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class MaterialConfigReference implements Reference {
+
+	public static MaterialConfigReference INSTANCE = new MaterialConfigReference();
+	private ArrayList entries = new ArrayList();
+
+	public MaterialConfigReference(){
+		entries.add(of("Addon", PACKID).required());
+		entries.add(of("ID", TEXT).required());
+		entries.add(of("Name", TEXT).def("Unnamed Material", true));
+		entries.add(of("CreativeTab", TEXT).def("default", true));
+		entries.add(of("Description", ARRAY_SIMPLE).add(of(TEXT).def("description entry", true)));
+		entries.add(of("ItemTexture", TEXLOC));
+		entries.add(of("MaxItemStackSize", INTEGER).limit(64, 0, 64));
+		entries.add(of("OreDictionary", TEXT));
+		entries.add(of("ContainerItem", TEXT));
+		//
+		entries.add(of("MaxItemDamage", INTEGER));
+		entries.add(of("ItemBurnTime", INTEGER));
+		entries.add(of("VehicleKey", BOOLEAN));
+		entries.add(of("FuelContainer", BOOLEAN));
+		entries.add(of("FuelCapacity", INTEGER));
+		entries.add(of("FuelType", TEXT));
+		entries.add(of("FuelGroup", TEXT));
+		entries.add(of("ImpactWrench", INTEGER).limit(-1, -1, 8));
+	}
+
+	@Override
+	public List<ConfigEntry> getEntries() {
+		return entries;
+	}
+
+}
