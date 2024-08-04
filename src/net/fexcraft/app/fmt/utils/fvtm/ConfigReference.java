07fcb9589a4c2fb1ad77194f2b14b113e9834412
Ferdinand Calo <fexcalo@gmail.com>
2024-08-04 14:08:35 +0200

    Create ConfigReference.java

---
+++ src/net/fexcraft/app/fmt/utils/fvtm/ConfigReference.java
@@ -0,0 +1,18 @@
+package net.fexcraft.app.fmt.utils.fvtm;
+
+import java.util.ArrayList;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class ConfigReference {
+
+    public final ArrayList<ConfigEntry> entries = new ArrayList<>();
+
+    public ConfigReference(){
+        init();
+    }
+
+    protected void init(){}
+
+}
