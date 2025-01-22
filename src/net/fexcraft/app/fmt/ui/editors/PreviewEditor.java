124205b1a32df37bda4a5b4b8dfcd8275b062b62
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-23 00:18:29 +0100

    Update PreviewEditor.java

--- src/net/fexcraft/app/fmt/ui/editors/PreviewEditor.java
+++ src/net/fexcraft/app/fmt/ui/editors/PreviewEditor.java
@@ -1,8 +1,6 @@
 package net.fexcraft.app.fmt.ui.editors;
 
 import net.fexcraft.app.fmt.ui.Editor;
-import net.fexcraft.app.fmt.ui.components.MultiplierComponent;
-import net.fexcraft.app.fmt.ui.components.PivotGeneral;
 import net.fexcraft.app.fmt.ui.components.PreviewGeneral;
 
 /**
@@ -12,7 +10,6 @@ public class PreviewEditor extends Editor {
 
 	public PreviewEditor(){
 		super("helper_editor", "Helper Editor", false);
-		addComponent(new MultiplierComponent());
 		addComponent(new PreviewGeneral());
 	}
 
