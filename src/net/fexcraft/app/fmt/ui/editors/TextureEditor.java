1c5d4c5f028f58f24560613642aad606990a0d3c
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-23 00:18:30 +0100

    Update TextureEditor.java

--- src/net/fexcraft/app/fmt/ui/editors/TextureEditor.java
+++ src/net/fexcraft/app/fmt/ui/editors/TextureEditor.java
@@ -3,9 +3,6 @@ package net.fexcraft.app.fmt.ui.editors;
 import net.fexcraft.app.fmt.settings.Settings;
 import net.fexcraft.app.fmt.ui.Editor;
 import net.fexcraft.app.fmt.ui.components.CurrentColor;
-import net.fexcraft.app.fmt.ui.components.ModelExports;
-import net.fexcraft.app.fmt.ui.components.ModelGeneral;
-import net.fexcraft.app.fmt.ui.components.MultiplierComponent;
 import net.fexcraft.app.fmt.ui.components.PainterPaletteGradient;
 import net.fexcraft.app.fmt.ui.components.PainterPaletteSpectrum;
 import net.fexcraft.app.fmt.ui.components.PainterTools;
@@ -19,7 +16,6 @@ public class TextureEditor extends Editor {
 	public TextureEditor(){
 		super("painter", "Painting Utils", false);
 		if(Settings.SHOW_QUICK_ADD.value) addComponent(new QuickAdd());
-		addComponent(new MultiplierComponent());
 		addComponent(new CurrentColor());
 		addComponent(new PainterPaletteGradient(true));
 		addComponent(new PainterPaletteSpectrum(true));
