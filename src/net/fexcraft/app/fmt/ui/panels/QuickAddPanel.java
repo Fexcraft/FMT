9b2c0ce255ef3144c7fcc424cb47a43943a0b533
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-24 20:55:23 +0100

    Update QuickAddPanel.java

--- src/net/fexcraft/app/fmt/ui/panels/QuickAddPanel.java
+++ src/net/fexcraft/app/fmt/ui/panels/QuickAddPanel.java
@@ -74,6 +74,7 @@ public class QuickAddPanel extends EditorPanel {
 			FMT.MODEL.addGroup(null, name.getTextState().getText());
 			dialog.close();
 		}));
+		dialog.getContainer().add(new RunButton("dialog.button.cancel", 200, 70, 100, 20, () -> dialog.close()));
 		dialog.setResizable(false);
 		dialog.show(FMT.FRAME);
 	}
