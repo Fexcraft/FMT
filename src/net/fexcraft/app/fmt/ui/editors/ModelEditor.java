037e79369d636bd099a1934b618705cd14faaa05
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-24 18:13:30 +0100

    Update ModelEditor.java

--- src/net/fexcraft/app/fmt/ui/editors/ModelEditor.java
+++ src/net/fexcraft/app/fmt/ui/editors/ModelEditor.java
@@ -1,10 +1,8 @@
 package net.fexcraft.app.fmt.ui.editors;
 
-import net.fexcraft.app.fmt.settings.Settings;
 import net.fexcraft.app.fmt.ui.Editor;
 import net.fexcraft.app.fmt.ui.components.ModelExports;
 import net.fexcraft.app.fmt.ui.components.ModelGeneral;
-import net.fexcraft.app.fmt.ui.components.QuickAdd;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -13,7 +11,6 @@ public class ModelEditor extends Editor {
 
 	public ModelEditor(){
 		super("model_editor", "Model Editor", false);
-		if(Settings.SHOW_QUICK_ADD.value) addComponent(new QuickAdd());
 		addComponent(new ModelGeneral());
 		addComponent(new ModelExports());
 	}
