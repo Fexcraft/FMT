5de9250bf93c135326fee39b663199ea9982782c
Ferdinand Calo <fercalo96@yahoo.de>
2025-02-06 22:29:02 +0100

    Update Toolbar.java

--- src/net/fexcraft/app/fmt/ui/Toolbar.java
+++ src/net/fexcraft/app/fmt/ui/Toolbar.java
@@ -116,7 +116,8 @@ public class Toolbar extends Panel {
 			new MenuButton(3, "editors.model", () -> Editor.MODEL_EDITOR.toggle()),
 			new MenuButton(4, "editors.texture", () -> Editor.TEXTURE_EDITOR.toggle()),
 			new MenuButton(5, "editors.uv", () -> Editor.UV_EDITOR.toggle()),
-			new MenuButton(6, "editors.preview", () -> Editor.PREVIEW_EDITOR.toggle())
+			new MenuButton(6, "editors.preview", () -> Editor.PREVIEW_EDITOR.toggle()),
+			new MenuButton(7, "editors.config", () -> Editor.CONFIG_EDITOR.toggle())
 		));
 		this.add(new ToolbarMenu(4, "trees",
 			new MenuButton(0, "trees.polygon", () -> Editor.POLYGON_TREE.toggle()),
