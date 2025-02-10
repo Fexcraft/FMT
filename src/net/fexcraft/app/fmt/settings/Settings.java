4f492acce09c1d9633560c38944f30d38d43d9b7
Ferdinand Calo <fercalo96@yahoo.de>
2025-02-10 19:04:31 +0100

    Update Settings.java

--- src/net/fexcraft/app/fmt/settings/Settings.java
+++ src/net/fexcraft/app/fmt/settings/Settings.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import net.fexcraft.app.fmt.texture.TexturePainter;
 import net.fexcraft.app.fmt.updater.Catalog;
 import net.fexcraft.app.fmt.polygon.Model;
 import net.fexcraft.app.fmt.port.im.ImportManager;
@@ -129,6 +130,7 @@ public class Settings {
 	public static RGBSetting SELECTION_LINES;
 	public static RGBSetting BOTTOM_INFO_BAR_COLOR;
 	public static RGBSetting LIGHT_COLOR;
+	public static Setting<Integer> PAINTER_CHANNELS;
 	//
 	public static Setting<Boolean> TESTING;
 	public static Setting<String> WORKSPACE_NAME;
@@ -274,6 +276,7 @@ public class Settings {
 		ARROW_SENSIVITY = new Setting<>("arrow_sensivity", 8f, CONTROL, map).minmax(0.001f, 100f);
 		NUMBERFIELD_BUTTONS = new Setting<>("numberfield_buttons", false, GENERAL, map);
 		AUTO_SHOW_COMPONENTS = new Setting<>("auto_show_components", true, GENERAL, map);
+		PAINTER_CHANNELS = new Setting<>("painter_channels", 2, GENERAL, map);
 		//
 		TESTING = new Setting<>("testing", false, GENERAL, map);
 		WORKSPACE_NAME = new Setting<>("name", "FMT Workspace", WORKSPACE, map);
@@ -356,6 +359,8 @@ public class Settings {
 		//
 		ExportManager.init(map);
 		ImportManager.init(map);
+		TexturePainter.CHANNELS = new RGB[PAINTER_CHANNELS.value];
+		for(Integer i = 0; i < PAINTER_CHANNELS.value; i++) TexturePainter.CHANNELS[i] = RGB.WHITE.copy();
 		//
 		for(Map.Entry<String, Map<String, Setting<?>>> entry : SETTINGS.entrySet()){
 			if(!map.has(entry.getKey())) continue;
