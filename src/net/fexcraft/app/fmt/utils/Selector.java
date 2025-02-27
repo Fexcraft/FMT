89324be456ca3a47de8475e917dca1e068902d5b
Ferdinand Calo <fexcalo@gmail.com>
2025-02-27 21:53:52 +0100

    Update Selector.java

--- src/net/fexcraft/app/fmt/utils/Selector.java
+++ src/net/fexcraft/app/fmt/utils/Selector.java
@@ -1,5 +1,7 @@
 package net.fexcraft.app.fmt.utils;
 
+import net.fexcraft.app.fmt.FMT;
+import net.fexcraft.app.fmt.polygon.VertexOffset;
 import net.fexcraft.app.fmt.utils.Picker.PickType;
 
 /**
@@ -7,10 +9,24 @@ import net.fexcraft.app.fmt.utils.Picker.PickType;
  */
 public class Selector {
 
+	public static boolean SHOW_VERTICES = true;
 	public static PickType TYPE = PickType.POLYGON;
 
 	public static void set(PickType sel){
 		TYPE = sel;
+	}
+
+	public static boolean showCorners(){
+		return TYPE != PickType.VERTEX && !SHOW_VERTICES;
+	}
+
+	public static void move(){
+		if(FMT.MODEL.getSelectedVerts().size() < 2) return;
+		VertexOffset vert0 = FMT.MODEL.getSelectedVerts().get(0);
+		VertexOffset vert1 = FMT.MODEL.getSelectedVerts().get(0);
+		if(!vert0.polygon.getShape().isShapebox()) return;
+
+		FMT.MODEL.getSelectedVerts().clear();
 	}
 
 }
