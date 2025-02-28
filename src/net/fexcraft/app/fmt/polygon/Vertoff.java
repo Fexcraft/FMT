08f568907e7bc70fd84a7dd2668e52db04c60f83
Ferdinand Calo <fexcalo@gmail.com>
2025-02-28 15:58:30 +0100

    Update Vertoff.java

--- src/net/fexcraft/app/fmt/polygon/Vertoff.java
+++ src/net/fexcraft/app/fmt/polygon/Vertoff.java
@@ -1,7 +1,6 @@
 package net.fexcraft.app.fmt.polygon;
 
 import net.fexcraft.app.fmt.FMT;
-import net.fexcraft.lib.common.math.Vec3f;
 import net.fexcraft.lib.frl.Vertex;
 
 import java.util.Map;
@@ -9,38 +8,29 @@ import java.util.Map;
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public class VertexOffset {
+public class Vertoff {
 
-	public Vertex vertex;
+	public VOKey key;
 	public float[] color;
 	public Polygon polygon;
 	public Vector3F off = new Vector3F();
-	public Vec3f org;
-	public float ou, ov;
 	public float u, v;
 
-	public VertexOffset(Vertex vert){
-		set(vert);
+	public Vertoff(VOKey vkey){
+		key = vkey;
 	}
 
-	public void set(Vertex vert){
-		vertex = vert;
-		org = vert.vector;
-		ou = vert.u;
-		ov = vert.v;
-	}
-
-	public void apply(Polygon poly){
+	public void apply(Polygon poly, Vertex vertex){
 		if(vertex == null) return;
 		polygon = poly;
-		vertex.vector = org.add(off.x, off.y, off.z);
+		vertex.vector = vertex.vector.add(off.x, off.y, off.z);
 		float su = polygon.glm.glObj.grouptex ? polygon.group().texSizeX : polygon.model().texSizeX;
 		float sv = polygon.glm.glObj.grouptex ? polygon.group().texSizeY : polygon.model().texSizeY;
-		vertex.u = ou + u / su;
-		vertex.v = ov + v / sv;
+		vertex.u += u / su;
+		vertex.v += v / sv;
 	}
 
-	public static VertexOffset getPicked(int pick){
+	public static Vertoff getPicked(int pick){
 		Vertex vert = null;
 		for(Map.Entry<Vertex, Integer> entry : Polygon.vertcolors.entrySet()){
 			if(entry.getValue() != pick) continue;
@@ -50,12 +40,33 @@ public class VertexOffset {
 		if(vert == null) return null;
 		for(Group group : FMT.MODEL.allgroups()){
 			for(Polygon poly : group){
-				for(VertexOffset off : poly.vertices){
-					if(off.vertex == vert) return off;
+				for(Vertoff off : poly.vertices.values()){
+					//TODO if(off.vertex == vert) return off;
 				}
 			}
 		}
 		return null;
+	}
+
+	public static record VOKey(VOType type, int vertix, int secondary){
+
+		@Override
+		public boolean equals(Object o){
+			if(o instanceof VOKey == false) return false;
+			VOKey vo = (VOKey)o;
+			return type == vo.type && vertix == vo.vertix && secondary == vo.secondary;
+		}
+
+	}
+
+	public static enum VOType {
+
+		BOX_CORNER,
+		CYL_INNER,
+		CYL_OUTER,
+		CURVE,
+		;
+
 	}
 
 }
