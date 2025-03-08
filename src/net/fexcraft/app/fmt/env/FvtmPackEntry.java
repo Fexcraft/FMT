f35e84aba936bb4a6f0ec5a55e4d879027158e1b
Ferdinand Calo <fexcalo@gmail.com>
2025-03-08 22:24:47 +0100

    Update FvtmPackEntry.java

--- src/net/fexcraft/app/fmt/env/FvtmPackEntry.java
+++ src/net/fexcraft/app/fmt/env/FvtmPackEntry.java
@@ -1,9 +1,12 @@
 package net.fexcraft.app.fmt.env;
 
+import net.fexcraft.app.fmt.ui.Icon;
 import net.fexcraft.app.json.JsonHandler;
 import net.fexcraft.app.json.JsonMap;
 
 import java.io.File;
+
+import static net.fexcraft.app.fmt.env.PackDevEnv.fe_height;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -14,6 +17,11 @@ public class FvtmPackEntry extends FileViewEntry {
 		super(env, file);
 		JsonMap map = JsonHandler.parse(cfg).asMap();
 		label.getTextState().setText(map.getString("Name", label.getTextState().getText()));
+	}
+
+	@Override
+	protected void addIcon(){
+		add(icon = new Icon(0, fe_height, 0, 0, 0, ICON_LOC.formatted("folder_fvtmpack"), () -> {}));
 	}
 
 }
