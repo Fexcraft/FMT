04c3ebb7bd579de6f2de6cb765de766ed444b49b
Ferdinand Calo <fexcalo@gmail.com>
2025-03-09 17:17:58 +0100

    Create FileEvent.java

---
+++ src/net/fexcraft/app/fmt/env/FileEvent.java
@@ -0,0 +1,15 @@
+package net.fexcraft.app.fmt.env;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public enum FileEvent {
+
+	DIR_CREATE,
+	DIR_DELETE,
+	DIR_CHANGE,
+	FILE_CREATE,
+	FILE_DELETE,
+	FILE_CHANGE
+
+}
