4a0da72c6b6249cda74ecca8a218a2ab67c8e8bb
Ferdinand Calo <fexcalo@gmail.com>
2025-03-09 22:01:58 +0100

    Update FileViewEntry.java

--- src/net/fexcraft/app/fmt/env/FileViewEntry.java
+++ src/net/fexcraft/app/fmt/env/FileViewEntry.java
@@ -3,6 +3,7 @@ package net.fexcraft.app.fmt.env;
 import com.spinyowl.legui.component.Component;
 import com.spinyowl.legui.component.Label;
 import net.fexcraft.app.fmt.ui.Icon;
+import net.fexcraft.app.fmt.utils.Logging;
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.app.json.JsonValue;
 
@@ -20,6 +21,7 @@ public class FileViewEntry extends Component {
 
 	protected static String ICON_LOC = "./resources/textures/icons/filetree/%s.png";
 	protected ArrayList<FileViewEntry> sub = new ArrayList<>();
+	protected FileViewEntry parent;
 	protected PackDevEnv root;
 	protected boolean maximized;
 	protected Label label;
@@ -32,15 +34,19 @@ public class FileViewEntry extends Component {
 		setPosition(0, fe_height);
 		root = env;
 		file = fil;
-		path = fil.toPath().toAbsolutePath();
 		add(label = new Label(file.getName(), fe_height + 5, 5, fp_width - 30, 20));
 		addIcon();
 		if(file.isDirectory()){
 			List<File> files = PackDevEnv.getSorted(file.listFiles(), true);
 			for(File sub : files){
-				addEntry(new FileViewEntry(env, sub));
+				addEntry(new FileViewEntry(env, sub).set(this));
 			}
 		}
+	}
+
+	private FileViewEntry set(FileViewEntry par){
+		parent = par;
+		return this;
 	}
 
 	private void addEntry(FileViewEntry entry){
@@ -51,6 +57,7 @@ public class FileViewEntry extends Component {
 	private void remEntry(FileViewEntry entry){
 		sub.remove(entry);
 		remove(entry);
+		if(!file.exists() && parent != null) parent.remEntry(this);
 	}
 
 	public FileViewEntry getEntry(String filename){
@@ -83,7 +90,36 @@ public class FileViewEntry extends Component {
 	}
 
 	public boolean onFileEvent(File file, FileEvent event){
-		for(FileViewEntry entry : sub) if(entry.onFileEvent(file, event)) return true;
+		if(event.change() && file.equals(this.file)){
+			ArrayList<FileViewEntry> rems = new ArrayList<>();
+			for(FileViewEntry entry : sub){
+				if(!entry.file.exists()) rems.add(entry);
+				Logging.log(entry.file.exists() + " " + entry.file);
+			}
+			for(FileViewEntry rem : rems){
+				remEntry(rem);
+			}
+			return true;
+		}
+		if(file.getParentFile().equals(this.file)){
+			switch(event){
+				case CREATE: {
+					FileViewEntry entry = getEntry(file.getName());
+					if(entry != null) return true;
+					addEntry(new FileViewEntry(root, file).set(this));
+					return true;
+				}
+				case DELETE: {
+					FileViewEntry entry = getEntry(file.getName());
+					remEntry(entry);
+					return true;
+				}
+			}
+		}
+		for(FileViewEntry entry : sub){
+			if(!entry.file.isDirectory()) continue;
+			if(entry.onFileEvent(file, event)) return true;
+		}
 		return false;
 	}
 
