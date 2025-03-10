4c9033261475e69328977e2a72edf6c38a071339
Ferdinand Calo <fexcalo@gmail.com>
2025-03-10 21:17:48 +0100

    Create EnvContent.java

---
+++ src/net/fexcraft/app/fmt/env/EnvContent.java
@@ -0,0 +1,12 @@
+package net.fexcraft.app.fmt.env;
+
+import com.spinyowl.legui.component.Component;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class EnvContent extends Component {
+
+	public EnvTab tab;
+
+}
