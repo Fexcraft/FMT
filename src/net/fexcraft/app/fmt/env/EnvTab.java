7f6f781b1378b7a17be0268c9c8417451ad3ff21
Ferdinand Calo <fexcalo@gmail.com>
2025-03-11 21:20:17 +0100

    Update EnvTab.java

--- src/net/fexcraft/app/fmt/env/EnvTab.java
+++ src/net/fexcraft/app/fmt/env/EnvTab.java
@@ -2,22 +2,29 @@ package net.fexcraft.app.fmt.env;
 
 import com.spinyowl.legui.component.Component;
 import com.spinyowl.legui.component.Label;
+import com.spinyowl.legui.event.CursorEnterEvent;
 import com.spinyowl.legui.event.MouseClickEvent;
 import net.fexcraft.app.fmt.env.con.JsonContent;
 import net.fexcraft.app.fmt.ui.Icon;
 
 import static com.spinyowl.legui.event.MouseClickEvent.MouseClickAction.CLICK;
 import static com.spinyowl.legui.input.Mouse.MouseButton.MOUSE_BUTTON_LEFT;
+import static com.spinyowl.legui.style.Style.DisplayType.MANUAL;
+import static com.spinyowl.legui.style.Style.DisplayType.NONE;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class EnvTab extends Component {
 
+	protected static Icon vit;
+	protected static Icon sit;
 	public final FileViewEntry entry;
 	protected EnvContent content;
 	private Label label;
 	private Icon icon;
+	private Icon save;
+	private Icon exit;
 
 	public EnvTab(FileViewEntry ent){
 		setSize(230, 30);
@@ -29,7 +36,18 @@ public class EnvTab extends Component {
 				entry.root.setContent(content);
 			}
 		});
-		content = new JsonContent(entry.file);
+		add(exit = new Icon(0, 20, 0, 205, 5, "./resources/textures/icons/configeditor/remove.png", () -> entry.root.remTab(entry)));
+		add(save = new Icon(0, 20, 0, 180, 5, "./resources/textures/icons/toolbar/save.png", () -> entry.root.remTab(entry)));
+		exit.getStyle().setDisplay(NONE);
+		save.getStyle().setDisplay(NONE);
+		label.getListenerMap().addListener(CursorEnterEvent.class, e -> {
+			if(!e.isEntered()) return;
+			if(vit != null) vit.getStyle().setDisplay(NONE);
+			(vit = exit).getStyle().setDisplay(MANUAL);
+			if(sit != null) sit.getStyle().setDisplay(NONE);
+			(sit = save).getStyle().setDisplay(MANUAL);
+		});
+		content = new JsonContent(this, entry.file);
 	}
 
 	public int updateDisplay(int buf){
