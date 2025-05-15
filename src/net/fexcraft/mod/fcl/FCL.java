b5f04f637a303d9b542ce55ee4375d5e4682a915
Ferdinand Calo <fexcalo@gmail.com>
2025-05-15 21:21:40 +0200

    Create FCL.java

---
+++ src/net/fexcraft/mod/fcl/FCL.java
@@ -0,0 +1,26 @@
+package net.fexcraft.mod.fcl;
+
+import io.netty.buffer.ByteBuf;
+import net.fexcraft.mod.uni.IDL;
+import net.fexcraft.mod.uni.tag.TagCW;
+import org.slf4j.Logger;
+
+public class FCL {
+
+	public static final Logger LOGGER = null;
+
+	public static void bindTex(IDL tex){}
+
+	public static UniFCL CONFIG;
+
+	public static IDL requestServerFile(String lis, String loc){
+		return null;
+	}
+
+	public static void writeTag(ByteBuf buffer, TagCW com){}
+
+	public static TagCW readTag(ByteBuf buffer){
+		return null;//
+	}
+
+}
