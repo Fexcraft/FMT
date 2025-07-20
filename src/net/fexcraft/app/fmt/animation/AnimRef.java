76e4ca883f23e04a40477a0124040d0ce1d7f8d4
Ferdinand Calo <fexcalo@gmail.com>
2025-07-20 22:52:49 +0200

    Create AnimRef.java

---
+++ src/net/fexcraft/app/fmt/animation/AnimRef.java
@@ -0,0 +1,59 @@
+package net.fexcraft.app.fmt.animation;
+
+import net.fexcraft.app.fmt.polygon.Group;
+import net.fexcraft.app.fmt.polygon.PolyRenderer;
+import net.fexcraft.app.json.JsonMap;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class AnimRef extends Animation {
+
+	private String id;
+
+	public AnimRef(String id){
+		this.id = id;
+	}
+
+	@Override
+	public Animation create(JsonMap map){
+		AnimRef ref = new AnimRef(map.getString("id", "fvtm:glow"));
+		return ref;
+	}
+
+	@Override
+	public JsonMap save(){
+		return new JsonMap();
+	}
+
+	@Override
+	public void pre(Group group, PolyRenderer.DrawMode mode, float alpha){
+
+	}
+
+	@Override
+	public void pst(Group group, PolyRenderer.DrawMode mode, float alpha){
+
+	}
+
+	@Override
+	public String id(){
+		return id;
+	}
+
+	@Override
+	public String[] keys(){
+		return new String[0];
+	}
+
+	@Override
+	public Object get(String str){
+		return null;
+	}
+
+	@Override
+	public void set(String str, Object val){
+
+	}
+
+}
