fb865291075b8bb6d63490c46f7b066b17c34301
Ferdinand Calo <fexcalo@gmail.com>
2025-07-23 19:48:33 +0200

    Create AnimationEditor.java

---
+++ src/net/fexcraft/app/fmt/ui/editors/AnimationEditor.java
@@ -0,0 +1,15 @@
+package net.fexcraft.app.fmt.ui.editors;
+
+import net.fexcraft.app.fmt.ui.Editor;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class AnimationEditor extends Editor {
+
+	public AnimationEditor(){
+		super("anim_editor", "Animation Editor", false);
+		//
+	}
+
+}
