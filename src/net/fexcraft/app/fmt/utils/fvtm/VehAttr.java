ca2c167b4a46dc3622e536ea2c2cc7ccb6100b2e
Ferdinand Calo <fexcalo@gmail.com>
2025-07-25 12:46:52 +0200

    Create VehAttr.java

---
+++ src/net/fexcraft/app/fmt/utils/fvtm/VehAttr.java
@@ -0,0 +1,38 @@
+package net.fexcraft.app.fmt.utils.fvtm;
+
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.app.json.JsonValue;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class VehAttr {
+
+	public Object value;
+	public Type type;
+
+	public VehAttr(JsonMap map){
+		type = Type.valueOf(map.getString("type", "string").toUpperCase());
+		switch(type){
+			case STRING -> value = map.getString("value", "");
+			case BOOL -> value = map.getBoolean("value", false);
+			case INT -> value = map.getInteger("value", 0);
+			case FLOAT -> value = map.getFloat("value", 0f);
+			case LONG -> value = map.getLong("value", 0l);
+		}
+	}
+
+	public JsonMap save(){
+		JsonMap map = new JsonMap();
+		map.add("type", type.name().toLowerCase());
+		map.add("value", new JsonValue<>(value));
+		return map;
+	}
+
+	public static enum Type {
+
+		STRING, BOOL, INT, FLOAT, LONG
+
+	}
+
+}
