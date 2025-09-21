e0e4bab7855cd40fce4cc75f75fe24420d924bb2
Ferdinand Calo <fexcalo@gmail.com>
2025-09-21 22:19:12 +0200

    Update EditorSidePanel.java

--- src/net/fexcraft/app/fmt/nui/EditorSidePanel.java
+++ src/net/fexcraft/app/fmt/nui/EditorSidePanel.java
@@ -1,10 +1,14 @@
 package net.fexcraft.app.fmt.nui;
 
+import net.fexcraft.app.fmt.ui.Editor;
+import net.fexcraft.app.fmt.update.UpdateEvent;
+import net.fexcraft.app.fmt.update.UpdateHandler;
 import net.fexcraft.lib.common.math.RGB;
 
+import java.util.function.Consumer;
+
 import static net.fexcraft.app.fmt.nui.EditorRoot.EDITOR_WIDTH;
-import static net.fexcraft.app.fmt.nui.FMTInterface.col_75;
-import static net.fexcraft.app.fmt.nui.FMTInterface.col_cd;
+import static net.fexcraft.app.fmt.nui.FMTInterface.*;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -40,7 +44,7 @@ public class EditorSidePanel extends Element {
 
 		@Override
 		public void init(Object... args){
-			add(new Element().size(32, 32).texture(args[0].toString()).onclick(() -> toggle()));
+			add(new Element().size(32, 32).texture(args[0].toString()).onclick(ci -> toggle()));
 		}
 
 		public void toggle(){
@@ -53,11 +57,11 @@ public class EditorSidePanel extends Element {
 
 	public static class Multiplier extends Panel {
 
-		private SelectorBar[] bars = new SelectorBar[1];
+		private SelectorBar[] bars = new SelectorBar[3];
 		private Element text;
 
 		public Multiplier(int x, int y){
-			super(x, y, 380, 90);
+			super(x, y, 380, 80);
 		}
 
 		@Override
@@ -65,7 +69,18 @@ public class EditorSidePanel extends Element {
 			super.init(args);
 			linecolor(RGB.BLACK);
 			add(text = new Element().size(80, 30).pos(38, 20).color(col_75).text("0").hide());
-			add((bars[0] = new SelectorBar()).pos(130, 10).hide(), 200);
+			Consumer<Float> mul = m -> {
+				if(Editor.RATE != m){
+					Editor.RATE = m;
+					UpdateHandler.update(new UpdateEvent.EditorRate(Editor.RATE));
+				}
+			};
+			add((bars[0] = new SelectorBar()).pos(130, 10).hide(), 200, 1, 16, 1, "1 - 16", mul);
+			add((bars[1] = new SelectorBar()).pos(130, 35).hide(), 200, 0.0625, 1, 0.0625, "0.0625 - 1", mul);
+			add((bars[2] = new SelectorBar()).pos(130, 60).hide(), 200, 0.1, 1, 0.1, "0.1 - 1", mul);
+			UpdateHandler.register(com -> {
+				com.add(UpdateEvent.EditorRate.class, e -> text.text(e.rate()));
+			});
 		}
 
 		@Override
