2ed11674734614d69b921810734ddd25c1822119
Ferdinand Calo <fexcalo@gmail.com>
2025-09-23 21:28:14 +0200

    Update EditorRoot.java

--- src/net/fexcraft/app/fmt/nui/EditorRoot.java
+++ src/net/fexcraft/app/fmt/nui/EditorRoot.java
@@ -17,6 +17,17 @@ public class EditorRoot extends Element {
 	@Override
 	public void init(Object... args){
 		add(new EditorSidePanel());
+		int iinc = 35, buff = -iinc + 4, yo = 4;
+		for(EditorMode value : EditorMode.values()){
+			add(new Element().pos(buff += iinc, yo).size(32, 32)
+				.texture("icons/editor/" + value.name().toLowerCase()).hoverable(true)
+				.onclick(ci -> setMode(EditorMode.POLYGON))
+				.hint("editor.mode." + value.name().toLowerCase()));
+		}
+	}
+
+	private void setMode(EditorMode mode){
+
 	}
 
 	@Override
@@ -26,6 +37,12 @@ public class EditorRoot extends Element {
 
 	public void toggle(){
 		visible = !visible;
+	}
+
+	public static enum EditorMode {
+
+		POLYGON, MODEL, PIVOT, TEXTURE, PAINTER, PREVIEW, VARIABLE, ANIMATION
+
 	}
 
 }
