cb7ff469a615521e192d7cc2ba623d96b29a2798
Ferdinand Calo <fexcalo@gmail.com>
2025-09-25 22:54:54 +0200

    Update EditorRoot.java

--- src/net/fexcraft/app/fmt/nui/EditorRoot.java
+++ src/net/fexcraft/app/fmt/nui/EditorRoot.java
@@ -9,6 +9,8 @@ import static net.fexcraft.app.fmt.nui.FMTInterface.EDITOR_WIDTH;
  */
 public class EditorRoot extends Element {
 
+	private static EditorTab[] editors = new EditorTab[EditorMode.values().length];
+
 	public EditorRoot(){
 		super();
 		onResize();
@@ -17,17 +19,19 @@ public class EditorRoot extends Element {
 	@Override
 	public void init(Object... args){
 		add(new EditorSidePanel());
-		int iinc = 35, buff = -iinc + 4, yo = 4;
-		for(EditorMode value : EditorMode.values()){
+		int iinc = 35, buff = -iinc + 4, yo = 4, ti = 0;
+		for(EditorMode mode : EditorMode.values()){
 			add(new Element().pos(buff += iinc, yo).size(32, 32)
-				.texture("icons/editor/" + value.name().toLowerCase()).hoverable(true)
-				.onclick(ci -> setMode(EditorMode.POLYGON))
-				.hint("editor.mode." + value.name().toLowerCase()));
+				.texture("icons/editor/" + mode.name().toLowerCase()).hoverable(true)
+				.onclick(ci -> setMode(mode))
+				.hint("editor.mode." + mode.name().toLowerCase()));
+			add(editors[ti++] = new EditorTab(mode));
 		}
 	}
 
 	private void setMode(EditorMode mode){
-
+		for(EditorTab editor : editors) editor.hide();
+		editors[mode.ordinal()].show();
 	}
 
 	@Override
