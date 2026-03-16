62340818e3ddc7657975c35cb2e03f11636effea
Ferdinand Calo <fexcalo@gmail.com>
2026-03-16 20:52:59 +0100

    Update Field.java

--- src/net/fexcraft/app/fmt/nui/Field.java
+++ src/net/fexcraft/app/fmt/nui/Field.java
@@ -1,23 +1,38 @@
 package net.fexcraft.app.fmt.nui;
 
-import net.fexcraft.lib.common.math.RGB;
+import org.lwjgl.glfw.GLFW;
+
+import java.util.function.Consumer;
+
+import static net.fexcraft.app.fmt.nui.FMTInterface.col_bd;
+import static org.lwjgl.glfw.GLFW.*;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class Field extends Element {
 
+	public Consumer<String> consumer;
+	public String previous;
 	public boolean hidden;
 
-	public Field(int width){
+	public Field(float width){
 		super();
-		size(width, 30);
+		size(width, 26);
+		hoverable = true;
+		selectable = true;
+		color(col_bd);
+	}
 
+	public Field(float width, Consumer<String> cons){
+		this(width);
+		consumer = cons;
 	}
 
 	@Override
 	public void init(Object... args){
-		add(new Element().color(RGB.BLACK).size(2, 20).pos(w - 2, 5));
+		text("");
+		//add(new Element().color(RGB.BLACK).size(2, 20).pos(w - 2, 5));
 	}
 
 	@Override
@@ -27,6 +42,38 @@ public class Field extends Element {
 			elements.get(0).pos(text.w > w ? w - 2 : text.w + 6, 5);
 		}
 		return this;
+	}
+
+	@Override
+	protected void onSelect(){
+		previous = text.text();
+	}
+
+	@Override
+	protected void onDeselect(Element current){
+		text(previous);
+	}
+
+	public boolean onInput(int key, int code, int action, int mods){
+		if(key == GLFW_KEY_LEFT_SHIFT || key == GLFW_KEY_RIGHT_SHIFT){
+			return false;
+		}
+		if(key == GLFW_KEY_ESCAPE){
+			select(null);
+			return false;
+		}
+		if(action != GLFW_RELEASE) return true;
+		if(key == GLFW_KEY_ENTER || key == GLFW_KEY_KP_ENTER){
+			if(consumer != null) consumer.accept(text.text());
+			previous = null;
+			return true;
+		}
+		if(key >= 32 && key <= 96){
+			String kn = GLFW.glfwGetKeyName(key, code);
+			if(kn == null) kn = "";
+			text(text.text() + kn);
+		}
+		return true;
 	}
 
 }
