fd4f96cbacaea0d02d145257fd8acda07def3ed1
Ferdinand Calo <fexcalo@gmail.com>
2026-03-18 21:45:57 +0100

    Update ETabCom.java

--- src/net/fexcraft/app/fmt/nui/editor/ETabCom.java
+++ src/net/fexcraft/app/fmt/nui/editor/ETabCom.java
@@ -1,7 +1,6 @@
 package net.fexcraft.app.fmt.nui.editor;
 
 import net.fexcraft.app.fmt.nui.Element;
-import net.fexcraft.lib.common.math.RGB;
 
 import static net.fexcraft.app.fmt.nui.FMTInterface.*;
 
@@ -10,12 +9,42 @@ import static net.fexcraft.app.fmt.nui.FMTInterface.*;
  */
 public class ETabCom extends Element {
 
+	private boolean minimized = false;
+	private int fullheight;
+
 	@Override
 	public void init(Object... args){
 		border(col_85);
-		size(EDITOR_CONTENT, (int)args[1]);
+		size(EDITOR_CONTENT, fullheight = (int)args[1]);
 		add(new Element().translate(args[0].toString()).color(col_85).size(EDITOR_CONTENT, 30));
 		elements.get(0).text.color(col_cd);
+		elements.get(0).add(new Element().hoverable(true).texture("icons/component/size").size(28, 28).pos(EDITOR_CONTENT - 29, 1).onclick(ci -> {
+			if(minimized) show();
+			else hide();
+		}));
+
+	}
+
+	@Override
+	public Element hide(){
+		minimized = true;
+		size(EDITOR_CONTENT, 30);
+		for(int i = 1; i < elements.size(); i++){
+			elements.get(i).hide();
+		}
+		recompile();
+		return this;
+	}
+
+	@Override
+	public Element show(){
+		minimized = false;
+		size(EDITOR_CONTENT, fullheight);
+		for(int i = 1; i < elements.size(); i++){
+			elements.get(i).show();
+		}
+		recompile();
+		return this;
 	}
 
 }
