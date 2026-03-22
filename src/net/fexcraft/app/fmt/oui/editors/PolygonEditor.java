5d9bade73c0a3d85491141e49630f8f187fffa69
Ferdinand Calo <fexcalo@gmail.com>
2026-03-22 21:21:33 +0100

    Update PolygonEditor.java

--- src/net/fexcraft/app/fmt/oui/editors/PolygonEditor.java
+++ src/net/fexcraft/app/fmt/oui/editors/PolygonEditor.java
@@ -27,7 +27,7 @@ public class PolygonEditor extends Editor {
 		//addComponent(BOXON = new PolygonAttributes(true));
 		//addComponent(BOXOFF = new PolygonAttributes(false));
 		//addComponent(SHAPEBOX = new ShapeboxComponent());
-		addComponent(CYLINDER = new CylinderComponentFull());
+		//addComponent(CYLINDER = new CylinderComponentFull());
 		addComponent(CURVE = new CurveComponent());
 		addComponent(MARKER = new MarkerComponent());
 		//BOXOFF.minimize(true);
