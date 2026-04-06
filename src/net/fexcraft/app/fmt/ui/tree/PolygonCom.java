0d2f073a20dcf060e0b1fa19809d6d4d8407321f
Ferdinand Calo <fexcalo@gmail.com>
2026-04-06 22:29:26 +0200

    Update PolygonCom.java

--- src/net/fexcraft/app/fmt/ui/tree/PolygonCom.java
+++ src/net/fexcraft/app/fmt/ui/tree/PolygonCom.java
@@ -32,14 +32,27 @@ public class PolygonCom extends Element {
 		add(new Element().hoverable(true).texture("icons/component/visible").size(26, 26).pos(EDITOR_CONTENT - 40, 1).onclick(ci -> {
 			polygon.visible = !polygon.visible;
 			UpdateHandler.update(new UpdateEvent.PolygonVisibility(polygon, polygon.visible));
-		}).hint("tree.polygon.polygon.visible"));
+		}).hint("tree.polygon.polygon.visible").hide());
 		add(new Element().hoverable(true).texture("icons/component/remove").size(26, 26).pos(EDITOR_CONTENT - 70, 1).onclick(ci -> {
 			if(ASK_POLYGON_REMOVAL.value){
 				//TODO
 			}
 			else polygon.group().remove(polygon);
-		}).hint("tree.polygon.polygon.remove"));
+		}).hint("tree.polygon.polygon.remove").hide());
 		updateLabelColor();
+	}
+
+	@Override
+	public void hovered(boolean bool){
+		super.hovered(bool);
+		if(bool){
+			elements.get(0).show();
+			elements.get(1).show();
+		}
+		else{
+			elements.get(0).hide();
+			elements.get(1).hide();
+		}
 	}
 
 	protected void updateLabelColor(){
