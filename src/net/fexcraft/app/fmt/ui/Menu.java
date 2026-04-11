fd685fe8a43bf10cf03187d39c765173d74429ff
Ferdinand Calo <fexcalo@gmail.com>
2026-04-11 23:02:40 +0200

    Update Menu.java

--- src/net/fexcraft/app/fmt/ui/Menu.java
+++ src/net/fexcraft/app/fmt/ui/Menu.java
@@ -1,61 +1,64 @@
 package net.fexcraft.app.fmt.ui;
 
+import net.fexcraft.lib.common.math.RGB;
+
 import java.util.function.Consumer;
+
+import static net.fexcraft.app.fmt.ui.FMTInterface.col_bd;
+import static net.fexcraft.app.fmt.ui.FMTInterface.col_cd;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class Menu extends Element {
 
-	private Consumer<Menu> onhover;
-	private boolean vertical;
 	private boolean open;
 
-	public Menu(){
+	public Menu(int width){
 		super();
-		hoverable = true;
+		border(RGB.BLACK);
+		color(col_cd);
+		size(width, 2);
+		hide();
 	}
 
 	@Override
 	public Element root(Element elm){
 		super.root(elm);
-		vertical = !(elm instanceof Menu);
-		if(vertical) z++;
+		if(elm instanceof Menu) pos(elm.w, y());
 		return this;
 	}
 
-	@Override
-	public void add(Element elm){
-		elm.visible = false;
-		elm.hoverable = true;
-		elm.size(w - 2, h);
-		super.add(elm);
-		elm.pos(1 + (vertical ? 0 : w), (elements.size() - (vertical ? 0 : 1)) * 30);
-		elm.z += 2;
-		elm.recompile();
+
+	public void addEntry(String text, Consumer<ClickInfo> cons){
+		addEntry(text, cons, true);
+	}
+
+	public void addEntry(String text, Consumer<ClickInfo> cons, boolean close){
+		add(new Element().pos(1, 1 + elms_size() * 30).size(w - 2, 30).onclick(ci -> {
+			if(cons != null) cons.accept(ci);
+			if(close) hide();
+		}).translate(text).color(col_bd).hoverable(true));
+		size(w, 2 + elms_size() * 30);
+		recompile();
 	}
 
 	@Override
 	public void update(){
-		if(hoveredx() && !open){
-			open = true;
-			for(Element elm : elements) elm.visible = open;
-		}
-		if(!hoveredx() && open){
-			open = false;
-			for(Element elm : elements) elm.visible = open;
-		}
+		if(root.hoveredx() && open) show();
+		if(!root.hoveredx() && open) hide();
 	}
 
 	@Override
-	public void hovered(boolean bool){
-		super.hovered(bool);
-		if(onhover != null) onhover.accept(this);
+	public Element show(){
+		open = true;
+		return super.show();
 	}
 
-	public Menu onhover(Consumer<Menu> cons){
-		onhover = cons;
-		return this;
+	@Override
+	public Element hide(){
+		open = false;
+		return super.hide();
 	}
 
 }
