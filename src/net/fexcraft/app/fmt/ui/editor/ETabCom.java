85d3883cd98d6bfdd4a2035671722f29c27037ef
Ferdinand Calo <fexcalo@gmail.com>
2026-04-13 17:40:46 +0200

    Update ETabCom.java

--- src/net/fexcraft/app/fmt/ui/editor/ETabCom.java
+++ src/net/fexcraft/app/fmt/ui/editor/ETabCom.java
@@ -2,6 +2,7 @@ package net.fexcraft.app.fmt.ui.editor;
 
 import net.fexcraft.app.fmt.ui.Element;
 
+import static net.fexcraft.app.fmt.settings.Settings.GENERIC_BACKGROUND_2;
 import static net.fexcraft.app.fmt.settings.Settings.GENERIC_TEXT_1;
 import static net.fexcraft.app.fmt.ui.FMTInterface.*;
 
@@ -15,9 +16,9 @@ public class ETabCom extends Element {
 
 	@Override
 	public void init(Object... args){
-		border(col_85);
+		border(GENERIC_BACKGROUND_2.value);
 		size(EDITOR_CONTENT, fullheight = (int)args[1]);
-		add(new Element().translate(args[0].toString()).color(col_85).size(EDITOR_CONTENT, 30));
+		add(new Element().translate(args[0].toString()).color(GENERIC_BACKGROUND_2.value).size(EDITOR_CONTENT, 30));
 		elements.get(0).text.color(GENERIC_TEXT_1.value);
 		elements.get(0).add(new Element().hoverable(true).texture("icons/component/size").size(28, 28).pos(EDITOR_CONTENT - 29, 1).onclick(ci -> {
 			if(minimized) show();
