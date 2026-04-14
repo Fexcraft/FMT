f2e01a010096a4de0c3b114de6902d3c293b8d36
Ferdinand Calo <fexcalo@gmail.com>
2026-04-14 22:28:47 +0200

    Update PolygonCom.java

--- src/net/fexcraft/app/fmt/ui/tree/PolygonCom.java
+++ src/net/fexcraft/app/fmt/ui/tree/PolygonCom.java
@@ -4,6 +4,7 @@ import net.fexcraft.app.fmt.FMT;
 import net.fexcraft.app.fmt.polygon.Polygon;
 import net.fexcraft.app.fmt.settings.Settings;
 import net.fexcraft.app.fmt.ui.Element;
+import net.fexcraft.app.fmt.ui.HidingElm;
 import net.fexcraft.app.fmt.update.UpdateEvent;
 import net.fexcraft.app.fmt.update.UpdateHandler;
 
@@ -29,30 +30,17 @@ public class PolygonCom extends Element {
 	@Override
 	public void init(Object... args){
 		text(polygon.name());
-		add(new Element().hoverable(true).texture("icons/component/visible").size(26, 26).pos(EDITOR_CONTENT - 40, 1).onclick(ci -> {
+		add(new HidingElm().hoverable(true).texture("icons/component/visible").size(26, 26).pos(EDITOR_CONTENT - 40, 1).onclick(ci -> {
 			polygon.visible = !polygon.visible;
 			UpdateHandler.update(new UpdateEvent.PolygonVisibility(polygon, polygon.visible));
 		}).hint("tree.polygon.polygon.visible").hide());
-		add(new Element().hoverable(true).texture("icons/component/remove").size(26, 26).pos(EDITOR_CONTENT - 70, 1).onclick(ci -> {
+		add(new HidingElm().hoverable(true).texture("icons/component/remove").size(26, 26).pos(EDITOR_CONTENT - 70, 1).onclick(ci -> {
 			if(ASK_POLYGON_REMOVAL.value){
 				//TODO
 			}
 			else polygon.group().remove(polygon);
 		}).hint("tree.polygon.polygon.remove").hide());
 		updateLabelColor();
-	}
-
-	@Override
-	public void hovered(boolean bool){
-		super.hovered(bool);
-		if(bool){
-			elements.get(0).show();
-			elements.get(1).show();
-		}
-		else{
-			elements.get(0).hide();
-			elements.get(1).hide();
-		}
 	}
 
 	protected void updateLabelColor(){
