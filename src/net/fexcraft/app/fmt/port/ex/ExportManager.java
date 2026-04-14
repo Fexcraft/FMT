9ea432dc74c613bd1fe0b8c29cb99be86037cdef
Ferdinand Calo <fexcalo@gmail.com>
2026-04-14 23:20:55 +0200

    Update ExportManager.java

--- src/net/fexcraft/app/fmt/port/ex/ExportManager.java
+++ src/net/fexcraft/app/fmt/port/ex/ExportManager.java
@@ -1,23 +1,17 @@
 package net.fexcraft.app.fmt.port.ex;
 
-import static net.fexcraft.app.fmt.utils.Translator.translate;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import com.spinyowl.legui.component.Dialog;
-import com.spinyowl.legui.component.Label;
-import com.spinyowl.legui.component.SelectBox;
 
 import net.fexcraft.app.fmt.FMT;
 import net.fexcraft.app.fmt.polygon.Group;
 import net.fexcraft.app.fmt.settings.Settings;
 import net.fexcraft.app.fmt.oui.FileChooser;
-import net.fexcraft.app.fmt.oui.GenericDialog;
-import net.fexcraft.app.fmt.oui.GroupSelectionPanel;
-import net.fexcraft.app.fmt.oui.fields.RunButton;
+import net.fexcraft.app.fmt.ui.Dialog;
+import net.fexcraft.app.fmt.ui.DropList;
 import net.fexcraft.app.fmt.ui.FMTInterface;
+import net.fexcraft.app.fmt.ui.FontRenderer;
 import net.fexcraft.app.json.JsonMap;
 
 /**
@@ -53,67 +47,63 @@ public class ExportManager {
 	}
 	
 	public static void export(){
-		Dialog dialog = new Dialog(translate("export.choose.dialog"), 400, 190);
-		dialog.setResizable(false);
-		//Settings.applyComponentTheme(dialog.getContainer());
-		dialog.getContainer().add(new Label(translate("export.choose.category"), 10, 10, 380, 25));
-		SelectBox<String> selcat = new SelectBox<>(10, 35, 380, 25);
-		for(String cat : CATEGORIES){
-			selcat.addElement(cat);
+		Dialog dia = FMT.UI.createDialog(500, 180, "export.choose.dialog");
+		DropList<String> cat = new DropList<>(490);
+		DropList<Exporter> exp = new DropList<>(490);
+		dia.addText(0, "export.choose.category");
+		dia.addRowElm(1, cat);
+		for(String c : CATEGORIES){
+			cat.addEntry(c, c);
 		}
-		selcat.setSelected(0, true);
-		selcat.setVisibleCount(6);
-		dialog.getContainer().add(selcat);
-		dialog.getContainer().add(new Label(translate("export.choose.exporter"), 10, 70, 380, 25));
-		SelectBox<String> select = new SelectBox<>(10, 95, 380, 25);
-		select.setVisibleCount(6);
+		cat.selectEntry(0);
+		dia.addText(2, "export.choose.exporter");
+		dia.addRowElm(3, exp);
 		String fircat = CATEGORIES.get(0);
 		for(Exporter exporter : EXPORTERS){
-			if(exporter.categories().contains(fircat)) select.addElement(exporter.name());
+			if(exporter.categories().contains(fircat)) exp.addEntry(exporter.name(), exporter);
 		}
-		dialog.getContainer().add(select);
-		selcat.addSelectBoxChangeSelectionEventListener(lis -> {
-			while(select.getElements().size() > 0) select.removeElement(0);
+		exp.selectEntry(0);
+		cat.onchange((key, val) -> {
+			exp.clear();
 			for(Exporter exporter : EXPORTERS){
-				if(exporter.categories().contains(lis.getNewValue())) select.addElement(exporter.name());
+				if(exporter.categories().contains(key)) exp.addEntry(exporter.name(), exporter);
 			}
+			exp.selectEntry(0);
 		});
-		dialog.getContainer().add(new RunButton("dialog.button.continue", 10, 135, 100, 25, () -> {
-			dialog.close();
-			Exporter exporter = null;
-			String sel = select.getSelection();
-			for(Exporter ex : EXPORTERS){
-				if(ex.name().equals(sel)) exporter = ex;
-			}
-			if(exporter == null) return;
+		dia.consumer(d -> {
+			Exporter exporter = exp.getSelVal();
 			if(exporter.nogroups()){
 				showFileChooserDialog(exporter, Collections.EMPTY_LIST);
 			}
 			else showGroupSelectionDialog(exporter);
-		}));
-		//dialog.show(FMT.FRAME);
+		}, null);
+		dia.buttons(100, Dialog.DialogButton.CONTINUE);
 	}
 
 	private static void showGroupSelectionDialog(Exporter exporter){
-		Dialog dialog = new Dialog(translate("export.choose.groups"), 400, 440);
-		dialog.setResizable(false);
-		//Settings.applyComponentTheme(dialog.getContainer());
-		GroupSelectionPanel panel = new GroupSelectionPanel(10, 10, 380, 360);
-		dialog.getContainer().add(panel);
-		dialog.getContainer().add(new RunButton("dialog.button.continue", 10, 380, 100, 30, () -> {
-			dialog.close();
-			showFileChooserDialog(exporter, panel.getSelectedGroups());
-		}));
-		//dialog.show(FMT.FRAME);
+		Dialog dia = FMT.UI.createDialog(500, 400, "export.choose.groups");
+		dia.addText(0, "//TODO");
+		dia.addText(1, "group selection panel");
+		dia.consumer(d -> {
+			showFileChooserDialog(exporter, FMT.MODEL.allgroups());//TODO panel selected groups
+		}, null);
+		dia.buttons(100, Dialog.DialogButton.CONTINUE);
 	}
 
 	private static void showFileChooserDialog(Exporter exporter, List<Group> groups){
 		FileChooser.chooseFile("export.choose.file", "", exporter.extensions(), true, file -> {
 			if(file == null){
-				GenericDialog.showYN(null, () -> showFileChooserDialog(exporter, groups), null, "export.choose.nofile");
+				Dialog dia = FMT.UI.createDialog(400, 50, "export.choose.nofile");
+				dia.buttons(100, Dialog.DialogButton.YES, Dialog.DialogButton.NO);
+				dia.consumer(d -> showFileChooserDialog(exporter, groups), null);
 				return;
 			}
-			Runnable run = () -> GenericDialog.showOK("export.result", null, null, exporter.export(FMT.MODEL, file, groups));
+			Runnable run = () -> {
+				String res = exporter.export(FMT.MODEL, file, groups);
+				Dialog dia = FMT.UI.createDialog(FontRenderer.getWidth(res, FontRenderer.FontType.PLAIN) * 3 + 30, 100, "export.result");
+				dia.addText(0, res);
+				dia.buttons(100, Dialog.DialogButton.OK);
+			};
 			if(Settings.SETTINGS.containsKey("exporter-" + exporter.id())){
 				exporter.initSettings();
 				FMTInterface.settings.show("exporter-" + exporter.id(), run);
