0fe5639d66a36dfeb9eab1ecc41960235b2af38a
Ferdinand Calo <fexcalo@gmail.com>
2026-04-16 22:12:41 +0200

    Update PolygonCom.java

--- src/net/fexcraft/app/fmt/ui/tree/PolygonCom.java
+++ src/net/fexcraft/app/fmt/ui/tree/PolygonCom.java
@@ -3,8 +3,10 @@ package net.fexcraft.app.fmt.ui.tree;
 import net.fexcraft.app.fmt.FMT;
 import net.fexcraft.app.fmt.polygon.Polygon;
 import net.fexcraft.app.fmt.settings.Settings;
+import net.fexcraft.app.fmt.ui.Dialog;
 import net.fexcraft.app.fmt.ui.Element;
 import net.fexcraft.app.fmt.ui.HidingElm;
+import net.fexcraft.app.fmt.ui.editor.EditorRoot;
 import net.fexcraft.app.fmt.update.UpdateEvent;
 import net.fexcraft.app.fmt.update.UpdateHandler;
 
@@ -30,16 +32,23 @@ public class PolygonCom extends Element {
 	@Override
 	public void init(Object... args){
 		text(polygon.name());
-		add(new HidingElm().hoverable(true).texture("icons/component/visible").size(26, 26).pos(EDITOR_CONTENT - 40, 1).onclick(ci -> {
+		add(new HidingElm().hoverable(true).texture("icons/component/visible").size(26, 26).pos(EDITOR_CONTENT - 32 * 2, 1).onclick(ci -> {
 			polygon.visible = !polygon.visible;
 			UpdateHandler.update(new UpdateEvent.PolygonVisibility(polygon, polygon.visible));
 		}).hint("tree.polygon.polygon.visible").hide());
-		add(new HidingElm().hoverable(true).texture("icons/component/remove").size(26, 26).pos(EDITOR_CONTENT - 70, 1).onclick(ci -> {
+		add(new HidingElm().hoverable(true).texture("icons/component/remove").size(26, 26).pos(EDITOR_CONTENT - 32 * 3, 1).onclick(ci -> {
 			if(ASK_POLYGON_REMOVAL.value){
-				//TODO
+				FMT.UI.createDialog(500, 120, "tree.mode.polygon")
+					.addText(0, "tree.polygon.polygon.removal")
+					.addText(1, polygon.name() + " (" + polygon.getShape() + ")")
+					.consumer(d -> polygon.group().remove(polygon), null)
+					.buttons(100, Dialog.DialogButton.CONFIRM, Dialog.DialogButton.CANCEL);
 			}
 			else polygon.group().remove(polygon);
 		}).hint("tree.polygon.polygon.remove").hide());
+		add(new HidingElm().hoverable(true).texture("icons/component/edit").size(26, 26).pos(EDITOR_CONTENT - 32 * 4, 1).onclick(ci -> {
+			EditorRoot.setMode(EditorRoot.EditorMode.POLYGON);
+		}).hint("tree.polygon.polygon.editor").hide());
 		updateLabelColor();
 	}
 
