300d2d8e259e031bdae7ddb577947c0c932b5e86
Ferdinand Calo <fexcalo@gmail.com>
2026-04-29 23:07:19 +0200

    Create FvtmType.java

---
+++ src/net/fexcraft/app/fmt/workspace/FvtmType.java
@@ -0,0 +1,23 @@
+package net.fexcraft.app.fmt.workspace;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public enum FvtmType {
+
+	VEHICLE,
+	PART,
+	BLOCK,
+	MATERIAL,
+	CONSUMABLE,
+	MULTIBLOCK,
+	CONTAINER,
+	RAILGAUGE,
+	DECORATION,
+	CLOTH,
+	FUEL,
+	WIRE,
+	WIRE_DECO,
+	SIGN
+
+}
