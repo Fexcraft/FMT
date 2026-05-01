7a9b2ac9162f1fd9d6357c14e0a096371bc18938
Ferdinand Calo <fexcalo@gmail.com>
2026-05-01 12:45:03 +0200

    Update FvtmPackElm.java

--- src/net/fexcraft/app/fmt/workspace/FvtmPackElm.java
+++ src/net/fexcraft/app/fmt/workspace/FvtmPackElm.java
@@ -1,17 +1,30 @@
 package net.fexcraft.app.fmt.workspace;
 
+import net.fexcraft.app.fmt.utils.fvtm.LangCache;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class FvtmPackElm extends DirElm {
 
-	protected String id;
+	public final HashMap<FvtmType, List<FileElm>> content = new HashMap<>();
+	public final List<FileElm> textures = new ArrayList<>();
+	public final List<FileElm> models = new ArrayList<>();
+	public final LangCache lang;
+	public final String id;
 
 	public FvtmPackElm(File file, String id){
 		super(VFileType.FVTM_FOLDER, file);
 		this.id = id;
+		for(FvtmType val : FvtmType.values()){
+			content.put(val, new ArrayList<>());
+		}
+		lang = new LangCache(this);
 	}
 
 }
