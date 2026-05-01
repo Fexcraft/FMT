60d32ac861ace636f8423983a57501a2a2e194fb
Ferdinand Calo <fexcalo@gmail.com>
2026-05-01 17:47:23 +0200

    Update FvtmType.java

--- src/net/fexcraft/app/fmt/workspace/FvtmType.java
+++ src/net/fexcraft/app/fmt/workspace/FvtmType.java
@@ -7,28 +7,30 @@ import org.apache.commons.lang3.tuple.Pair;
  */
 public enum FvtmType {
 
-	VEHICLE("Vehicle", "vehicles"),
-	PART("Part", "parts"),
-	BLOCK("Block", "blocks"),
-	MATERIAL("Material", "materials"),
-	CONSUMABLE("Consumable", "consumables"),
-	MULTIBLOCK("MultiBlock", "multiblocks"),
-	CONTAINER("Shipping Container", "containers"),
-	RAILGAUGE("Rail Gauge", "railgauges"),
-	DECORATION("Decoration", "decos"),
-	CLOTH("Cloth", "clothes"),
-	FUEL("Fuel", "fuels"),
-	WIRE("Wire", "wires"),
-	WIRE_DECO("Wire Deco", "wires"),
-	SIGN("Sign", "signs");
+	VEHICLE("Vehicle", "vehicles", "vehicle"),
+	PART("Part", "parts", "part"),
+	BLOCK("Block", "blocks", "block"),
+	MATERIAL("Material", "materials", "material"),
+	CONSUMABLE("Consumable", "consumables", "consumable"),
+	MULTIBLOCK("MultiBlock", "multiblocks", "multiblock"),
+	CONTAINER("Shipping Container", "containers", "container"),
+	RAILGAUGE("Rail Gauge", "railgauges", "railgauge"),
+	DECORATION("Decoration", "decos", "deco"),
+	CLOTH("Cloth", "clothes", "cloth"),
+	FUEL("Fuel", "fuels", "fuel"),
+	WIRE("Wire", "wires", "wire"),
+	WIRE_DECO("Wire Deco", "wires", "wiredeco"),
+	SIGN("Sign", "signs", "sign");
 
 	public final Pair<VFileType, FvtmType> pair = Pair.of(VFileType.FVTM_CONFIG, this);
 	public final String _name;
 	public final String folder;
+	public final String suffix;
 
-	FvtmType(String _name, String folder){
+	FvtmType(String _name, String folder, String suffix){
 		this._name = _name;
 		this.folder = folder;
+		this.suffix = suffix;
 	}
 
 }
