0224647eecd88f45e0b11ceb4d2caebecf8ebfdd
Ferdinand Calo <fexcalo@gmail.com>
2026-05-01 17:47:17 +0200

    Update ConfigEditorTab.java

--- src/net/fexcraft/app/fmt/ui/editor/ConfigEditorTab.java
+++ src/net/fexcraft/app/fmt/ui/editor/ConfigEditorTab.java
@@ -1,9 +1,10 @@
 package net.fexcraft.app.fmt.ui.editor;
 
-import net.fexcraft.app.fmt.oui.components.ConfigGeneral;
+import net.fexcraft.app.fmt.FMT;
 import net.fexcraft.app.fmt.oui.workspace.WorkspaceViewer;
 import net.fexcraft.app.fmt.ui.*;
 import net.fexcraft.app.fmt.workspace.ConfigUtils;
+import net.fexcraft.app.fmt.workspace.IconFrameFrame;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -26,14 +27,15 @@ public class ConfigEditorTab extends EditorTab {
 		pack_utils.add(new RunElm(FO, next_y_pos(1), FF, "editor.config.pack_utils.pack_new", ci -> ConfigUtils.createNewFvtmPack()));
 		pack_utils.add(new RunElm(FO, next_y_pos(1), FF, "editor.config.pack_utils.gen_asset_dirs", ci -> ConfigUtils.genAssetDirs()));
 		pack_utils.add(new RunElm(FO, next_y_pos(1), FF, "editor.config.pack_utils.gen_icons", ci -> ConfigUtils.genIconsInPack()));
-		pack_utils.add(new RunElm(FO, next_y_pos(1), FF, "editor.config.pack_utils.content_new", ci -> ConfigGeneral.createNewContent()));
-		pack_utils.add(new RunElm(FO, next_y_pos(1), FF, "editor.config.pack_utils.icon_from_view", ci -> ConfigGeneral.createNewIcon()));
-		pack_utils.add(new RunElm(FO, next_y_pos(1), FF, "editor.config.pack_utils.road_assets", ci -> ConfigGeneral.genRoadAssets()));
+		pack_utils.add(new RunElm(FO, next_y_pos(1), FF, "editor.config.pack_utils.content_new", ci -> ConfigUtils.createNewContent()));
+		pack_utils.add(new RunElm(FO, next_y_pos(1), FF, "editor.config.pack_utils.icon_from_view",
+			ci -> FMT.WORKSPACE.selectPack(pack -> FMT.UI.add(new IconFrameFrame(), pack))));
+		pack_utils.add(new RunElm(FO, next_y_pos(1), FF, "editor.config.pack_utils.road_assets", ci -> ConfigUtils.genRoadAssets()));
 		//
 		next_y_elm_pos = 5;
 		container.add(file_utils = new ETabCom("file_utils"), "editor.config.file_utils", 100);
-		file_utils.add(new RunElm(FO, next_y_pos(1), FF, "editor.config.file_utils.open_json", ci -> ConfigGeneral.openJson()));
-		file_utils.add(new RunElm(FO, next_y_pos(1), FF, "editor.config.file_utils.mirror_lang", ci -> ConfigGeneral.mirrorLang()));
+		file_utils.add(new RunElm(FO, next_y_pos(1), FF, "editor.config.file_utils.open_json", ci -> ConfigUtils.openJson()));
+		file_utils.add(new RunElm(FO, next_y_pos(1), FF, "editor.config.file_utils.mirror_lang", ci -> ConfigUtils.mirrorLang()));
 		//
 		next_y_elm_pos = 5;
 		container.add(run_utils = new ETabCom("run_utils"), "editor.config.run_utils", 100);
