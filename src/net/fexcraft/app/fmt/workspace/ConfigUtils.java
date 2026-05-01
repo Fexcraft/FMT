7e2a1e4cf4d5c9249ef6d32c0a70627206dfaf58
Ferdinand Calo <fexcalo@gmail.com>
2026-05-01 12:44:56 +0200

    Update ConfigUtils.java

--- src/net/fexcraft/app/fmt/workspace/ConfigUtils.java
+++ src/net/fexcraft/app/fmt/workspace/ConfigUtils.java
@@ -1,8 +1,12 @@
 package net.fexcraft.app.fmt.workspace;
 
+import com.google.common.io.Files;
 import net.fexcraft.app.fmt.FMT;
+import net.fexcraft.app.fmt.ui.BoolElm;
 import net.fexcraft.app.fmt.ui.Dialog;
+import net.fexcraft.app.fmt.ui.Dialog.DialogButton;
 import net.fexcraft.app.fmt.ui.Field;
+import net.fexcraft.app.fmt.utils.Logging;
 import net.fexcraft.app.fmt.utils.SessionHandler;
 import net.fexcraft.app.fmt.utils.fvtm.LangCache;
 import net.fexcraft.app.json.JsonArray;
@@ -12,6 +16,9 @@ import net.fexcraft.app.json.JsonMap;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -99,10 +106,86 @@ public class ConfigUtils {
 				LangCache.genLangJson(new File(pr, "/assets/" + pid + "/lang/en_us.json"));
 				LangCache.genLangFile(new File(pr, "/assets/" + pid + "/lang/en_us.lang"));
 				//
-				FMT.WORKSPACE.reloadPacks();
-				FMT.WORKSPACE.show();
+				FMT.WORKSPACE.reloadPacks(() -> FMT.WORKSPACE.show());
 			}, null)
-			.buttons(100, Dialog.DialogButton.CONTINUE);
+			.buttons(100, DialogButton.CONTINUE);
+	}
+
+	public static void genAssetDirs(){
+		FMT.WORKSPACE.selectPack(pack -> {
+			Dialog dia = FMT.UI.createDialog(500, 80 + FvtmType.values().length * 30, "workspace.content_utils");
+			dia.addBoldText(0, "editor.config.pack_utils.gen_asset_dirs.select");
+			HashMap<FvtmType, Boolean> map = new HashMap();
+			int row = 1;
+			BoolElm elm;
+			for(FvtmType value : FvtmType.values()){
+				elm = dia.addBoolText(row++, value._name + " (" + value.folder + ")");
+				elm.set(() -> map.get(value), b -> map.put(value, b));
+				map.put(value, false);
+			}
+			dia.consumer(c -> {
+				File fl;
+				String folder;
+				for(Map.Entry<FvtmType, Boolean> entry : map.entrySet()){
+					if(!entry.getValue()) continue;
+					folder = entry.getKey().folder;
+					fl = new File(pack.file, "/assets/" + pack.id + "/config/" + folder);
+					fl.mkdirs();
+					fl = new File(pack.file, "/assets/" + pack.id + "/textures/" + folder.substring(0, folder.length() - 1));
+					fl.mkdirs();
+					fl = new File(pack.file, "/assets/" + pack.id + "/models/" + folder.substring(0, folder.length() - 1));
+					fl.mkdirs();
+				}
+				FMT.WORKSPACE.reloadPacks(null);
+			}, null);
+			dia.buttons(100, DialogButton.CONFIRM, DialogButton.CANCEL);
+		});
+	}
+
+	public static void genIconsInPack(){
+		FMT.WORKSPACE.selectPack(pack -> {
+			for(List<FileElm> files : pack.content.values()){
+				for(FileElm elm : files){
+					JsonMap map = JsonHandler.parse(elm.file);
+					String cid = map.getString("ID", map.getString("RegistryName", null));
+					if(cid == null) return;
+					if(cid.contains(":")) cid = cid.split(":")[1];
+					pack.lang.fill(cid, map.getString("Name", "Unnamed Content"));
+					File file = new File(pack.file, "/assets/" + pack.id + "/models/item/" + cid + ".json");
+					if(!file.getParentFile().exists()) file.getParentFile().mkdirs();
+					if(!file.exists()){
+						map = new JsonMap();
+						map.add("parent", "item/generated");
+						map.add("textures", new JsonMap("layer0", pack.id + ":item/" + cid));
+						JsonHandler.print(file, map, JsonHandler.PrintOption.DEFAULT);
+					}
+					file = new File(pack.file, "/assets/" + pack.id + "/items/" + cid + ".json");
+					if(!file.getParentFile().exists()) file.getParentFile().mkdirs();
+					if(!file.exists()){
+						map = new JsonMap();
+						JsonMap mod = new JsonMap();
+						mod.add("type", "minecraft:model");
+						mod.add("model", pack.id + ":item/" + cid);
+						map.add("model", mod);
+						JsonHandler.print(file, map, JsonHandler.PrintOption.DEFAULT);
+					}
+					file = new File(pack.file, "/assets/" + pack.id + "/textures/item/" + cid + ".png");
+					if(!file.getParentFile().exists()) file.getParentFile().mkdirs();
+					if(!file.exists()){
+						try{
+							Files.copy(new File("./resources/textures/icons/configeditor/rename.png"), file);
+						}
+						catch(Exception e){
+							Logging.log(e);
+						}
+					}
+				}
+			}
+			FMT.UI.createDialog(500, 80, "workspace.content_utils")
+				.addText(0, "editor.config.pack_utils.gen_icons.complete")
+				.buttons(100, DialogButton.OK);
+			FMT.WORKSPACE.reloadPacks(null);
+		});
 	}
 
 }
