fca3d0e6e4af3d5c542739d7da43cc3bdd12a01b
Ferdinand Calo <fexcalo@gmail.com>
2026-05-02 14:37:31 +0200

    Update Workspace.java

--- src/net/fexcraft/app/fmt/workspace/Workspace.java
+++ src/net/fexcraft/app/fmt/workspace/Workspace.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 import net.fexcraft.app.fmt.FMT;
 import net.fexcraft.app.fmt.ui.Dialog.DialogButton;
@@ -22,6 +23,7 @@ import net.fexcraft.app.fmt.utils.Logging;
 import net.fexcraft.lib.common.math.RGB;
 
 import static net.fexcraft.app.fmt.settings.Settings.*;
+import static net.fexcraft.app.fmt.ui.editor.EditorTab.FS;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -31,17 +33,21 @@ public class Workspace extends Frame {
 	public static int FILES_PANEL_WIDTH = 400;
 	public static int FILES_PANEL_DIR = FILES_PANEL_WIDTH - 30;
 	private ArrayList<FvtmPackElm> fvtm_packs = new ArrayList<>();
+	private ArrayList<WFileEditor> file_editors = new ArrayList<>();
+	private WFileEditor file_editor;
+	protected Scrollable opened_files;
 	protected Scrollable packs;
 	protected CheckMode cm_in_packs;
+	protected Element files_open;
 	private boolean loaded;
 	public String name;
 	public File root_folder;
-	
+
 	public Workspace(){
 		name = Settings.WORKSPACE_NAME.value;
 		root_folder = new File(Settings.WORKSPACE_ROOT.value);
 		pos(20, 20);
-		size(1400, 800);
+		size(1200, 800);
 		color(GENERIC_BACKGROUND_0.value);
 		border(RGB.BLACK);
 	}
@@ -57,6 +63,38 @@ public class Workspace extends Frame {
 		packs.border(RGB.BLACK);
 		packs.updateBar();
 		cm_in_packs = CheckMode.gen(packs);
+		//
+		add(files_open = new Element().pos(FILES_PANEL_WIDTH, 31).size(w - FILES_PANEL_WIDTH - 1, 30)
+			.color(GENERIC_BACKGROUND_1.value).border(GENERIC_BACKGROUND_2.value)
+			.translate("workspace.file_editor.none_open").text_autoscale());
+		files_open.add(new Element().pos(files_open.w - 185, 2).size(FS, FS)
+			.texture("icons/component/list").border(GENERIC_BACKGROUND_0.value)
+			.onclick(ci -> opened_files.toggleVisibility())
+			.hint("workspace.file_editor.list"));
+		files_open.add(new Element().pos(files_open.w - 155, 2).size(FS, FS)
+			.texture("icons/component/edit").border(GENERIC_BACKGROUND_0.value)
+			.onclick(ci -> { if(file_editor != null) openExternal(file_editor.file); })
+			.hint("workspace.file_editor.external"));
+		files_open.add(new Element().pos(files_open.w - 125, 2).size(FS, FS)
+			.texture("icons/toolbar/save").border(GENERIC_BACKGROUND_0.value)
+			.onclick(ci -> { if(file_editor != null) file_editor.save(); })
+			.hint("workspace.file_editor.save"));
+		files_open.add(new Element().pos(files_open.w - 95, 2).size(FS, FS)
+			.texture("icons/component/move_left").border(GENERIC_BACKGROUND_0.value)
+			.onclick(ci -> { setActive(file_editor, -1); })
+			.hint("workspace.file_editor.prev"));
+		files_open.add(new Element().pos(files_open.w - 65, 2).size(FS, FS)
+			.texture("icons/component/move_right").border(GENERIC_BACKGROUND_0.value)
+			.onclick(ci -> { setActive(file_editor, 1); })
+			.hint("workspace.file_editor.next"));
+		files_open.add(new Element().pos(files_open.w - 35, 2).size(FS, FS)
+			.texture("icons/component/exit").border(GENERIC_BACKGROUND_0.value)
+			.onclick(ci -> closeActive())
+			.hint("workspace.file_editor.close"));
+		files_open.add(opened_files = new Scrollable(true, 0));
+		opened_files.updateSize(400, 400);
+		opened_files.pos(files_open.w - 440, 30);
+		opened_files.border(RGB.BLACK).hide();
 	}
 
 	public void update(Setting<?> setting){
@@ -183,7 +221,8 @@ public class Workspace extends Frame {
 		}
 	}
 
-	public static void open(File file){
+	public static void openExternal(File file){
+		if(file == null || !file.exists()) return;
 		String cmd = Settings.TEXT_EDITOR.value.formatted(file.getPath());
 		try{
 			Process pr = Runtime.getRuntime().exec(cmd.split(" "), null, file.getParentFile());
@@ -205,6 +244,52 @@ public class Workspace extends Frame {
 			Logging.log("Opening external editor failed.");
 			Logging.log("=================");
 		}
+	}
+
+	public void open(File file, Function<File, WFileEditor> func){
+		for(WFileEditor fe : file_editors){
+			if(fe.file.equals(file)){
+				setActive(fe, 0);
+				return;
+			}
+		}
+		WFileEditor edit = func.apply(file);
+		edit.size(w - FILES_PANEL_WIDTH - 1, h - 61).pos(FILES_PANEL_WIDTH + 1, 61);
+		file_editors.add(edit);
+		add(edit);
+		setActive(edit, 0);
+		opened_files.add(new WFileEditor.WFileEditorEntry(edit).size(opened_files.w - 30, FS));
+		opened_files.updateBar();
+	}
+
+	protected void setActive(WFileEditor edit, int changeindex){
+		if(changeindex != 0){
+			int idx = file_editors.indexOf(edit) + changeindex;
+			if(idx < 0) idx = file_editors.size() - 1;
+			if(idx >= file_editors.size()) idx = 0;
+			edit = file_editors.get(idx);
+		}
+		files_open.text(edit.get_title());
+		opened_files.hide();
+		for(Element editor : file_editors) editor.hide();
+		file_editor = edit;
+		edit.show();
+	}
+
+	protected void closeActive(){
+		if(file_editors.size() < 2){
+			file_editor = null;
+			file_editors.clear();
+			opened_files.clear();
+			files_open.translate("workspace.file_editor.none_open");
+		}
+		else{
+			opened_files.remElmIf(elm -> elm instanceof WFileEditor.WFileEditorEntry entry && entry.editor == file_editor);
+			file_editors.remove(file_editor);
+			file_editor = file_editors.get(file_editors.size() - 1);
+			setActive(file_editor, 0);
+		}
+		opened_files.updateBar();
 	}
 
 }
