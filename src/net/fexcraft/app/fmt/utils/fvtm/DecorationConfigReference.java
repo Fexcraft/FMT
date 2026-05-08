e607ac4a1c808bda6f04e67ff6f92df129054204
Ferdinand Calo <fexcalo@gmail.com>
2026-05-08 23:05:56 +0200

    Update DecorationConfigReference.java

--- src/net/fexcraft/app/fmt/utils/fvtm/DecorationConfigReference.java
+++ src/net/fexcraft/app/fmt/utils/fvtm/DecorationConfigReference.java
@@ -18,7 +18,7 @@ public class DecorationConfigReference extends ConfigReference {
 		entries.add(of("CreativeTab", TEXT).def("default", true));
 		entries.add(of("Description", ARRAY_SIMPLE).add(of(TEXT).def("description entry", true)));
 		entries.add(of("Model", MODELLOC));
-		entries.add(of("ModelData", SEPARATE));
+		entries.add(of("ModelData", SEPARATE).static_(true).of(ModelDataReference.INSTANCE));
 		entries.add(of("Textures", ARRAY_SIMPLE).add(of(TEXLOC)).alt("Texture"));
 		entries.add(of("RandomTexture", BOOLEAN));
 		entries.add(of("ColorChannels", OBJECT_KEY_VAL).add(of(COLOR).def("#fdfdfd", true)));
