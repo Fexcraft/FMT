e5a8598998d4b0139e37e03e1e58666833c3166c
Ferdinand Calo <fexcalo@gmail.com>
2026-05-09 00:43:40 +0200

    Update PartConfigReference.java

--- src/net/fexcraft/app/fmt/utils/fvtm/PartConfigReference.java
+++ src/net/fexcraft/app/fmt/utils/fvtm/PartConfigReference.java
@@ -1,7 +1,5 @@
 package net.fexcraft.app.fmt.utils.fvtm;
 
-import java.util.ArrayList;
-
 import static net.fexcraft.app.fmt.utils.fvtm.ConfigEntry.TEXT_ENTRY;
 import static net.fexcraft.app.fmt.utils.fvtm.ConfigEntry.of;
 import static net.fexcraft.app.fmt.utils.fvtm.EntryType.*;
@@ -31,10 +29,10 @@ public class PartConfigReference extends ConfigReference {
 		entries.add(of("Textures", ARRAY_SIMPLE).add(of(TEXLOC)).alt("Texture"));
 		entries.add(of("ItemTexture", TEXLOC));
 		entries.add(of("Category", ARRAY_SIMPLE).add(TEXT_ENTRY));
-		entries.add(of("Installation", ENUM_SEPARATE).enums(installhandlers).add(of("Handler", STATIC)));
+		entries.add(of("Installation", ENUM_SEPARATE).refs(PartInstallConfigReference.REFERENCES).def("default").add(of("Handler", STATIC)));
 		VehicleConfigReference.addAttrsEntry(entries);
 		entries.add(of("AttributeModifiers", OBJECT_KEY_VAL).add(TEXT_ENTRY));
-		entries.add(of("Functions", OBJECT_KEY_VAL).add(of(ENUM_SEPARATE).enums(partfunctions)));
+		entries.add(of("Functions", OBJECT_KEY_VAL).add(of(ENUM_SEPARATE).refs(PartFunctionConfigReference.REFERENCES)));
 		VehicleConfigReference.addPointsEntry(entries);
 		entries.add(of("Sounds", OBJECT).add(
 			of("volume", DECIMAL).limit(1, 0),
