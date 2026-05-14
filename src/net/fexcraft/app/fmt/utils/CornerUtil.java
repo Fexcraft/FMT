220fc101057f5f4854c9b0cae8a33721fbac91ce
Ferdinand Calo <fexcalo@gmail.com>
2026-05-14 13:26:05 +0200

    Update CornerUtil.java

--- src/net/fexcraft/app/fmt/utils/CornerUtil.java
+++ src/net/fexcraft/app/fmt/utils/CornerUtil.java
@@ -16,9 +16,9 @@ import net.fexcraft.lib.tmt.ModelRendererTurbo;
 
 public class CornerUtil {
 
-	public static Polyhedron<GLObject> ROT_MARKER_NORMAL;
-	public static Polyhedron<GLObject> ROT_MARKER_SMALL;
-	private static Polyhedron<GLObject>[] CORNER_MARKER = new Polyhedron[8];
+	public static Polyhedron ROT_MARKER_NORMAL;
+	public static Polyhedron ROT_MARKER_SMALL;
+	private static Polyhedron[] CORNER_MARKER = new Polyhedron[8];
 	public static RGB[] CORNER_COLOURS = new RGB[]{
 		new RGB(255, 255, 0),//yellow
 		new RGB(255, 0, 0),//red
@@ -38,18 +38,18 @@ public class CornerUtil {
 		if(ROT_MARKER_NORMAL != null){
 			ROT_MARKER_NORMAL.delete();
 			ROT_MARKER_SMALL.delete();
-			for(Polyhedron<GLObject> poly : CORNER_MARKER){
+			for(Polyhedron poly : CORNER_MARKER){
 				poly.delete();
 			}
 		}
-		ROT_MARKER_NORMAL = new Polyhedron<GLObject>().importMRT(new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-.25f, -.25f, -.25f, .5f, .5f, .5f), false, EditorRoot.MARKER_SCALE);
-		ROT_MARKER_SMALL = new Polyhedron<GLObject>().importMRT(new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-.2f, -.2f, -.2f, .4f, .4f, .4f), false, EditorRoot.MARKER_SCALE * 0.5f);
+		ROT_MARKER_NORMAL = new Polyhedron().importMRT(new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-.25f, -.25f, -.25f, .5f, .5f, .5f), false, EditorRoot.MARKER_SCALE);
+		ROT_MARKER_SMALL = new Polyhedron().importMRT(new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-.2f, -.2f, -.2f, .4f, .4f, .4f), false, EditorRoot.MARKER_SCALE * 0.5f);
 		for(int i = 0; i < 8; i++){
-			CORNER_MARKER[i] = new Polyhedron<GLObject>().importMRT(new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-.25f, -.25f, -.25f, .5f, .5f, .5f), false, EditorRoot.MARKER_SCALE);
-			CORNER_MARKER[i].setGlObj(new GLObject()).glObj.polycolor = CORNER_COLOURS[i].toFloatArray();
+			CORNER_MARKER[i] = new Polyhedron().importMRT(new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-.25f, -.25f, -.25f, .5f, .5f, .5f), false, EditorRoot.MARKER_SCALE);
+			CORNER_MARKER[i].glObj(GLObject.class).polycolor = CORNER_COLOURS[i].toFloatArray();
 		}
-		ROT_MARKER_NORMAL.setGlObj(new GLObject()).glObj.polycolor = RGB.GREEN.toFloatArray();
-		ROT_MARKER_SMALL.setGlObj(new GLObject()).glObj.polycolor = RGB.WHITE.toFloatArray();
+		ROT_MARKER_NORMAL.glObj(GLObject.class).polycolor = RGB.GREEN.toFloatArray();
+		ROT_MARKER_SMALL.glObj(GLObject.class).polycolor = RGB.WHITE.toFloatArray();
 	}
 
 	public static void renderCorners(){
