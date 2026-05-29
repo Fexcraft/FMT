9fbba15250f8fac34ff74f3c6644cf35db9ccf1e
Ferdinand Calo <fexcalo@gmail.com>
2026-05-30 00:00:55 +0200

    Update ObjPolyCom.java

--- src/net/fexcraft/app/fmt/ui/tree/ObjPolyCom.java
+++ src/net/fexcraft/app/fmt/ui/tree/ObjPolyCom.java
@@ -30,6 +30,7 @@ public class ObjPolyCom extends TTabCom implements GroupCom.GroupComSubElm {
 	public static PolygonValue OBJ_FACES = new PolygonValue(PolyVal.OBJ_FACES);
 	public static PolygonValue VERT_ACT = new PolygonValue(PolyVal.VERT_ACTIVE);
 	public static PolygonValue OBJ_FACE_ACT = new PolygonValue(PolyVal.OBJ_FACE_ACTIVE);
+	public static PolygonValue OBJ_FACE_VRT = new PolygonValue(PolyVal.OBJ_FACE_VERTEX);
 	protected PolyObject polygon;
 
 	public ObjPolyCom(Polygon poly){
@@ -51,6 +52,10 @@ public class ObjPolyCom extends TTabCom implements GroupCom.GroupComSubElm {
 			.onclick(ci -> FMT.MODEL.askAndRemove(polygon)).hint("tree.polygon.polygon.remove").hide());
 		add(new HidingElm().hoverable(true).texture("icons/component/edit").size(26, 26).pos(EDITOR_CONTENT - 34 * 4, 1)
 			.onclick(ci -> EditorRoot.setMode(EditorRoot.EditorMode.POLYGON)).hint("tree.polygon.polygon.editor").hide());
+		add(new HidingElm().hoverable(true).texture("icons/component/add").size(26, 26).pos(EDITOR_CONTENT - 34 * 5, 1)
+			.onclick(ci -> polygon.addFace()).hint("tree.polygon.object.add_face").hide());
+		add(new HidingElm().hoverable(true).texture("icons/component/add").size(26, 26).pos(EDITOR_CONTENT - 34 * 6, 1)
+			.onclick(ci -> polygon.addVertex()).hint("tree.polygon.object.add_vertex").hide());
 		updateLabelColor();
 		container.hide();
 	}
@@ -75,19 +80,25 @@ public class ObjPolyCom extends TTabCom implements GroupCom.GroupComSubElm {
 			container.add(new Element().pos(2, size).size(FS, FS)
 				.color(polygon.vertoffs.get(polygon.vectors.get(vi)).color).onclick(ci -> {
 					if(!polygon.selected) return;
-					try(MemoryStack stack = MemoryStack.stackPush()){
-						ByteBuffer color = stack.malloc(3);
-						String result = TinyFileDialogs.tinyfd_colorChooser("Choose a Color", "#" + Integer.toHexString(polygon.vertoffs.get(polygon.vectors.get(vi)).color), null, color);
-						if(result == null) return;
-						if(polygon.selvec != vi) FMT.MODEL.updateValue(VERT_ACT, null, vi, true);
-						int col = Integer.parseInt(result.replace("#", ""), 16);
-						polygon.vertoffs.get(polygon.vectors.get(vi)).color = col;
-						polygon.vertoffs.get(polygon.vectors.get(vi)).arr_color = new RGB(col).toFloatArray();
-						FMT.MODEL.updateValue(COLOR, null, col, true);
+					int col = 0xffffff;
+					if(ci.button() == 1){
+						col = RGB.random().packed;
 					}
-					catch(Exception e){
-						e.printStackTrace();
+					else{
+						try(MemoryStack stack = MemoryStack.stackPush()){
+							ByteBuffer color = stack.malloc(3);
+							String result = TinyFileDialogs.tinyfd_colorChooser("Choose a Color", "#" + Integer.toHexString(polygon.vertoffs.get(polygon.vectors.get(vi)).color), null, color);
+							if(result == null) return;
+							col = Integer.parseInt(result.replace("#", ""), 16);
+						}
+						catch(Exception e){
+							e.printStackTrace();
+						}
 					}
+					refresh();//if(polygon.selvec != vi) FMT.MODEL.updateValue(VERT_ACT, null, vi, true);
+					polygon.vertoffs.get(polygon.vectors.get(vi)).color = col;
+					polygon.vertoffs.get(polygon.vectors.get(vi)).arr_color = new RGB(col).toFloatArray();
+					FMT.MODEL.updateValue(COLOR, null, col, true);
 				}));
 			container.add(new Element().pos(30, size).size(EDITOR_CONTENT - 45, FS)
 				.text("vector " + v).defTextPos()
@@ -100,18 +111,53 @@ public class ObjPolyCom extends TTabCom implements GroupCom.GroupComSubElm {
 						FMT.MODEL.select(Pair.of(polygon, polygon.vectors.get(vi)));
 					}
 				}));
+			container.lastElement().add(new HidingElm().hoverable(true)
+				.texture("icons/component/remove").size(24, 24).pos(container.lastElement().w - 26, 1)
+				.onclick(ci -> {
+					if(polygon.selected && av) polygon.removeVertex(vi);
+				})
+				.hint("tree.polygon.object.remove_vertex").hide());
 			size += 28;
 		}
 		for(int f = 0; f < polygon.faces.size(); f++){
 			boolean af = polygon.selfac == f;
+			PolyObject.ObjFace face = polygon.faces.get(f);
 			int fi = f;
-			container.add(new Element().pos(2, size).size(EDITOR_CONTENT - 14, FS)
+			for(int vi = 0; vi < face.vecs.length; vi++){
+				int v = vi;
+				container.add(new Element().pos(2 + vi * 20, size).size(20, FS)
+					.color(polygon.vertoffs.get(polygon.vectors.get(face.vecs[v])).color)
+					.onclick(ci -> {
+						int i = face.vecs[v] + (ci.button() == 0 ? 1 : -1);
+						if(i >= polygon.vectors.size()) i = 0;
+						if(i < 0) i = polygon.vectors.size() - 1;
+						face.vecs[v] = i;
+						polygon.recompile();
+						refresh();
+					}));
+			}
+			container.add(new Element().pos(85, size).size(EDITOR_CONTENT - 100, FS)
 				.text("face " + f).defTextPos()
 				.text_color(GENERIC_TEXT_0.value.packed)
 				.color(af ? POLYGON_INV_SEL.value : GENERIC_BACKGROUND_1.value)
 				.onclick(ci -> {
 					if(polygon.selected) FMT.MODEL.updateValue(OBJ_FACE_ACT, null, fi, true);
 				}));
+			container.lastElement().add(new HidingElm().hoverable(true)
+				.texture("icons/component/remove").size(24, 24).pos(container.lastElement().w - 26, 1)
+				.onclick(ci -> {
+					if(polygon.selected && af) polygon.removeFace(fi);
+				})
+				.hint("tree.polygon.object.remove_face").hide());
+			container.lastElement().add(new HidingElm().hoverable(true)
+				.texture("icons/component/adjust").size(24, 24).pos(container.lastElement().w - 52, 1)
+				.onclick(ci -> {
+					if(polygon.selected && af){
+						polygon.toggleTriangleQuad(fi);
+						FMT.MODEL.updateValue(OBJ_FACE_ACT, null, fi, true);
+					}
+				})
+				.hint("tree.polygon.object.triangle_quad").hide());
 			size += 28;
 		}
 		container.size(EDITOR_CONTENT - 10, size);
