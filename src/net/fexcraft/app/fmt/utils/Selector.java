e6035ee0e5346da2b49cb80bfc9723923b8edb08
Ferdinand Calo <fexcalo@gmail.com>
2026-05-30 23:45:54 +0200

    Update Selector.java

--- src/net/fexcraft/app/fmt/utils/Selector.java
+++ src/net/fexcraft/app/fmt/utils/Selector.java
@@ -4,14 +4,9 @@ import net.fexcraft.app.fmt.FMT;
 import net.fexcraft.app.fmt.polygon.Pivot;
 import net.fexcraft.app.fmt.polygon.Polygon;
 import net.fexcraft.app.fmt.polygon.Vertoff;
-import net.fexcraft.app.fmt.polygon.Vertoff.VOKey;
-import net.fexcraft.app.fmt.update.UpdateEvent;
-import net.fexcraft.app.fmt.update.UpdateEvent.VertexSelected;
-import net.fexcraft.app.fmt.update.UpdateHandler;
+import net.fexcraft.app.fmt.polygon.Vertoff.VOSelection;
 import net.fexcraft.app.fmt.utils.Picker.PickType;
 import net.fexcraft.lib.common.math.V3D;
-import net.fexcraft.lib.common.math.Vec3f;
-import org.apache.commons.lang3.tuple.Pair;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -31,21 +26,21 @@ public class Selector {
 
 	public static void move(){
 		if(FMT.MODEL.getSelectedVerts().size() < 2) return;
-		Pair<Polygon, VOKey> vo0 = FMT.MODEL.getSelectedVerts().get(0);
-		Pair<Polygon, VOKey> vo1 = FMT.MODEL.getSelectedVerts().get(1);
-		V3D v0 = vo0.getLeft().vertoffs.get(vo0.getRight()).cache;
-		V3D v1 = vo1.getLeft().vertoffs.get(vo1.getRight()).cache;
-		Pivot pi0 = FMT.MODEL.getP(vo0.getLeft().group().pivot);
+		VOSelection vo0 = FMT.MODEL.getSelectedVerts().get(0);
+		VOSelection vo1 = FMT.MODEL.getSelectedVerts().get(1);
+		V3D v0 = vo0.vertoff().cache;
+		V3D v1 = vo1.vertoff().cache;
+		Pivot pi0 = FMT.MODEL.getP(vo0.polygon().group().pivot);
 		Polygon.vo_axe.setDegrees(-pi0.rot.y, -pi0.rot.z, -pi0.rot.x);
 		v0 = Polygon.vo_axe.rotate(v0);
-		Pivot pi1 = FMT.MODEL.getP(vo1.getLeft().group().pivot);
+		Pivot pi1 = FMT.MODEL.getP(vo1.polygon().group().pivot);
 		Polygon.vo_axe.setDegrees(-pi1.rot.y, -pi1.rot.z, -pi1.rot.x);
 		v1 = Polygon.vo_axe.rotate(v1);
-		Vertoff off = vo0.getLeft().vertoffs.get(vo0.getRight());
+		Vertoff off = vo0.vertoff();
 		off.off.x = (float)(v1.x - v0.x);
 		off.off.y = (float)(v1.y - v0.y);
 		off.off.z = (float)(v1.z - v0.z);
-		vo0.getLeft().recompile();
+		vo0.polygon().recompile();
 		Logging.bar("Offset applied if applicable.");
 		FMT.MODEL.clearSelectedVerts();
 	}
