e993ca2c05d2a24f3ebbb3506cb69d11198f932b
Ferdinand Calo <fexcalo@gmail.com>
2026-06-05 23:34:05 +0200

    Create Container.java

---
+++ src/net/fexcraft/app/fmt/ui/Container.java
@@ -0,0 +1,13 @@
+package net.fexcraft.app.fmt.ui;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class Container extends Element {
+
+	@Override
+	public boolean isContainer(){
+		return true;
+	}
+
+}
