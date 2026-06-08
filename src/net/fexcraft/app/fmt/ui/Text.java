7371e1b56853ed6d702e0ff515773230a5806465
Ferdinand Calo <fexcalo@gmail.com>
2026-06-08 22:50:37 +0200

    Update Text.java

--- src/net/fexcraft/app/fmt/ui/Text.java
+++ src/net/fexcraft/app/fmt/ui/Text.java
@@ -1,10 +1,13 @@
 package net.fexcraft.app.fmt.ui;
 
 import net.fexcraft.app.fmt.polygon.GLObject;
+import net.fexcraft.app.fmt.polygon.Vector3F;
 import net.fexcraft.app.fmt.texture.TextureManager;
 import net.fexcraft.lib.common.math.RGB;
-import net.fexcraft.lib.frl.Polygon;
 import net.fexcraft.lib.frl.Polyhedron;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static net.fexcraft.app.fmt.settings.Settings.GENERIC_TEXT_0;
 
@@ -14,7 +17,7 @@ import static net.fexcraft.app.fmt.settings.Settings.GENERIC_TEXT_0;
 public class Text {
 
 	public final Element root;
-	public Polyhedron hedron;
+	public List<CharInfo> chardata = new ArrayList<>();
 	public FontRenderer.FontType font = FontRenderer.FontType.PLAIN;
 	public RGB color = GENERIC_TEXT_0.value.copy();
 	public boolean autoscale;
@@ -26,17 +29,15 @@ public class Text {
 	public float h;
 	public float x = 5;
 	public float y = 0;
+	public float rx;
+	public float ry;
 
 	public Text(Element elm){
-		hedron = new Polyhedron();
 		root = elm;
 	}
 
 	public void recompile(){
-		hedron.recompile = true;
-		hedron.polygons.clear();
-		//if(hedron.glObj.pickercolor == null) hedron.glObj.pickercolor = root.hedron.glObj.pickercolor;
-		hedron.glObj(GLObject.class).textured = true;
+		chardata.clear();
 		if(autoscale){
 			w = FontRenderer.getWidth(text, font);
 			scale = (root.w - 5 - x) < w ? (root.w - 5 - x) / w : 1;
@@ -48,14 +49,17 @@ public class Text {
 			h = FontRenderer.getHeight(text, font) * scale;
 		}
 		root.postext();
-		FontRenderer.compile(this, text, font, color);
+		FontRenderer.compile(this, text, font, color.toFloatArray());
 	}
 
 	public void render(){
 		TextureManager.bind("font/" + font);
-		//PolyRenderer.mode(PolyRenderer.DrawMode.UI_TINTED);
-		hedron.render();
-		//PolyRenderer.mode(PolyRenderer.DrawMode.UI);
+		GLObject obj;
+		for(CharInfo info : chardata){
+			obj = info.hedron.glObj();
+			obj.polycolor = info.col;
+			info.hedron.pos(rx + info.pos.x, ry + info.pos.y, info.pos.z).render();
+		}
 	}
 
 	public Text text(Object ntext){
@@ -80,12 +84,31 @@ public class Text {
 
 	public void color(int col){
 		color.packed = col;
-		if(hedron.polygons.size() > 0){
-			for(Polygon polygon : hedron.polygons){
-				polygon.color(color);
-			}
-			hedron.recompile = true;
+		float[] arr = color.toFloatArray();;
+		for(CharInfo info : chardata){
+			info.col = arr;
 		}
+	}
+
+	public void postext(){
+		rx = root.hedron.posX;
+		ry = root.hedron.posY;
+		rx += centered ? (root.w - w) * 0.5f : x;
+		ry += centered ? (root.h - h) * 0.5f : y;
+	}
+
+	public static class CharInfo {
+
+		private Polyhedron hedron;
+		private Vector3F pos;
+		private float[] col;
+
+		public CharInfo(Polyhedron hed, Vector3F vec, float[] arr){
+			hedron = hed;
+			pos = vec;
+			col = arr;
+		}
+
 	}
 
 }
