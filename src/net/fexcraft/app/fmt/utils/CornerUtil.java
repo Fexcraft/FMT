62b810c7416fa96ce9fcc9fe8be14a5243fbc89b
Ferdinand Calo <fexcalo@gmail.com>
2026-09-12 23:01:09 +0200

    Update CornerUtil.java

--- src/net/fexcraft/app/fmt/utils/CornerUtil.java
+++ src/net/fexcraft/app/fmt/utils/CornerUtil.java
@@ -5,13 +5,14 @@ import net.fexcraft.app.fmt.ui.editor.EditorRoot;
 import net.fexcraft.app.fmt.polygon.GLObject;
 import net.fexcraft.lib.common.math.RGB;
 import net.fexcraft.lib.frl.Polyhedron;
-import net.fexcraft.lib.tmt.ModelRendererTurbo;
+import net.fexcraft.lib.frl.gen.Generator;
+
+import static net.fexcraft.lib.frl.gen.Generator.Values.*;
 
 public class CornerUtil {
 
 	public static Polyhedron ROT_MARKER_NORMAL;
 	public static Polyhedron ROT_MARKER_SMALL;
-	//private static Polyhedron[] CORNER_MARKER = new Polyhedron[8];
 	public static RGB[] CORNER_COLOURS = new RGB[]{
 		new RGB(255, 255, 0),//yellow
 		new RGB(255, 0, 0),//red
@@ -30,16 +31,15 @@ public class CornerUtil {
 		if(ROT_MARKER_NORMAL != null){
 			ROT_MARKER_NORMAL.delete();
 			ROT_MARKER_SMALL.delete();
-			/*for(Polyhedron poly : CORNER_MARKER){
-				poly.delete();
-			}*/
 		}
-		ROT_MARKER_NORMAL = new Polyhedron().importMRT(new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-.25f, -.25f, -.25f, .5f, .5f, .5f), false, EditorRoot.MARKER_SCALE);
-		ROT_MARKER_SMALL = new Polyhedron().importMRT(new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-.2f, -.2f, -.2f, .4f, .4f, .4f), false, EditorRoot.MARKER_SCALE * 0.5f);
-		/*for(int i = 0; i < 8; i++){
-			CORNER_MARKER[i] = new Polyhedron().importMRT(new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-.25f, -.25f, -.25f, .5f, .5f, .5f), false, EditorRoot.MARKER_SCALE);
-			CORNER_MARKER[i].glObj(GLObject.class).polycolor = CORNER_COLOURS[i].toFloatArray();
-		}*/
+		ROT_MARKER_NORMAL = new Generator(Generator.Type.CUBOID)
+			.set(OFF_X, -.25f).set(OFF_Y, -.25f).set(OFF_Z, -.25f)
+			.set(WIDTH, .5f).set(HEIGHT, .5f).set(DEPTH, .5f)
+			.set(SCALE, EditorRoot.MARKER_SCALE).make();
+		ROT_MARKER_SMALL = new Generator(Generator.Type.CUBOID)
+			.set(OFF_X, -.2f).set(OFF_Y, -.2f).set(OFF_Z, -.2f)
+			.set(WIDTH, .4f).set(HEIGHT, .4f).set(DEPTH, .4f)
+			.set(SCALE, EditorRoot.MARKER_SCALE * 0.5f).make();
 		ROT_MARKER_NORMAL.glObj(GLObject.class).polycolor = RGB.GREEN.toFloatArray();
 		ROT_MARKER_SMALL.glObj(GLObject.class).polycolor = RGB.WHITE.toFloatArray();
 	}
